3e9f9718d920f403cabafdc92f21be910c42f07e
Jochen Kupperschmidt <homework@nwsnet.de>
2014-07-29 16:49:19 +0200

    Set maximum content length for production.

--- webapp/config/production.py
+++ webapp/config/production.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 
 DEBUG = False
+MAX_CONTENT_LENGTH = 4000000
 PERMANENT_SESSION_LIFETIME = timedelta(14)
 SECRET_KEY = b'\xcb;\xcd\xdc\x11\xf2\xa8\x08_Ks\xa3\xb0\xc5K/\x9b\xf1\xd9\xdc_\x16\x8e\xa8'
 
