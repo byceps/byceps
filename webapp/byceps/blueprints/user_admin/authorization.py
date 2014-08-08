acb29c3aedbec05f9bc31350231b4a905b7337d3
Jochen Kupperschmidt <homework@nwsnet.de>
2014-08-08 20:02:53 +0200

    Introduced user permissions and protected user list.

---
+++ webapp/byceps/blueprints/user_admin/authorization.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+byceps.blueprints.user_admin.authorization
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Copyright: 2006-2014 Jochen Kupperschmidt
+"""
+
+from byceps.util.authorization import create_permission_enum
+
+
+UserPermission = create_permission_enum('User', [
+    'list',
+])
--- webapp/byceps/blueprints/user_admin/views.py
+++ webapp/byceps/blueprints/user_admin/views.py
@@ -10,13 +10,21 @@ byceps.blueprints.user_admin.views
 from ...util.framework import create_blueprint
 from ...util.templating import templated
 
+from ..authorization.decorators import permission_required
+from ..authorization.registry import permission_registry
 from ..user.models import User
+
+from .authorization import UserPermission
 
 
 blueprint = create_blueprint('user_admin', __name__)
 
 
+permission_registry.register_enum('user', UserPermission)
+
+
 @blueprint.route('/')
+@permission_required(UserPermission.list)
 @templated
 def index():
     """List users."""
