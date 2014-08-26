6c5a6471060ca6ef0a61d678d92c2b9cc2791a31
Jochen Kupperschmidt <homework@nwsnet.de>
2014-08-26 02:09:55 +0200

    Refactored test.

--- webapp/tests/blueprints/authorization/test_registry.py
+++ webapp/tests/blueprints/authorization/test_registry.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from enum import Enum
 from unittest import TestCase
 
 from byceps.blueprints.authorization.models import Permission
 from byceps.blueprints.authorization.registry import PermissionRegistry
+from byceps.util.authorization import create_permission_enum
 
 
 class PermissionRegistryTestCase(TestCase):
@@ -14,15 +14,15 @@ class PermissionRegistryTestCase(TestCase):
         self.registry.register_enum('item', ItemPermission)
 
     def test_lookup_of_existing_enum_member(self):
-        permission = Permission(id='item.create')
+        permission = create_permission('item.create')
         self.assert_get_enum_member_equals(permission, ItemPermission.create)
 
     def test_lookup_of_nonexistent_member_of_existing_enum(self):
-        permission = Permission(id='item.delete')
+        permission = create_permission('item.delete')
         self.assert_get_enum_member_equals(permission, None)
 
     def test_lookup_of_member_of_nonexistent_enum(self):
-        permission = Permission(id='article.create')
+        permission = create_permission('article.create')
         self.assert_get_enum_member_equals(permission, None)
 
     def assert_get_enum_member_equals(self, permission, expected):
@@ -30,4 +30,8 @@ class PermissionRegistryTestCase(TestCase):
         self.assertEqual(actual, expected)
 
 
-ItemPermission = Enum('ItemPermission', 'view create update')
+ItemPermission = create_permission_enum('item', ['view', 'create', 'update'])
+
+
+def create_permission(id):
+    return Permission(id=id)
