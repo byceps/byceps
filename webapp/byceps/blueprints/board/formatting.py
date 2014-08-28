59c11e0c7fa4c2da0147849a93b4e455e41cbc21
Jochen Kupperschmidt <homework@nwsnet.de>
2014-08-28 03:29:59 +0200

    Fixed BBcode parser to both auto-link URLs and replace image tags. Added tests.

--- webapp/byceps/blueprints/board/formatting.py
+++ webapp/byceps/blueprints/board/formatting.py
@@ -12,11 +12,11 @@ import bbcode
 
 def render_html(value):
     """Render text as HTML, interpreting BBcode."""
-    # No automatic link replacement for now as that interfers with
-    # image embedding.
-    parser = bbcode.Parser(replace_cosmetic=False, replace_links=False)
+    parser = bbcode.Parser(replace_cosmetic=False)
 
     # Replace image tags.
-    parser.add_simple_formatter('img', '<img src="%(value)s"/>')
+    def render_image(name, value, options, parent, context):
+        return '<img src="{}"/>'.format(value)
+    parser.add_formatter('img', render_image, replace_links=False)
 
     return parser.format(value)
---
+++ webapp/tests/blueprints/board/test_formatting.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from unittest import TestCase
+
+from byceps.blueprints.board.formatting import render_html
+
+
+class RenderBbcodeTestCase(TestCase):
+
+    def test_auto_url_linking(self):
+        text = 'before http://example.com/index.html after'
+        expected = 'before <a href="http://example.com/index.html">http://example.com/index.html</a> after'
+        self.assert_rendered_html(text, expected)
+
+    def test_explicit_url_linking(self):
+        text = 'before [url]http://example.com/index.html[/url] after'
+        expected = 'before <a href="http://example.com/index.html">http://example.com/index.html</a> after'
+        self.assert_rendered_html(text, expected)
+
+    def test_labeled_url_linking(self):
+        text = 'before [url=http://example.com/index.html]Example[/url] after'
+        expected = 'before <a href="http://example.com/index.html">Example</a> after'
+        self.assert_rendered_html(text, expected)
+
+    def test_image(self):
+        text = 'before [img]http://example.com/image.png[/img] after'
+        expected = 'before <img src="http://example.com/image.png"/> after'
+        self.assert_rendered_html(text, expected)
+
+    def test_linked_image(self):
+        text = 'before [url=http://example.com/index.html][img]http://example.com/image.png[/img][/url] after'
+        expected = 'before <a href="http://example.com/index.html"><img src="http://example.com/image.png"/></a> after'
+        self.assert_rendered_html(text, expected)
+
+    def assert_rendered_html(self, text, expected):
+        actual = render_html(text)
+        self.assertEqual(actual, expected)
