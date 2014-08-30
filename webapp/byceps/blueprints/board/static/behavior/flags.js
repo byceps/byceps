b984a6c48d1957f189293f2c248cdd402e985b4c
Jochen Kupperschmidt <homework@nwsnet.de>
2014-08-30 18:38:52 +0200

    Extracted JavaScript utility functions into separate file.

--- webapp/byceps/blueprints/board/static/behavior/flags.js
+++ webapp/byceps/blueprints/board/static/behavior/flags.js
@@ -10,41 +10,4 @@ $(function() {
   confirmed_post_on_click('a#posting-hide', 'Beitrag verstecken?');
   confirmed_delete_on_click('a#posting-unhide', 'Beitrag wieder anzeigen?');
 
-  /* utilities */
-
-  function confirmed_post_on_click(selector, confirmation_label) {
-    _confirmed_request_on_click(selector, confirmation_label, 'POST');
-  }
-
-  function confirmed_delete_on_click(selector, confirmation_label) {
-    _confirmed_request_on_click(selector, confirmation_label, 'DELETE');
-  }
-
-  function _confirmed_request_on_click(selector, confirmation_label, method) {
-    $(selector).click(function() {
-      if (confirm(confirmation_label)) {
-        var request_url = $(this).attr('href');
-        _ajax_and_redirect(method, request_url);
-      };
-      return false;
-    });
-  }
-
-  function _ajax_and_redirect(method, request_url) {
-    $.ajax({
-      type: method,
-      url: request_url,
-      complete: function(xhr, text_status) {
-        if (text_status == 'nocontent') {
-          var redirect_url = _get_location(xhr);
-          location.href = redirect_url;
-        }
-      }
-    });
-  }
-
-  function _get_location(xhr) {
-    return xhr.getResponseHeader('Location');
-  }
-
 });
---
+++ webapp/byceps/blueprints/core/static/behavior/utils.js
@@ -0,0 +1,34 @@
+function confirmed_post_on_click(selector, confirmation_label) {
+  _confirmed_request_on_click(selector, confirmation_label, 'POST');
+}
+
+function confirmed_delete_on_click(selector, confirmation_label) {
+  _confirmed_request_on_click(selector, confirmation_label, 'DELETE');
+}
+
+function _confirmed_request_on_click(selector, confirmation_label, method) {
+  $(selector).click(function() {
+    if (confirm(confirmation_label)) {
+      var request_url = $(this).attr('href');
+      _ajax_and_redirect(method, request_url);
+    };
+    return false;
+  });
+}
+
+function _ajax_and_redirect(method, request_url) {
+  $.ajax({
+    type: method,
+    url: request_url,
+    complete: function(xhr, text_status) {
+      if (text_status == 'nocontent') {
+        var redirect_url = _get_location(xhr);
+        location.href = redirect_url;
+      }
+    }
+  });
+}
+
+function _get_location(xhr) {
+  return xhr.getResponseHeader('Location');
+}
