1b81e7edc8d720892cc5b649e87d7b865d587262
Jochen Kupperschmidt <homework@nwsnet.de>
2014-08-31 18:21:22 +0200

    Limit allowed characters in screen name.

--- webapp/byceps/blueprints/user/forms.py
+++ webapp/byceps/blueprints/user/forms.py
@@ -7,10 +7,19 @@ byceps.blueprints.user.forms
 :Copyright: 2006-2014 Jochen Kupperschmidt
 """
 
+from itertools import chain
+from string import ascii_letters, digits
+
 from wtforms import BooleanField, FileField, PasswordField, StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 
 from ...util.l10n import LocalizedForm
+
+
+GERMAN_CHARS = 'äöüß'
+SPECIAL_CHARS = '!$%&()*,-./:<=>?[]_'
+VALID_SCREEN_NAME_CHARS = frozenset(chain(
+    ascii_letters, digits, GERMAN_CHARS, SPECIAL_CHARS))
 
 
 class CreateForm(LocalizedForm):
@@ -18,6 +27,12 @@ class CreateForm(LocalizedForm):
     email_address = StringField('E-Mail-Adresse', validators=[DataRequired()])
     password = PasswordField('Passwort', validators=[DataRequired()])
     consent_to_terms = BooleanField('AGB', validators=[DataRequired()])
+
+    def validate_screen_name(form, field):
+        if not all(map(VALID_SCREEN_NAME_CHARS.__contains__, field.data)):
+            raise ValidationError(
+                'Enthält ungültige Zeichen. Erlaubt sind Buchstaben, '
+                ' Ziffern und diese Sonderzeichen: {}'.format(SPECIAL_CHARS))
 
 
 class AvatarImageUpdateForm(LocalizedForm):
