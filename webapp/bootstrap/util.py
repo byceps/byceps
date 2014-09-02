4c7abecf430eb5ef3a7fc030fefc0fd04a257c4f
Jochen Kupperschmidt <homework@nwsnet.de>
2014-09-02 04:53:17 +0200

    Added utility method to obtain configuration name for bootstrapping from command line arguments.

--- webapp/bootstrap/util.py
+++ webapp/bootstrap/util.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from argparse import ArgumentParser
 from itertools import count, islice
 
 from byceps.database import db
+
+
+def get_config_name_from_args():
+    parser = ArgumentParser()
+    parser.add_argument('config_name', metavar='<config name>')
+    args = parser.parse_args()
+    return args.config_name
 
 
 def add_to_database(f):
