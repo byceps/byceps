dd2283c4d2fd9997112c6d9f701cb620bbabf0dc
Jochen Kupperschmidt <homework@nwsnet.de>
2014-09-03 21:59:50 +0200

    Propagate exceptions in production.

--- webapp/config/env/production.py
+++ webapp/config/env/production.py
@@ -5,6 +5,7 @@ from pathlib import Path
 DEBUG = False
 MAX_CONTENT_LENGTH = 4000000
 PERMANENT_SESSION_LIFETIME = timedelta(14)
+PROPAGATE_EXCEPTIONS = True
 SECRET_KEY = b'\xcb;\xcd\xdc\x11\xf2\xa8\x08_Ks\xa3\xb0\xc5K/\x9b\xf1\xd9\xdc_\x16\x8e\xa8'
 
 LOCALE = 'de_DE.UTF-8'
