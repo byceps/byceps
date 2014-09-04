7aaf2955f5ab310f640039568494527eb3dc13e5
Jochen Kupperschmidt <homework@nwsnet.de>
2014-09-04 18:24:15 +0200

    Added global template function to render a snippet and include the result in the calling template.

--- webapp/byceps/blueprints/snippet/views.py
+++ webapp/byceps/blueprints/snippet/views.py
@@ -10,10 +10,21 @@ byceps.blueprints.snippet.views
 from ...util.framework import create_blueprint
 
 from .models import Snippet
-from .templating import render_snippet
+from .templating import load_template, render_snippet
 
 
 blueprint = create_blueprint('snippet', __name__)
+
+
+@blueprint.app_template_global('render_snippet')
+def render_snippet_as_partial(name):
+    """Render the latest version of the snippet with the given name and
+    return the result.
+    """
+    snippet = Snippet.query.for_current_party.filter_by(name=name).one()
+    version = snippet.get_latest_version()
+    template = load_template(version)
+    return template.render()
 
 
 def view_latest_by_name(name):
