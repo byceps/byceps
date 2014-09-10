31ecde28a16eb4eb069fde91b021323ed3e652f0
Jochen Kupperschmidt <homework@nwsnet.de>
2014-09-10 13:43:20 +0200

    Now that the issue in bbcode is fixed with v1.0.19: Added tests for quote author names containing square brackets.

--- webapp/tests/blueprints/board/test_formatting.py
+++ webapp/tests/blueprints/board/test_formatting.py
@@ -2,6 +2,8 @@
 
 from unittest import TestCase
 
+from nose2.tools import params
+
 from byceps.blueprints.board.formatting import render_html
 
 
@@ -40,6 +42,35 @@ class RenderBbcodeTestCase(TestCase):
     def test_quote_with_author(self):
         text = '[quote author="CATS"]All your base are belong to us.[/quote]'
         expected = '<p class="quote-intro"><cite>CATS</cite> schrieb:</p>\n<blockquote>All your base are belong to us.</blockquote>'
+        self.assert_rendered_html(text, expected)
+
+    @params(
+        (
+            '[quote author="foo]bar"]blah[/quote]',
+            '<p class="quote-intro"><cite>foo]bar</cite> schrieb:</p>\n<blockquote>blah</blockquote>',
+        ),
+        (
+            '[quote author="foo[bar"]blah[/quote]',
+            '<p class="quote-intro"><cite>foo[bar</cite> schrieb:</p>\n<blockquote>blah</blockquote>',
+        ),
+        (
+            '[quote author="foo][bar"]blah[/quote]',
+            '<p class="quote-intro"><cite>foo][bar</cite> schrieb:</p>\n<blockquote>blah</blockquote>',
+        ),
+        (
+            '[quote author="foo[]bar"]blah[/quote]',
+            '<p class="quote-intro"><cite>foo[]bar</cite> schrieb:</p>\n<blockquote>blah</blockquote>',
+        ),
+        (
+            '[quote author="[foobar]"]blah[/quote]',
+            '<p class="quote-intro"><cite>[foobar]</cite> schrieb:</p>\n<blockquote>blah</blockquote>',
+        ),
+        (
+            '[quote author="]foobar["]blah[/quote]',
+            '<p class="quote-intro"><cite>]foobar[</cite> schrieb:</p>\n<blockquote>blah</blockquote>',
+        ),
+    )
+    def test_quote_with_author_whose_name_contains_square_brackets(self, text, expected):
         self.assert_rendered_html(text, expected)
 
     def assert_rendered_html(self, text, expected):
