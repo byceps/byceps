9e0178ca3ba318a4b77c5f61696a2317a425a952
Jochen Kupperschmidt <homework@nwsnet.de>
2014-09-11 20:09:49 +0200

    Retrieve current party ID from config object, not the application.

--- webapp/byceps/blueprints/party/views.py
+++ webapp/byceps/blueprints/party/views.py
@@ -7,8 +7,9 @@ byceps.blueprints.party.views
 :Copyright: 2006-2014 Jochen Kupperschmidt
 """
 
-from flask import current_app, g
+from flask import g
 
+from ...config import get_current_party_id
 from ...util.framework import create_blueprint
 from ...util.templating import templated
 
@@ -20,7 +21,8 @@ blueprint = create_blueprint('party', __name__)
 
 @blueprint.before_app_request
 def before_request():
-    g.party = Party.query.get(current_app.party_id)
+    party_id = get_current_party_id()
+    g.party = Party.query.get(party_id)
     if g.party is None:
         raise Exception('Unknown party "{}".'.format(party_id))
 
