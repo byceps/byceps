3a098d2659baa70549b6e5af291d9224259eaa17
Jochen Kupperschmidt <homework@nwsnet.de>
2014-09-20 20:50:55 +0200

    Changed method to retrieve the config environment from an environment variable instead of CLI arguments so the later can be parsed and used by a script.

--- webapp/bootstrap/util.py
+++ webapp/bootstrap/util.py
@@ -1,22 +1,30 @@
 # -*- coding: utf-8 -*-
 
-from argparse import ArgumentParser
 from contextlib import contextmanager
 from itertools import count, islice
+import os
 
 from byceps.application import create_app
 from byceps.database import db
 
 
-def get_config_name_from_args():
-    parser = ArgumentParser()
-    parser.add_argument('config_name', metavar='<config name>')
-    args = parser.parse_args()
-    return args.config_name
+def get_config_name_from_env():
+    """Return the config environment name set via environment variable.
+
+    Raise an exception if it isn't set.
+    """
+    env = os.environ.get('ENV')
+    if env is None:
+        raise Exception(
+            'No configuration was specified via the ENV environment variable.')
+    return env
 
 
 @contextmanager
 def app_context(config_name):
+    """Provide a context in which the application is available with the
+    specified environment.
+    """
     app = create_app(config_name, initialize=False)
     with app.app_context():
         yield
@@ -40,4 +48,5 @@ def add_all_to_database(f):
 
 
 def generate_positive_numbers(n):
+    """Generate a sequence of `n` positive, consecutive numbers."""
     return islice(count(1), n)
