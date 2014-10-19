478e327da6d63ffa4d19ffcbf0b705a271034ff3
Jochen Kupperschmidt <homework@nwsnet.de>
2014-10-19 19:17:47 +0200

    Added utility method to return elements from an iterable pair-wise.

--- webapp/byceps/util/iterables.py
+++ webapp/byceps/util/iterables.py
@@ -7,7 +7,7 @@ byceps.util.iterables
 :Copyright: 2006-2014 Jochen Kupperschmidt
 """
 
-from itertools import islice
+from itertools import islice, tee
 
 
 def find(iterable, predicate):
@@ -30,3 +30,18 @@ def index_of(iterable, predicate):
     for i, elem in enumerate(iterable):
         if predicate(elem):
             return i
+
+
+def pairwise(iterable):
+    """Return each element paired with its next one.
+
+    Example:
+        xs -> (x0, x1), (x1, x2), (x2, x3), ...
+
+    As seen in the Python standard library documentation of the
+    `itertools` module:
+    https://docs.python.org/3/library/itertools.html#itertools-recipes
+    """
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
--- webapp/tests/util/test_iterables.py
+++ webapp/tests/util/test_iterables.py
@@ -4,7 +4,7 @@ from unittest import TestCase
 
 from nose2.tools import params
 
-from byceps.util.iterables import find, index_of
+from byceps.util.iterables import find, index_of, pairwise
 
 
 class IterablesTestCase(TestCase):
@@ -55,3 +55,21 @@ class IterablesTestCase(TestCase):
     def test_index_of(self, iterable, predicate, expected):
         actual = index_of(iterable, predicate)
         self.assertEqual(actual, expected)
+
+    @params(
+        (
+            [],
+            [],
+        ),
+        (
+            ['a', 'b', 'c'],
+            [('a', 'b'), ('b', 'c')],
+        ),
+        (
+            range(5),
+            [(0, 1), (1, 2), (2, 3), (3, 4)],
+        ),
+    )
+    def test_pairwise(self, iterable, expected):
+        actual = pairwise(iterable)
+        self.assertEqual(list(actual), expected)
