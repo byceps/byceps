3cbd1fbac9edbc595f207f840cdb606bafccd5e6
Jochen Kupperschmidt <homework@nwsnet.de>
2014-11-12 22:43:58 +0100

    Added blueprint and model for user messaging.

---
+++ webapp/byceps/blueprints/messaging/models.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+"""
+byceps.blueprints.messaging.models
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Copyright: 2006-2014 Jochen Kupperschmidt
+"""
+
+from datetime import datetime
+
+from ...database import db, generate_uuid
+from ...util.instances import ReprBuilder
+
+from ..user.models import User
+
+
+class Message(db.Model):
+    """A message sent from one user to another."""
+    __tablename__ = 'messages'
+
+    id = db.Column(db.Uuid, default=generate_uuid, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    sender_id = db.Column(db.Uuid, db.ForeignKey('users.id'), index=True, nullable=False)
+    sender = db.relationship(User, foreign_keys=[sender_id])
+    recipient_id = db.Column(db.Uuid, db.ForeignKey('users.id'), index=True, nullable=False)
+    recipient = db.relationship(User, foreign_keys=[recipient_id])
+    subject = db.Column(db.Unicode(40), nullable=True)
+    body = db.Column(db.UnicodeText, nullable=False)
+    is_read = db.Column(db.Boolean, default=False, nullable=False)
+
+    def __init__(self, sender, recipient, body, *, subject=None):
+        self.sender = sender
+        self.recipient = recipient
+        self.subject = subject
+        self.body = body
+
+    def __repr__(self):
+        return ReprBuilder(self) \
+            .add_with_lookup('id') \
+            .add_with_lookup('created_at') \
+            .add('sender', self.sender.screen_name) \
+            .add('recipient', self.recipient.screen_name) \
+            .add_with_lookup('subject') \
+            .add_custom('read' if self.is_read else 'unread') \
+            .build()
