58db8bc908c36522bdb781e61ee5fb7dd79a9911
Jochen Kupperschmidt <homework@nwsnet.de>
2014-12-04 00:51:47 +0100

    Make sure every payment state is

--- webapp/byceps/blueprints/shop_admin/service.py
+++ webapp/byceps/blueprints/shop_admin/service.py
@@ -9,12 +9,16 @@ byceps.blueprints.shop_admin.service
 
 from collections import Counter
 
+from ..shop.models import PaymentState
+
 
 def count_ordered_articles(article):
     """Count how often the article has been ordered, grouped by the
     order's payment state.
     """
-    counter = Counter()
+    counter = Counter({state: 0 for state in PaymentState})
+
     for order_item in article.order_items:
         counter[order_item.order.payment_state] += order_item.quantity
+
     return dict(counter)
