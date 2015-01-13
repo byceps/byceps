1846b7f09ef12afb70c16f987c6f18caea39bf25
Jochen Kupperschmidt <homework@nwsnet.de>
2015-01-13 03:39:54 +0100

    Added context manager to set the current party in tests.

---
+++ webapp/tests/helpers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+tests.helpers
+~~~~~~~~~~~~~
+
+:Copyright: 2006-2015 Jochen Kupperschmidt
+"""
+
+from contextlib import contextmanager
+
+from flask import appcontext_pushed, g
+
+
+@contextmanager
+def current_party_set(app, party):
+    def handler(sender, **kwargs):
+        g.party = party
+
+    with appcontext_pushed.connected_to(handler, app):
+        yield
