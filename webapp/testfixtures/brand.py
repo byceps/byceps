dd8d5083be5c7e9fe7cac5539bca6f4a7f544759
Jochen Kupperschmidt <homework@nwsnet.de>
2015-02-11 02:50:26 +0100

    Extended test fixtures to support and pre-fill the new fields introduced for the order number.

--- webapp/testfixtures/brand.py
+++ webapp/testfixtures/brand.py
@@ -3,5 +3,6 @@
 from byceps.blueprints.brand.models import Brand
 
 
-def create_brand(*, id='acme', title='Acme Entertainment Convention'):
-    return Brand(id=id, title=title)
+def create_brand(*, id='acme', title='Acme Entertainment Convention',
+                 code='AEC'):
+    return Brand(id=id, title=title, code=code)
--- webapp/testfixtures/party.py
+++ webapp/testfixtures/party.py
@@ -7,13 +7,15 @@ from byceps.blueprints.party.models import Party
 from .brand import create_brand
 
 
-def create_party(*, id='acme-2014', brand=None, title='Acme Entertainment Convention 2014'):
+def create_party(*, id='acme-2014', brand=None, brand_party_serial=1,
+                 title='Acme Entertainment Convention 2014'):
     if brand is None:
         brand = create_brand()
 
     return Party(
         id=id,
         brand=brand,
+        brand_party_serial=brand_party_serial,
         title=title,
         starts_at=datetime(2014, 10, 24, 16, 0),
         ends_at=datetime(2014, 10, 26, 13, 0))
