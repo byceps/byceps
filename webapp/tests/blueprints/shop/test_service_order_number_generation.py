703a2cba0cace3309afbf038532a05b5731cc1a3
Jochen Kupperschmidt <homework@nwsnet.de>
2015-02-11 03:59:02 +0100

    Turned unit test into one that uses the application test case. Splitted original test case into one for the test defailts and another for custom brand, party and order serial.

--- webapp/tests/blueprints/shop/test_service_order_number_generation.py
+++ webapp/tests/blueprints/shop/test_service_order_number_generation.py
@@ -1,23 +1,38 @@
 # -*- coding: utf-8 -*-
 
-from unittest import TestCase
-
+from byceps.blueprints.shop.models import OrderNumberSequence
 from byceps.blueprints.shop.service import generate_order_number
 
+from testfixtures.brand import create_brand
 from testfixtures.party import create_party
 
+from tests import AbstractAppTestCase
 
-class OrderNumberGenerationTestCase(TestCase):
 
-    def test_generate_order_number(self):
-        brand_order_serial = 207
+class OrderNumberGenerationTestCase(AbstractAppTestCase):
 
-        party = create_party(brand_party_serial=3)
+    def test_generate_order_number_default(self):
+        self.set_order_number_sequence()
 
-        def generate_brand_order_serial(brand):
-            return brand_order_serial
+        actual = generate_order_number(self.party)
 
-        actual = generate_order_number(party,
-            brand_order_serial_generator=generate_brand_order_serial)
+        self.assertEqual(actual, 'AEC-01-B00001')
 
-        self.assertEqual(actual, 'AEC-03-B00207')
+    def test_generate_order_number_custom(self):
+        last_assigned_brand_order_serial = 206
+
+        brand = create_brand(code='LOL')
+        party = create_party(brand=brand, brand_party_serial=3)
+        self.set_order_number_sequence(value=last_assigned_brand_order_serial)
+
+        actual = generate_order_number(party)
+
+        self.assertEqual(actual, 'LOL-03-B00207')
+
+    def set_order_number_sequence(self, *, brand=None, value=0):
+        if not brand:
+            brand = self.brand
+
+        sequence = OrderNumberSequence(brand=brand, value=value)
+        self.db.session.add(sequence)
+        self.db.session.commit()
