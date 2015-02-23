8a43a0055e4d3557642822e8294f84ae36367f60
Jochen Kupperschmidt <homework@nwsnet.de>
2015-02-23 04:25:55 +0100

    Moved cart models upwards inside the module to reflect their position in the process (article, cart, order).

--- webapp/byceps/blueprints/shop/models.py
+++ webapp/byceps/blueprints/shop/models.py
@@ -134,6 +134,48 @@ def range_all(theType):
 
 
 # -------------------------------------------------------------------- #
+# shopping cart
+
+
+class Cart(object):
+    """A shopping cart."""
+
+    def __init__(self):
+        self._items = []
+
+    def add_item(self, item):
+        self._items.append(item)
+
+    def get_items(self):
+        return self._items
+
+    def is_empty(self):
+        return not self._items
+
+    def __repr__(self):
+        return ReprBuilder(self) \
+            .add_custom('{:d} items'.format(len(self._items))) \
+            .build()
+
+
+class CartItem(object):
+    """An article with a quantity."""
+
+    def __init__(self, article, quantity):
+        if quantity < 1:
+            raise ValueError('Quantity must be a positive number.')
+
+        self.article = article
+        self.quantity = quantity
+
+    def __repr__(self):
+        return ReprBuilder(self) \
+            .add_with_lookup('item_number') \
+            .add_with_lookup('quantity') \
+            .build()
+
+
+# -------------------------------------------------------------------- #
 # orders
 
 
@@ -305,46 +347,4 @@ class PartySequence(db.Model):
             .add('party', self.party_id) \
             .add('purpose', self.purpose.name) \
             .add_with_lookup('value') \
-            .build()
-
-
-# -------------------------------------------------------------------- #
-# shopping cart
-
-
-class Cart(object):
-    """A shopping cart."""
-
-    def __init__(self):
-        self._items = []
-
-    def add_item(self, item):
-        self._items.append(item)
-
-    def get_items(self):
-        return self._items
-
-    def is_empty(self):
-        return not self._items
-
-    def __repr__(self):
-        return ReprBuilder(self) \
-            .add_custom('{:d} items'.format(len(self._items))) \
-            .build()
-
-
-class CartItem(object):
-    """An article with a quantity."""
-
-    def __init__(self, article, quantity):
-        if quantity < 1:
-            raise ValueError('Quantity must be a positive number.')
-
-        self.article = article
-        self.quantity = quantity
-
-    def __repr__(self):
-        return ReprBuilder(self) \
-            .add_with_lookup('item_number') \
-            .add_with_lookup('quantity') \
             .build()
