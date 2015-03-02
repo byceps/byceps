a6387b33c5177c6eac46920d1ff41e38f08fcfec
Jochen Kupperschmidt <homework@nwsnet.de>
2015-03-02 17:57:06 +0100

    Extracted user session class into separate module.

---
+++ webapp/byceps/blueprints/user/session.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+"""
+byceps.blueprints.user.session
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Copyright: 2006-2015 Jochen Kupperschmidt
+"""
+
+from flask import session
+
+from .models import User
+
+
+class UserSession(object):
+
+    KEY_USER_ID = 'user_id'
+    KEY_USER_AUTH_TOKEN = 'user_auth_token'
+
+    @classmethod
+    def start(cls, user, *, permanent=False):
+        """Initialize the user's session by putting the relevant data
+        into the session cookie.
+        """
+        session[cls.KEY_USER_ID] = str(user.id)
+        session[cls.KEY_USER_AUTH_TOKEN] = str(user.auth_token)
+        session.permanent = permanent
+
+    @classmethod
+    def end(cls):
+        """End the user's session by deleting the session cookie."""
+        session.pop(cls.KEY_USER_ID, None)
+        session.pop(cls.KEY_USER_AUTH_TOKEN, None)
+        session.permanent = False
+
+    @classmethod
+    def get_user(cls):
+        """Return the current user, falling back to the anonymous user."""
+        return User.load(cls.get_user_id(), cls.get_auth_token())
+
+    @classmethod
+    def get_user_id(cls):
+        """Return the current user's ID, or `None` if not available."""
+        return session.get(cls.KEY_USER_ID)
+
+    @classmethod
+    def get_auth_token(cls):
+        """Return the current user's auth token, or `None` if not available."""
+        return session.get(cls.KEY_USER_AUTH_TOKEN)
--- webapp/byceps/blueprints/user/views.py
+++ webapp/byceps/blueprints/user/views.py
@@ -34,6 +34,7 @@ from .forms import AvatarImageUpdateForm, DetailsForm, LoginForm, \
     RequestConfirmationEmailForm, RequestPasswordResetForm, \
     ResetPasswordForm, UpdatePasswordForm, UserCreateForm
 from .models import User
+from .session import UserSession
 
 
 MAXIMUM_AVATAR_IMAGE_DIMENSIONS = Dimensions(110, 150)
@@ -562,40 +563,3 @@ def get_current_user_or_404():
         abort(404)
 
     return user
-
-
-class UserSession(object):
-
-    KEY_USER_ID = 'user_id'
-    KEY_USER_AUTH_TOKEN = 'user_auth_token'
-
-    @classmethod
-    def start(cls, user, *, permanent=False):
-        """Initialize the user's session by putting the relevant data
-        into the session cookie.
-        """
-        session[cls.KEY_USER_ID] = str(user.id)
-        session[cls.KEY_USER_AUTH_TOKEN] = str(user.auth_token)
-        session.permanent = permanent
-
-    @classmethod
-    def end(cls):
-        """End the user's session by deleting the session cookie."""
-        session.pop(cls.KEY_USER_ID, None)
-        session.pop(cls.KEY_USER_AUTH_TOKEN, None)
-        session.permanent = False
-
-    @classmethod
-    def get_user(cls):
-        """Return the current user, falling back to the anonymous user."""
-        return User.load(cls.get_user_id(), cls.get_auth_token())
-
-    @classmethod
-    def get_user_id(cls):
-        """Return the current user's ID, or `None` if not available."""
-        return session.get(cls.KEY_USER_ID)
-
-    @classmethod
-    def get_auth_token(cls):
-        """Return the current user's auth token, or `None` if not available."""
-        return session.get(cls.KEY_USER_AUTH_TOKEN)
