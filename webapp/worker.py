190bb7ef06bffacbbadc6cb1d1317e432aab2b1d
Jochen Kupperschmidt <homework@nwsnet.de>
2015-03-27 03:26:18 +0100

    Included rq as a job queue and created a custom worker.

---
+++ webapp/worker.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Run a worker for the job queue."""
+
+import os
+import sys
+
+from redis import StrictRedis
+from rq import Connection, Queue, Worker
+
+from byceps.application import create_app
+
+
+environment = os.environ.get('ENVIRONMENT')
+if environment is None:
+    sys.stderr.write("Environment variable 'ENVIRONMENT' must be set but isn't.")
+    sys.exit()
+
+app = create_app(environment, initialize=False)
+
+with app.app_context():
+    redis = StrictRedis(app.config['REDIS_URL'])
+    with Connection(redis):
+        queues = [Queue()]
+        worker = Worker(queues)
+        worker.work()
