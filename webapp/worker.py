dbb5e8b5c5b7e1945e1dffd2501f7e3158380a0b
Jochen Kupperschmidt <homework@nwsnet.de>
2015-04-21 02:23:24 +0200

    Extracted method.

--- webapp/worker.py
+++ webapp/worker.py
@@ -11,12 +11,16 @@ from rq import Connection, Queue, Worker
 from bootstrap.util import app_context, get_config_name_from_env
 
 
-if __name__ == '__main__':
+def get_config_name():
     try:
-        config_name = get_config_name_from_env()
+        return get_config_name_from_env()
     except Exception as e:
         sys.stderr.write(str(e) + '\n')
         sys.exit()
+
+
+if __name__ == '__main__':
+    config_name = get_config_name()
 
     with app_context(config_name) as app:
         redis = StrictRedis(app.config['REDIS_URL'])
