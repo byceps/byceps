cc11798d857a48ba609f2e5125c30475bfcd8c45
Jochen Kupperschmidt <homework@nwsnet.de>
2015-11-13 04:20:14 +0100

    Introduced an id column to attached articles.

--- webapp/byceps/blueprints/shop/models/article.py
+++ webapp/byceps/blueprints/shop/models/article.py
@@ -133,12 +133,20 @@ def range_all(theType):
 class AttachedArticle(db.Model):
     """An article that is attached to another article."""
     __tablename__ = 'shop_attached_articles'
+    __table_args__ = (
+        db.UniqueConstraint('article_number', 'attached_to_article_number'),
+    )
 
-    article_number = db.Column(db.Unicode(20), db.ForeignKey('shop_articles.item_number'), primary_key=True)
+    id = db.Column(db.Uuid, default=generate_uuid, primary_key=True)
+    article_number = db.Column(db.Unicode(20),
+                               db.ForeignKey('shop_articles.item_number'),
+                               nullable=False, index=True)
     article = db.relationship(Article, foreign_keys=[article_number],
                               backref=db.backref('articles_attached_to', collection_class=set))
     quantity = db.Column(db.Integer, nullable=False)
-    attached_to_article_number = db.Column(db.Unicode(20), db.ForeignKey('shop_articles.item_number'), primary_key=True)
+    attached_to_article_number = db.Column(db.Unicode(20),
+                                           db.ForeignKey('shop_articles.item_number'),
+                                           nullable=False, index=True)
     attached_to_article = db.relationship(Article, foreign_keys=[attached_to_article_number],
                                           backref=db.backref('attached_articles', collection_class=set))
 
