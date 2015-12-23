ddf554e9e5cbc1a422d0be27a924d73a1cee9a57
Jochen Kupperschmidt <homework@nwsnet.de>
2015-12-23 16:26:52 +0100

    Simplified code by using a guard in the inner 'unseen' posting predicate instead of overriding the predicate later. The performance loss should be negligible. :)

--- webapp/byceps/blueprints/board/views.py
+++ webapp/byceps/blueprints/board/views.py
@@ -165,17 +165,16 @@ def topic_view(id, page):
 def add_unseen_flag_to_postings(postings, user, last_viewed_at):
     """Add the attribute 'unseen' to each posting."""
     def unseen(posting):
+        # Don't display any posting as new to a guest.
+        if user.is_anonymous:
+            return False
+
         # Don't display the author's own posting as new to him/her.
         if posting.creator == user:
             return False
 
         return last_viewed_at is None \
             or posting.created_at > last_viewed_at
-
-    # Don't display any posting as new to a guest.
-    if user.is_anonymous:
-        def unseen(posting):
-            return False
 
     for posting in postings:
         posting.unseen = unseen(posting)
