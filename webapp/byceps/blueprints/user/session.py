187c7ef8848aec70e710f7b04fb58d5489496a51
Jochen Kupperschmidt <homework@nwsnet.de>
2016-01-06 03:37:57 +0100

    Moved key constants out of the class to the module level.

--- webapp/byceps/blueprints/user/session.py
+++ webapp/byceps/blueprints/user/session.py
@@ -13,25 +13,26 @@ from flask import session
 from .models import User
 
 
-class UserSession(object):
+KEY_USER_ID = 'user_id'
+KEY_USER_AUTH_TOKEN = 'user_auth_token'
 
-    KEY_USER_ID = 'user_id'
-    KEY_USER_AUTH_TOKEN = 'user_auth_token'
+
+class UserSession(object):
 
     @classmethod
     def start(cls, user, *, permanent=False):
         """Initialize the user's session by putting the relevant data
         into the session cookie.
         """
-        session[cls.KEY_USER_ID] = str(user.id)
-        session[cls.KEY_USER_AUTH_TOKEN] = str(user.auth_token)
+        session[KEY_USER_ID] = str(user.id)
+        session[KEY_USER_AUTH_TOKEN] = str(user.auth_token)
         session.permanent = permanent
 
     @classmethod
     def end(cls):
         """End the user's session by deleting the session cookie."""
-        session.pop(cls.KEY_USER_ID, None)
-        session.pop(cls.KEY_USER_AUTH_TOKEN, None)
+        session.pop(KEY_USER_ID, None)
+        session.pop(KEY_USER_AUTH_TOKEN, None)
         session.permanent = False
 
     @classmethod
@@ -42,9 +43,9 @@ class UserSession(object):
     @classmethod
     def get_user_id(cls):
         """Return the current user's ID, or `None` if not available."""
-        return session.get(cls.KEY_USER_ID)
+        return session.get(KEY_USER_ID)
 
     @classmethod
     def get_auth_token(cls):
         """Return the current user's auth token, or `None` if not available."""
-        return session.get(cls.KEY_USER_AUTH_TOKEN)
+        return session.get(KEY_USER_AUTH_TOKEN)
