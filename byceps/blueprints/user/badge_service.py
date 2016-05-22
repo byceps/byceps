8271b6e61d511d9ce8395a64caa5db9284018abf
Jochen Kupperschmidt <homework@nwsnet.de>
2016-05-22 17:34:01 +0200

    Added service function to create a new user badge.

--- byceps/blueprints/user/badge_service.py
+++ byceps/blueprints/user/badge_service.py
@@ -10,6 +10,18 @@ byceps.blueprints.user.service.badge_service
 
 from ...database import db
 
+from .models.badge import Badge
+
+
+def create_badge(label, image_filename, *, description=None):
+    """Introduce a new badge."""
+    badge = Badge(label, image_filename, description=description)
+
+    db.session.add(badge)
+    db.session.commit()
+
+    return badge
+
 
 def award_badge_to_user(badge, user):
     """Award the badge to the user."""
