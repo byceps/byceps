12860a99ae62e579e1745962f431497ad42369f9
Jochen Kupperschmidt <homework@nwsnet.de>
2016-06-19 16:53:36 +0200

    Refactored tests and replaced test class with functions.

--- tests/blueprints/authorization/test_registry.py
+++ tests/blueprints/authorization/test_registry.py
@@ -5,37 +5,42 @@
 :License: Modified BSD, see LICENSE for details.
 """
 
-from unittest import TestCase
-
 from byceps.blueprints.authorization.models import Permission
 from byceps.blueprints.authorization.registry import PermissionRegistry
 from byceps.util.authorization import create_permission_enum
 
 
-class PermissionRegistryTestCase(TestCase):
-
-    def setUp(self):
-        self.registry = PermissionRegistry()
-        self.registry.register_enum(ItemPermission)
-
-    def test_lookup_of_existing_enum_member(self):
-        permission = create_permission('item.create')
-        self.assert_get_enum_member_equals(permission, ItemPermission.create)
-
-    def test_lookup_of_nonexistent_member_of_existing_enum(self):
-        permission = create_permission('item.delete')
-        self.assert_get_enum_member_equals(permission, None)
-
-    def test_lookup_of_member_of_nonexistent_enum(self):
-        permission = create_permission('article.create')
-        self.assert_get_enum_member_equals(permission, None)
-
-    def assert_get_enum_member_equals(self, permission, expected):
-        actual = self.registry.get_enum_member(permission)
-        self.assertEqual(actual, expected)
-
-
 ItemPermission = create_permission_enum('item', ['view', 'create', 'update'])
+
+
+def test_lookup_of_existing_enum_member():
+    registry = create_registry_with_registered_enum()
+
+    permission = create_permission('item.create')
+
+    assert registry.get_enum_member(permission) == ItemPermission.create
+
+
+def test_lookup_of_nonexistent_member_of_existing_enum():
+    registry = create_registry_with_registered_enum()
+
+    permission = create_permission('item.delete')
+
+    assert registry.get_enum_member(permission) == None
+
+
+def test_lookup_of_member_of_nonexistent_enum():
+    registry = create_registry_with_registered_enum()
+
+    permission = create_permission('article.create')
+
+    assert registry.get_enum_member(permission) == None
+
+
+def create_registry_with_registered_enum():
+    registry = PermissionRegistry()
+    registry.register_enum(ItemPermission)
+    return registry
 
 
 def create_permission(id):
