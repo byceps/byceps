759fba843fe3714bf45df0e25a8e0a482d94111c
Jochen Kupperschmidt <homework@nwsnet.de>
2016-06-19 17:01:18 +0200

    Refactored tests and replaced test class with functions.

--- tests/blueprints/user/test_models_avatar_image.py
+++ tests/blueprints/user/test_models_avatar_image.py
@@ -6,7 +6,6 @@
 """
 
 from datetime import datetime
-from unittest import TestCase
 
 from nose2.tools import params
 
@@ -18,41 +17,44 @@ from testfixtures.user import create_user
 CREATED_AT = datetime(2014, 7, 29, 14, 43, 30, 196165)
 
 
-class AvatarImageTestCase(TestCase):
+@params(
+    (None      , None          , False),
+    (CREATED_AT, None          , False),
+    (None      , ImageType.jpeg, False),
+    (CREATED_AT, ImageType.jpeg, True ),
+)
+def test_has_avatar_image(created_at, image_type, expected):
+    user = create_user(1)
 
-    def setUp(self):
-        self.user = create_user(1)
+    user.avatar_image_created_at = created_at
+    user.avatar_image_type = image_type
 
-    @params(
-        (None      , None          , False),
-        (CREATED_AT, None          , False),
-        (None      , ImageType.jpeg, False),
-        (CREATED_AT, ImageType.jpeg, True ),
-    )
-    def test_has_avatar_image(self, created_at, image_type, expected):
-        self.user.avatar_image_created_at = created_at
-        self.user.avatar_image_type = image_type
+    assert user.has_avatar_image == expected
 
-        self.assertEqual(self.user.has_avatar_image, expected)
 
-    @params(
-        (None  , None          ),
-        ('gif' , ImageType.gif ),
-        ('jpeg', ImageType.jpeg),
-        ('png' , ImageType.png ),
-    )
-    def test_hybrid_image_type_getter(self, database_value, expected):
-        self.user._avatar_image_type = database_value
+@params(
+    (None  , None          ),
+    ('gif' , ImageType.gif ),
+    ('jpeg', ImageType.jpeg),
+    ('png' , ImageType.png ),
+)
+def test_hybrid_image_type_getter(database_value, expected):
+    user = create_user(1)
 
-        self.assertEqual(self.user.avatar_image_type, expected)
+    user._avatar_image_type = database_value
 
-    @params(
-        (None          , None  ),
-        (ImageType.gif , 'gif' ),
-        (ImageType.jpeg, 'jpeg'),
-        (ImageType.png , 'png' ),
-    )
-    def test_hybrid_image_type_setter(self, image_type, expected):
-        self.user.avatar_image_type = image_type
+    assert user.avatar_image_type == expected
 
-        self.assertEqual(self.user._avatar_image_type, expected)
+
+@params(
+    (None          , None  ),
+    (ImageType.gif , 'gif' ),
+    (ImageType.jpeg, 'jpeg'),
+    (ImageType.png , 'png' ),
+)
+def test_hybrid_image_type_setter(image_type, expected):
+    user = create_user(1)
+
+    user.avatar_image_type = image_type
+
+    assert user._avatar_image_type == expected
