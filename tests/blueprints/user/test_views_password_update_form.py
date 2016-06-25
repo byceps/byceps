546e54cc13cbac9bd218163c7e2df5775df89d49
Jochen Kupperschmidt <homework@nwsnet.de>
2016-06-25 22:42:35 +0200

    Added test to ensure that the password update form is only available to users that are logged in, not anonymous ones.

---
+++ tests/blueprints/user/test_views_password_update_form.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""
+:Copyright: 2006-2016 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+from tests.base import AbstractAppTestCase
+
+from testfixtures.user import create_user
+
+
+class PasswordUpdateFormTestCase(AbstractAppTestCase):
+
+    def test_when_logged_in_form_is_available(self):
+        user = self.create_user()
+
+        response = self.send_request(user=user)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_when_not_logged_in_form_is_unavailable(self):
+        response = self.send_request()
+
+        self.assertEqual(response.status_code, 404)
+
+    # helpers
+
+    def create_user(self):
+        user = create_user(8516)
+
+        self.db.session.add(user)
+        self.db.session.commit()
+
+        return user
+
+    def send_request(self, *, user=None):
+        url = '/users/me/password/update'
+        with self.client(user=user) as client:
+            return client.get(url)
