52e8f7636210fb288c161bcc387245827734010e
Jochen Kupperschmidt <homework@nwsnet.de>
2016-06-25 16:39:55 +0200

    Parametrized test and added a second case.

--- tests/blueprints/user_avatar/test_models_image_path.py
+++ tests/blueprints/user_avatar/test_models_image_path.py
@@ -8,6 +8,8 @@
 from pathlib import Path
 from uuid import UUID
 
+from nose2.tools import params
+
 from byceps.util.image import ImageType
 
 from tests.base import AbstractAppTestCase
@@ -23,14 +25,23 @@ class AvatarImagePathTestCase(AbstractAppTestCase):
 
         self.user = create_user(1)
 
-    def test_path(self):
-        avatar_id = UUID('2e17cb15-d763-4f93-882a-371296a3c63f')
-
-        expected = Path(
-            '/var/data/avatars/2e17cb15-d763-4f93-882a-371296a3c63f.jpeg')
-
-        avatar = create_avatar(self.user, id=avatar_id, image_type=ImageType.jpeg)
+    @params(
+        (
+            Path('/var/data/avatars'),
+            UUID('2e17cb15-d763-4f93-882a-371296a3c63f'),
+            ImageType.jpeg,
+            Path('/var/data/avatars/2e17cb15-d763-4f93-882a-371296a3c63f.jpeg'),
+        ),
+        (
+            Path('/home/byceps/data/global/users/avatars'),
+            UUID('f0266761-c37e-4519-8cb8-5812d2bfe595'),
+            ImageType.png,
+            Path('/home/byceps/data/global/users/avatars/f0266761-c37e-4519-8cb8-5812d2bfe595.png'),
+        ),
+    )
+    def test_path(self, avatar_images_path, avatar_id, image_type, expected):
+        avatar = create_avatar(self.user, id=avatar_id, image_type=image_type)
 
         with self.app.app_context():
-            self.app.config['PATH_USER_AVATAR_IMAGES'] = Path('/var/data/avatars')
+            self.app.config['PATH_USER_AVATAR_IMAGES'] = avatar_images_path
             self.assertEqual(avatar.path, expected)
