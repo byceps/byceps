7415d59701b424bca7f32273cf2d137084244900
Jochen Kupperschmidt <homework@nwsnet.de>
2016-06-25 17:05:38 +0200

    Turned class-based test into a function.

--- tests/blueprints/user_avatar/test_models_image_path.py
+++ tests/blueprints/user_avatar/test_models_image_path.py
@@ -10,38 +10,33 @@ from uuid import UUID
 
 from nose2.tools import params
 
+from byceps.application import create_app
 from byceps.util.image import ImageType
-
-from tests.base import AbstractAppTestCase
 
 from testfixtures.user import create_user
 from testfixtures.user_avatar import create_avatar
 
 
-class AvatarImagePathTestCase(AbstractAppTestCase):
+@params(
+    (
+        Path('/var/data/avatars'),
+        UUID('2e17cb15-d763-4f93-882a-371296a3c63f'),
+        ImageType.jpeg,
+        Path('/var/data/avatars/2e17cb15-d763-4f93-882a-371296a3c63f.jpeg'),
+    ),
+    (
+        Path('/home/byceps/data/global/users/avatars'),
+        UUID('f0266761-c37e-4519-8cb8-5812d2bfe595'),
+        ImageType.png,
+        Path('/home/byceps/data/global/users/avatars/f0266761-c37e-4519-8cb8-5812d2bfe595.png'),
+    ),
+)
+def test_path(avatar_images_path, avatar_id, image_type, expected):
+    user = create_user(1)
 
-    def setUp(self):
-        super().setUp()
+    avatar = create_avatar(user, id=avatar_id, image_type=image_type)
 
-        self.user = create_user(1)
-
-    @params(
-        (
-            Path('/var/data/avatars'),
-            UUID('2e17cb15-d763-4f93-882a-371296a3c63f'),
-            ImageType.jpeg,
-            Path('/var/data/avatars/2e17cb15-d763-4f93-882a-371296a3c63f.jpeg'),
-        ),
-        (
-            Path('/home/byceps/data/global/users/avatars'),
-            UUID('f0266761-c37e-4519-8cb8-5812d2bfe595'),
-            ImageType.png,
-            Path('/home/byceps/data/global/users/avatars/f0266761-c37e-4519-8cb8-5812d2bfe595.png'),
-        ),
-    )
-    def test_path(self, avatar_images_path, avatar_id, image_type, expected):
-        avatar = create_avatar(self.user, id=avatar_id, image_type=image_type)
-
-        with self.app.app_context():
-            self.app.config['PATH_USER_AVATAR_IMAGES'] = avatar_images_path
-            self.assertEqual(avatar.path, expected)
+    app = create_app('test')
+    with app.app_context():
+        app.config['PATH_USER_AVATAR_IMAGES'] = avatar_images_path
+        assert avatar.path == expected
