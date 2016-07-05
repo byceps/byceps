dab08cc20be79c83c97b1e8871a01f5ce05d8e63
Jochen Kupperschmidt <homework@nwsnet.de>
2016-07-05 02:53:48 +0200

    Added missing authorization test fixtures module.

---
+++ testfixtures/authorization.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+testfixtures.authorization
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Copyright: 2006-2016 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+from byceps.blueprints.authorization.models import Permission, Role
+
+
+def create_permission(id, title=None):
+    return Permission(id, title)
+
+
+def create_permission_from_enum_member(permission_enum_member):
+    return Permission.from_enum_member(permission_enum_member)
+
+
+def create_role(id, title=None):
+    return Role(id, title)
