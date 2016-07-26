e82702cc1bb6e93f3d86aee503ddc583a2dbf6f0
Jochen Kupperschmidt <homework@nwsnet.de>
2016-07-26 02:32:36 +0200

    Determine image type without using the `imghdr` module.

--- byceps/util/image.py
+++ byceps/util/image.py
@@ -10,7 +10,6 @@ byceps.util.image
 
 from collections import namedtuple
 from enum import Enum
-import imghdr
 from io import BytesIO
 
 from PIL import Image
@@ -22,14 +21,31 @@ Dimensions = namedtuple('Dimensions', ['width', 'height'])
 ImageType = Enum('ImageType', ['gif', 'jpeg', 'png'])
 
 
+# See: https://www.w3.org/Graphics/GIF/spec-gif89a.txt
+GIF_SIGNATURE = b'GIF'
+GIF_VERSIONS = frozenset([b'87a', b'89a'])
+
+# See: https://en.wikipedia.org/wiki/JPEG#Syntax_and_structure
+JPEG_MARKER_SOI = b'\xff\xd8'
+
+# See: https://tools.ietf.org/html/rfc2083#page-11
+PNG_SIGNATURE = b'\x89PNG\r\n\x1a\n'
+
+
+
 def guess_type(data):
     """Return the guessed type, or `None` if the type could not be
     guessed or is not allowed (i.e. not a member of the enum).
     """
-    type_str = imghdr.what(data)
-    try:
-        return ImageType[type_str]
-    except KeyError:
+    header = data.read(8)
+
+    if (header[:3] == GIF_SIGNATURE) and (header[3:6] in GIF_VERSIONS):
+        return ImageType.gif
+    elif header[:8] == PNG_SIGNATURE:
+        return ImageType.png
+    elif header[:2] == JPEG_MARKER_SOI:
+        return ImageType.jpeg
+    else:
         return None
 
 
