d056d6a14e048d49c80f38fa43c9ea356b7595a4
Jochen Kupperschmidt <homework@nwsnet.de>
2016-10-08 02:01:55 +0200

    Allow overriding the default set of allowed image types, and dynamically assemble the related error message

--- byceps/blueprints/user_avatar/service.py
+++ byceps/blueprints/user_avatar/service.py
@@ -28,9 +28,9 @@ def get_image_type_names(types):
     return frozenset(t.name.upper() for t in types)
 
 
-def update_avatar_image(user, stream):
+def update_avatar_image(user, stream, *, allowed_types=ALL_IMAGE_TYPES):
     """Set a new avatar image for the user."""
-    image_type = _determine_image_type(stream)
+    image_type = _determine_image_type(stream, allowed_types)
 
     if _is_image_too_large(stream):
         stream = create_thumbnail(stream, image_type.name, MAXIMUM_DIMENSIONS)
@@ -46,10 +46,15 @@ def update_avatar_image(user, stream):
     db.session.commit()
 
 
-def _determine_image_type(stream):
+def _determine_image_type(stream, allowed_types):
     image_type = guess_type(stream)
-    if image_type is None:
-        abort(400, 'Only GIF, JPEG and PNG images are allowed.')
+
+    if image_type not in allowed_types:
+        allowed_type_names = get_image_type_names(allowed_types)
+        allowed_type_names_string = ', '.join(sorted(allowed_type_names))
+
+        abort(400, 'Only images of these types are allowed: {}'
+            .format(allowed_type_names_string))
 
     stream.seek(0)
     return image_type
