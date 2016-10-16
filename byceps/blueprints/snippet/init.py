c2573e2fa81b1c82fdae466f3e370729a7607cb2
Jochen Kupperschmidt <homework@nwsnet.de>
2016-10-17 01:04:31 +0200

    Retrieve mountpoints via existing service function instead of directly using the model to execute a query

--- byceps/blueprints/snippet/init.py
+++ byceps/blueprints/snippet/init.py
@@ -10,14 +10,15 @@ byceps.blueprints.snippet.init
 
 from flask import current_app
 
-from ...services.snippet.models.mountpoint import Mountpoint
+from ...services.snippet import service as snippet_service
 
 from .views import blueprint, view_latest_by_name
 
 
 def add_routes_for_snippets(party_id):
     """Register routes for snippets with the application."""
-    mountpoints = Mountpoint.query.for_party_with_id(party_id).all()
+    mountpoints = snippet_service.get_mountpoints_for_party(party_id)
+
     for mountpoint in mountpoints:
         add_route_for_snippet(mountpoint)
 
