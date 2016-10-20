6335884d191050e400b5db9bff656e38eedd3f6c
Jochen Kupperschmidt <homework@nwsnet.de>
2016-10-20 02:49:05 +0200

    Move functionality from view module to service functions

--- byceps/blueprints/orga_presence/views.py
+++ byceps/blueprints/orga_presence/views.py
@@ -8,14 +8,10 @@ byceps.blueprints.orga_presence.views
 :License: Modified BSD, see LICENSE for details.
 """
 
-from itertools import groupby
-
-from arrow import Arrow
 from flask import abort
 
 from ...services.orga_presence import service as orga_presence_service
 from ...services.party import service as party_service
-from ...util.datetime.range import create_adjacent_ranges
 from ...util.framework import create_blueprint
 from ...util.templating import templated
 
@@ -37,17 +33,17 @@ permission_registry.register_enum(OrgaPresencePermission)
 def view(party_id):
     """List orga presence and task time slots for that party."""
     party = party_service.find_party(party_id)
+
     if party is None:
         abort(404)
 
     presences = orga_presence_service.get_presences(party.id)
     tasks = orga_presence_service.get_tasks(party.id)
 
-    time_slot_ranges = list(_get_time_slot_ranges(party, tasks))
+    hour_ranges = list(orga_presence_service.get_hour_ranges(party, tasks))
 
-    hour_ranges = list(_get_hour_ranges(time_slot_ranges))
-
-    days_and_hour_totals = _get_days_and_hour_totals(hour_ranges)
+    days_and_hour_totals = list(orga_presence_service \
+        .get_days_and_hour_totals(hour_ranges))
 
     return {
         'party': party,
@@ -56,45 +52,3 @@ def view(party_id):
         'presences': presences,
         'tasks': tasks,
     }
-
-
-def _get_time_slot_ranges(party, tasks):
-    time_slots = [party] + tasks
-    for time_slot in time_slots:
-        yield time_slot.range
-
-
-def _get_hour_ranges(time_slot_ranges):
-    hour_starts = _get_hour_starts(time_slot_ranges)
-    return create_adjacent_ranges(hour_starts)
-
-
-def _get_hour_starts(dt_ranges):
-    min_starts_at = _find_earliest_start(dt_ranges)
-    max_ends_at = _find_latest_end(dt_ranges)
-
-    hour_starts_arrow = Arrow.range('hour', min_starts_at, max_ends_at)
-
-    return _to_datetimes_without_tzinfo(hour_starts_arrow)
-
-
-def _find_earliest_start(dt_ranges):
-    return min(dt_range.start for dt_range in dt_ranges)
-
-
-def _find_latest_end(dt_ranges):
-    return max(dt_range.end for dt_range in dt_ranges)
-
-
-def _to_datetimes_without_tzinfo(arrow_datetimes):
-    for arrow_datetime in arrow_datetimes:
-        yield arrow_datetime.datetime.replace(tzinfo=None)
-
-
-def _get_days_and_hour_totals(hour_ranges):
-    def get_date(dt_range):
-        return dt_range.start.date()
-
-    for day, hour_ranges_for_day in groupby(hour_ranges, key=get_date):
-        hour_total = len(list(hour_ranges_for_day))
-        yield day, hour_total
--- byceps/services/orga_presence/service.py
+++ byceps/services/orga_presence/service.py
@@ -8,7 +8,12 @@ byceps.services.orga_presence.service
 :License: Modified BSD, see LICENSE for details.
 """
 
+from itertools import groupby
+
+from arrow import Arrow
+
 from ...database import db
+from ...util.datetime.range import create_adjacent_ranges
 
 from .models import Presence, Task
 
@@ -26,3 +31,51 @@ def get_tasks(party_id):
     return Task.query \
         .for_party_id(party_id) \
         .all()
+
+
+# -------------------------------------------------------------------- #
+
+
+def get_hour_ranges(party, tasks):
+    """Yield our ranges based on the party and tasks."""
+    time_slot_ranges = list(_get_time_slot_ranges(party, tasks))
+    hour_starts = _get_hour_starts(time_slot_ranges)
+    return create_adjacent_ranges(hour_starts)
+
+
+def _get_time_slot_ranges(party, tasks):
+    time_slots = [party] + tasks
+    for time_slot in time_slots:
+        yield time_slot.range
+
+
+def _get_hour_starts(dt_ranges):
+    min_starts_at = _find_earliest_start(dt_ranges)
+    max_ends_at = _find_latest_end(dt_ranges)
+
+    hour_starts_arrow = Arrow.range('hour', min_starts_at, max_ends_at)
+
+    return _to_datetimes_without_tzinfo(hour_starts_arrow)
+
+
+def _find_earliest_start(dt_ranges):
+    return min(dt_range.start for dt_range in dt_ranges)
+
+
+def _find_latest_end(dt_ranges):
+    return max(dt_range.end for dt_range in dt_ranges)
+
+
+def _to_datetimes_without_tzinfo(arrow_datetimes):
+    for arrow_datetime in arrow_datetimes:
+        yield arrow_datetime.datetime.replace(tzinfo=None)
+
+
+def get_days_and_hour_totals(hour_ranges):
+    """Yield (day, relevant hours total) pairs."""
+    def get_date(dt_range):
+        return dt_range.start.date()
+
+    for day, hour_ranges_for_day in groupby(hour_ranges, key=get_date):
+        hour_total = len(list(hour_ranges_for_day))
+        yield day, hour_total
