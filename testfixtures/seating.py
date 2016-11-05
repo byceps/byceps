2758c1086e06a77f9676d678a3d41a53a352ec01
Jochen Kupperschmidt <homework@nwsnet.de>
2016-11-05 17:43:23 +0100

    Add function to create a seat category test fixture

--- testfixtures/seating.py
+++ testfixtures/seating.py
@@ -8,7 +8,12 @@ testfixtures.seating
 :License: Modified BSD, see LICENSE for details.
 """
 
+from byceps.services.seating.models.category import Category
 from byceps.services.seating.models.seat_group import SeatGroup
+
+
+def create_seat_category(party_id, title):
+    return SeatCategory(party_id, title)
 
 
 def create_seat_group(party_id, seat_category, title, *, seat_quantity=4):
