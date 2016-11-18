903fa928cdee338ae1835b56fcdd3e521ee7231a
Jochen Kupperschmidt <homework@nwsnet.de>
2016-11-18 12:30:23 +0100

    Don't fall back to a default configuration in the application factory

--- byceps/application_factory.py
+++ byceps/application_factory.py
@@ -15,7 +15,7 @@ from .application import create_app, init_app
 from .util.system import get_config_env_name_from_env
 
 
-environment = get_config_env_name_from_env(default='development')
+environment = get_config_env_name_from_env()
 
 app = create_app(environment)
 init_app(app)
