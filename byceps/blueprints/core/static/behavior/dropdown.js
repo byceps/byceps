616ea2bbdceadac59b4ce873acdcd2ac8e4d8108
Jochen Kupperschmidt <homework@nwsnet.de>
2017-03-17 04:30:49 +0100

    Port dropdown behavior code from jQuery to vanilla JavaScript

--- byceps/blueprints/core/static/behavior/dropdown.js
+++ byceps/blueprints/core/static/behavior/dropdown.js
@@ -1,15 +1,23 @@
 $(function() {
 
-  $('.dropdown .dropdown-toggle').click(function() {
-    $(this).parent().toggleClass('open');
-    return false;
+  // Open dropdown if user clicks on dropdown trigger.
+  document.querySelectorAll('.dropdown .dropdown-toggle').forEach(function(triggerElement) {
+    triggerElement.addEventListener('click', function(event) {
+      var dropdown = triggerElement.parentNode;
+      dropdown.classList.toggle('open');
+
+      event.preventDefault();
+    });
   });
 
-  $(document).click(function(event) {
-    // For details, see: https://css-tricks.com/dangers-stopping-event-propagation/
-    if (!$(event.target).closest('.dropdown').length) {
-      $('.dropdown.open').removeClass('open');
-    }
+  // Close open dropdowns if user clicks outside of an open dropdown.
+  document.addEventListener('click', function(event) {
+    var openDropdowns = document.querySelectorAll('.dropdown.open');
+    openDropdowns.forEach(function(openDropdown) {
+      if (!openDropdown.contains(event.target)) {
+        openDropdown.classList.remove('open');
+      }
+    });
   });
 
 });
