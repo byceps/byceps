f9c7060362bb4bb8e00d105addbf9f8aab00b9c8
Jochen Kupperschmidt <homework@nwsnet.de>
2017-04-13 14:13:03 +0200

    Revert "Remove unused import"
    This reverts commit a684062313e565e2f65793df2d2466240a5fc49b.

--- byceps/services/user_avatar/service.py
+++ byceps/services/user_avatar/service.py
@@ -12,6 +12,7 @@ from ...util.image.models import Dimensions
 from ...util import upload
 
 from ..image import service as image_service
+from ..image.service import ImageTypeProhibited
 
 from .models import Avatar, AvatarCreationTuple, AvatarSelection
 
