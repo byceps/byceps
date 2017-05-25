cea7c71940be45289fc8b46579d9258f79dc6d4a
Jochen Kupperschmidt <homework@nwsnet.de>
2017-05-25 20:36:00 +0200

    Add type hints to user activity service

--- byceps/services/user_activity/service.py
+++ byceps/services/user_activity/service.py
@@ -7,6 +7,9 @@ byceps.services.user_activity.service
 """
 
 from itertools import chain
+from typing import Iterator, List
+
+from ...typing import UserID
 
 from ..newsletter import service as newsletter_service
 from ..shop.order import service as order_service
@@ -16,7 +19,7 @@ from ..user_avatar import service as avatar_service
 from .models import Activity, ActivityType
 
 
-def get_activities_for_user(user_id):
+def get_activities_for_user(user_id: UserID) -> List[Activity]:
     activities = list(chain(
         get_avatar_updates_for_user(user_id),
         get_newsletter_subscription_updates_for_user(user_id),
@@ -29,7 +32,7 @@ def get_activities_for_user(user_id):
     return activities
 
 
-def get_avatar_updates_for_user(user_id):
+def get_avatar_updates_for_user(user_id: UserID) -> Iterator[Activity]:
     """Yield the user's avatar updates as activities."""
     avatars = avatar_service.get_avatars_uploaded_by_user(user_id)
 
@@ -39,7 +42,8 @@ def get_avatar_updates_for_user(user_id):
         yield Activity(avatar.created_at, type_, avatar)
 
 
-def get_newsletter_subscription_updates_for_user(user_id):
+def get_newsletter_subscription_updates_for_user(user_id: UserID) \
+                                                 -> Iterator[Activity]:
     """Yield the user's newsletter subscription updates as activities."""
     updates = newsletter_service.get_subscription_updates_for_user(user_id)
 
@@ -49,7 +53,7 @@ def get_newsletter_subscription_updates_for_user(user_id):
         yield Activity(update.expressed_at, type_, update)
 
 
-def get_orders_for_user(user_id):
+def get_orders_for_user(user_id: UserID) -> Iterator[Activity]:
     """Yield the orders placed by the user as activities."""
     orders = order_service.get_orders_placed_by_user(user_id)
 
@@ -59,7 +63,7 @@ def get_orders_for_user(user_id):
         yield Activity(order.created_at, type_, order)
 
 
-def get_terms_consents_for_user(user_id):
+def get_terms_consents_for_user(user_id: UserID) -> Iterator[Activity]:
     """Yield the user's consents to terms as activities."""
     consents = terms_service.get_consents_by_user(user_id)
 
@@ -69,6 +73,6 @@ def get_terms_consents_for_user(user_id):
         yield Activity(consent.expressed_at, type_, consent)
 
 
-def _sort_activities(activities):
+def _sort_activities(activities: List[Activity]) -> None:
     """Sort activities chronologically backwards, in-place."""
     activities.sort(key=lambda a: a.occured_at, reverse=True)
