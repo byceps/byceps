c26498c9295ffca534d42d80d7d22e53c19822b2
Jochen Kupperschmidt <homework@nwsnet.de>
2017-05-25 20:18:38 +0200

    Add module with BYCEPS-specific types for PEP 484 type hints

---
+++ byceps/typing.py
@@ -0,0 +1,18 @@
+"""
+byceps.typing
+~~~~~~~~~~~~~
+
+BYCEPS-specific type aliases for PEP 484 type hints
+
+:Copyright: 2006-2017 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+from uuid import UUID
+
+
+UserID = UUID
+
+BrandID = str
+
+PartyID = str
