eeb6907690f531f6e36f4665aecac28213f8d398
Jochen Kupperschmidt <homework@nwsnet.de>
2017-08-06 18:51:32 +0200

    Introduce model for brand-based e-mail configuration

---
+++ byceps/services/email/models.py
@@ -0,0 +1,30 @@
+"""
+byceps.services.email.models
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Copyright: 2006-2017 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+from ...database import db
+from ...typing import BrandID
+from ...util.instances import ReprBuilder
+
+from ..brand.models import Brand
+
+
+class EmailConfig(db.Model):
+    """E-mail configuration for a brand."""
+    __tablename__ = 'email_configs'
+
+    brand_id = db.Column(db.Unicode(20), db.ForeignKey('brands.id'), primary_key=True)
+    sender_address = db.Column(db.Unicode(80), nullable=False)
+
+    def __init__(self, brand_id: BrandID, sender_address: str) -> None:
+        self.brand_id = brand_id
+        self.sender_address = sender_address
+
+    def __repr__(self) -> str:
+        return ReprBuilder(self) \
+            .add_with_lookup('brand') \
+            .build()
--- byceps/services/email/service.py
+++ byceps/services/email/service.py
@@ -9,6 +9,15 @@ byceps.services.email.service
 from typing import List, Optional
 
 from ... import email
+from ...typing import BrandID
+
+from .models import EmailConfig
+
+
+def find_sender_address_for_brand(brand_id: BrandID) -> Optional[str]:
+    """Return the configured sender e-mail address for the brand."""
+    config = EmailConfig.query.get(brand_id)
+    return config.sender_address
 
 
 def send_email(recipients: List[str], subject: str, body: str, *,
