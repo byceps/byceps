d790d0dbf16ef8def4fd6ed59ab4c14d35f57eeb
Jochen Kupperschmidt <homework@nwsnet.de>
2017-08-17 02:53:27 +0200

    Merge dropdown behavior code into utility file

--- byceps/blueprints/core/static/behavior/utils.js
+++ byceps/blueprints/core/static/behavior/utils.js
@@ -118,6 +118,35 @@ function _get_location(xhr) {
 
 
 /**
+ * Add behavior to dropdown menus.
+ */
+onDomReady(function() {
+
+  // Open dropdown if user clicks on dropdown trigger.
+  var dropdownToggles = document.querySelectorAll('.dropdown .dropdown-toggle');
+  forEach(dropdownToggles, function(triggerElement) {
+    triggerElement.addEventListener('click', function(event) {
+      var dropdown = triggerElement.parentNode;
+      dropdown.classList.toggle('open');
+
+      event.preventDefault();
+    });
+  });
+
+  // Close open dropdowns if user clicks outside of an open dropdown.
+  document.addEventListener('click', function(event) {
+    var openDropdowns = document.querySelectorAll('.dropdown.open');
+    forEach(openDropdowns, function(openDropdown) {
+      if (!openDropdown.contains(event.target)) {
+        openDropdown.classList.remove('open');
+      }
+    });
+  });
+
+});
+
+
+/**
  * Register an element as click trigger to copy the value of a field to
  * the clipboard.
  */
