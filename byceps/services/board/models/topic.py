0de38fa8aa5d8e6b65ed3893ac32fca83266b3da
Jochen Kupperschmidt <homework@nwsnet.de>
2017-10-01 21:33:44 +0200

    Fix module docstring

--- byceps/services/board/models/topic.py
+++ byceps/services/board/models/topic.py
@@ -1,6 +1,6 @@
 """
-byceps.services.board.models
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+byceps.services.board.models.topic
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 :Copyright: 2006-2017 Jochen Kupperschmidt
 :License: Modified BSD, see LICENSE for details.
