f094a86fdfa3e542a4cbe52e38daa2b4f88dbe37
Jochen Kupperschmidt <homework@nwsnet.de>
2017-10-11 08:43:12 +0200

    Use tickets' order reference instead of using order events to find which tickets to revoke when canceling a paid order

--- byceps/services/shop/order/actions/revoke_tickets.py
+++ byceps/services/shop/order/actions/revoke_tickets.py
@@ -6,9 +6,9 @@ byceps.services.shop.order.actions.revoke_tickets
 :License: Modified BSD, see LICENSE for details.
 """
 
-from typing import Set
+from typing import Sequence
 
-from ....ticketing.models.ticket import Ticket, TicketID
+from ....ticketing.models.ticket import Ticket
 from ....ticketing import ticket_service
 
 from ...article.models.article import ArticleNumber
@@ -21,24 +21,15 @@ from ..models.order_action import Parameters
 def revoke_tickets(order: OrderTuple, article_number: ArticleNumber,
                    quantity: int, parameters: Parameters) -> None:
     """Revoke tickets."""
-    ticket_ids = _get_ticket_ids(order.id)
-        
+    tickets = ticket_service.find_tickets_created_by_order(order.order_number)
+
+    ticket_ids = {t.id for t in tickets}
     ticket_service.revoke_tickets(ticket_ids)
 
-    _create_order_events(order.id, ticket_ids)
+    _create_order_events(order.id, tickets)
 
 
-def _get_ticket_ids(order_id: OrderID) -> Set[TicketID]:
-    event_type = 'ticket-created'
-
-    events = event_service.find_events(order_id, event_type)
-
-    return {event.data['ticket_id'] for event in events}
-
-
-def _create_order_events(order_id: OrderID, ticket_ids: Set[TicketID]) -> None:
-    tickets = ticket_service.find_tickets(ticket_ids)
-
+def _create_order_events(order_id: OrderID, tickets: Sequence[Ticket]) -> None:
     event_type = 'ticket-revoked'
 
     datas = [
