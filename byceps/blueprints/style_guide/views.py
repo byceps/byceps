00969ad1a1f86d905ae68c287e9cef38e00e8090
Jochen Kupperschmidt <homework@nwsnet.de>
2017-10-13 03:03:31 +0200

    Add icon index to style guide

--- byceps/blueprints/style_guide/views.py
+++ byceps/blueprints/style_guide/views.py
@@ -6,6 +6,10 @@ byceps.blueprints.style_guide.views
 :License: Modified BSD, see LICENSE for details.
 """
 
+import xml.etree.ElementTree as ET
+
+from flask import current_app
+
 from ...util.framework.blueprint import create_blueprint
 from ...util.framework.templating import templated
 
@@ -13,7 +17,25 @@ from ...util.framework.templating import templated
 blueprint = create_blueprint('style_guide', __name__)
 
 
+ICONS_FILENAME = 'blueprints/core/static/style/icons.svg'
+
+
 @blueprint.route('/')
 @templated
 def index():
     """Show style guide."""
+    icon_names = list(sorted(_get_icon_names()))
+
+    return {
+        'icon_names': icon_names,
+    }
+
+
+def _get_icon_names():
+    """Extract icon names from SVG file."""
+    with current_app.open_resource(ICONS_FILENAME) as f:
+        tree = ET.parse(f)
+
+    svg_namespace = 'http://www.w3.org/2000/svg'
+    symbol_elems = tree.iterfind('.//{%s}symbol' % svg_namespace)
+    return {elem.get('id') for elem in symbol_elems}
