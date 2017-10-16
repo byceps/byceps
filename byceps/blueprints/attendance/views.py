1197c0bfbd093b6dd1d6fd29d09a5414c5cbea29
Jochen Kupperschmidt <homework@nwsnet.de>
2017-10-16 23:26:31 +0200

    Save lots of SQL queries by separately retrieving users with their avatars in one step
    The orga flag is not set for any of the users. But since this is the
    list of attendees, orgas should not be on it anyway.

--- byceps/blueprints/attendance/views.py
+++ byceps/blueprints/attendance/views.py
@@ -8,6 +8,7 @@ byceps.blueprints.attendance.views
 from flask import g, request
 
 from ...services.ticketing import ticket_service
+from ...services.user import service as user_service
 from ...util.framework.blueprint import create_blueprint
 from ...util.framework.templating import templated
 
@@ -25,6 +26,17 @@ def attendees(page):
 
     tickets = ticket_service.get_tickets_in_use_for_party_paginated(
         g.party_id, page, per_page, search_term=search_term)
+
+    user_ids = {t.used_by_id for t in tickets.items}
+    users = user_service.find_users(user_ids)
+    users_by_id = user_service.index_users_by_id(users)
+
+    tickets.items = [
+        {
+            'user': users_by_id[t.used_by_id],
+        }
+        for t in tickets.items
+    ]
 
     return {
         'search_term': search_term,
