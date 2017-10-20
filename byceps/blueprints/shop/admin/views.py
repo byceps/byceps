c7e9bafa6f10bafbd3907ab9612d328ba20d84ec
Jochen Kupperschmidt <homework@nwsnet.de>
2017-10-20 02:49:33 +0200

    Move shop admin blueprint to `shop.admin` package

--- byceps/blueprints/shop/admin/views.py
+++ byceps/blueprints/shop/admin/views.py
@@ -1,12 +1,12 @@
 """
-byceps.blueprints.shop_admin.views
+byceps.blueprints.shop.admin.views
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 :Copyright: 2006-2017 Jochen Kupperschmidt
 :License: Modified BSD, see LICENSE for details.
 """
 
-from ...util.framework.blueprint import create_blueprint
+from ....util.framework.blueprint import create_blueprint
 
 
 blueprint = create_blueprint('shop_admin', __name__)
