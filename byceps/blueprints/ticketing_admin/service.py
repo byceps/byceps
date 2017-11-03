83ab1e9392ccc2479d6f15650996bdad381dd684
Jochen Kupperschmidt <homework@nwsnet.de>
2017-11-03 01:52:16 +0100

    Fake ticket event for ticket creation in code rather than having a separate (complete) markup block

--- byceps/blueprints/ticketing_admin/service.py
+++ byceps/blueprints/ticketing_admin/service.py
@@ -10,11 +10,14 @@ from typing import Iterator
 
 from ...services.ticketing import event_service
 from ...services.ticketing.models.ticket import TicketID
-from ...services.ticketing.models.ticket_event import TicketEventData
+from ...services.ticketing.models.ticket_event import TicketEvent, \
+    TicketEventData
+from ...services.ticketing import ticket_service
 
 
 def get_events(ticket_id: TicketID) -> Iterator[TicketEventData]:
     events = event_service.get_events_for_ticket(ticket_id)
+    events.insert(0, _fake_ticket_creation_event(ticket_id))
 
     for event in events:
         yield {
@@ -22,3 +25,13 @@ def get_events(ticket_id: TicketID) -> Iterator[TicketEventData]:
             'occured_at': event.occured_at,
             'data': event.data,
         }
+
+
+def _fake_ticket_creation_event(ticket_id: TicketID) -> TicketEvent:
+    ticket = ticket_service.find_ticket(ticket_id)
+    if ticket is None:
+        raise ValueError('Unknown ticket ID')
+
+    data = {}
+
+    return TicketEvent(ticket.created_at, 'ticket-created', ticket.id, data)
