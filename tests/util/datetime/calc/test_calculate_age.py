bc243a5c4378da13737503925718638805bb05ba
Jochen Kupperschmidt <homework@nwsnet.de>
2017-12-29 18:20:22 +0100

    Split date/time-related test module

---
+++ tests/util/datetime/calc/test_calculate_age.py
@@ -0,0 +1,27 @@
+"""
+:Copyright: 2006-2017 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+from datetime import date
+
+import pytest
+
+from byceps.util.datetime.calc import calculate_age
+
+
+SOME_DATE = date(1994, 3, 18)
+
+
+@pytest.mark.parametrize('today, expected', [
+    (date(2014, 3, 17), 19),
+    (date(2014, 3, 18), 20),
+    (date(2014, 3, 19), 20),
+    (date(2015, 3, 17), 20),
+    (date(2015, 3, 18), 21),
+    (date(2015, 3, 19), 21),
+])
+def test_calculate_age(today, expected):
+    actual = calculate_age(SOME_DATE, today)
+
+    assert actual == expected
--- tests/util/datetime/calc/test_calculate_days_util.py
+++ tests/util/datetime/calc/test_calculate_days_util.py
@@ -7,24 +7,10 @@ from datetime import date
 
 import pytest
 
-from byceps.util.datetime.calc import calculate_age, calculate_days_until
+from byceps.util.datetime.calc import calculate_days_until
 
 
 SOME_DATE = date(1994, 3, 18)
-
-
-@pytest.mark.parametrize('today, expected', [
-    (date(2014, 3, 17), 19),
-    (date(2014, 3, 18), 20),
-    (date(2014, 3, 19), 20),
-    (date(2015, 3, 17), 20),
-    (date(2015, 3, 18), 21),
-    (date(2015, 3, 19), 21),
-])
-def test_calculate_age(today, expected):
-    actual = calculate_age(SOME_DATE, today)
-
-    assert actual == expected
 
 
 @pytest.mark.parametrize('today, expected', [
