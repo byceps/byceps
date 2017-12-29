782b546e8ad56f744bf2f0c2459dd5ba70141326
Jochen Kupperschmidt <homework@nwsnet.de>
2017-12-29 18:23:08 +0100

    Split iterables-related test module

---
+++ tests/util/iterables/test_find.py
@@ -0,0 +1,30 @@
+"""
+:Copyright: 2006-2017 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+import pytest
+
+from byceps.util.iterables import find
+
+
+@pytest.mark.parametrize('predicate, iterable, expected', [
+    (
+        lambda x: x > 3,
+        [],
+        None,
+    ),
+    (
+        lambda x: x > 3,
+        [-2, 0, 1, 3],
+        None,
+    ),
+    (
+        lambda x: x > 3,
+        [2, 3, 4, 5],
+        4,
+    ),
+])
+def test_find(predicate, iterable, expected):
+    actual = find(predicate, iterable)
+    assert actual == expected
---
+++ tests/util/iterables/test_pairwise.py
@@ -0,0 +1,27 @@
+"""
+:Copyright: 2006-2017 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+import pytest
+
+from byceps.util.iterables import pairwise
+
+
+@pytest.mark.parametrize('iterable, expected', [
+    (
+        [],
+        [],
+    ),
+    (
+        ['a', 'b', 'c'],
+        [('a', 'b'), ('b', 'c')],
+    ),
+    (
+        range(5),
+        [(0, 1), (1, 2), (2, 3), (3, 4)],
+    ),
+])
+def test_pairwise(iterable, expected):
+    actual = pairwise(iterable)
+    assert list(actual) == expected
