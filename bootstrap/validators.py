73657ffeacf49ad4e7db0e8b57477cdc25ab5493
Jochen Kupperschmidt <homework@nwsnet.de>
2018-01-27 00:13:48 +0100

    Add (non-Click) type hints to script parameter validators

--- bootstrap/validators.py
+++ bootstrap/validators.py
@@ -12,12 +12,16 @@ Validators for use with Click_.
 
 import click
 
+from byceps.services.brand.models.brand import Brand
 from byceps.services.brand import service as brand_service
+from byceps.services.party.models.party import Party
 from byceps.services.party import service as party_service
+from byceps.services.user.models.user import User
 from byceps.services.user import service as user_service
+from byceps.typing import BrandID, PartyID, UserID
 
 
-def validate_brand(ctx, param, brand_id):
+def validate_brand(ctx, param, brand_id: BrandID) -> Brand:
     brand = brand_service.find_brand(brand_id)
 
     if not brand:
@@ -26,7 +30,7 @@ def validate_brand(ctx, param, brand_id):
     return brand
 
 
-def validate_party(ctx, param, party_id):
+def validate_party(ctx, param, party_id: PartyID) -> Party:
     party = party_service.find_party(party_id)
 
     if not party:
@@ -35,7 +39,7 @@ def validate_party(ctx, param, party_id):
     return party
 
 
-def validate_user_id(ctx, param, user_id):
+def validate_user_id(ctx, param, user_id: UserID) -> User:
     user = user_service.find_user(user_id)
 
     if not user:
@@ -44,7 +48,7 @@ def validate_user_id(ctx, param, user_id):
     return user
 
 
-def validate_user_screen_name(ctx, param, screen_name):
+def validate_user_screen_name(ctx, param, screen_name: str) -> User:
     user = user_service.find_user_by_screen_name(screen_name)
 
     if not user:
