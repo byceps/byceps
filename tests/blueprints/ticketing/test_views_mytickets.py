da5858011a6c914074b335b49487fcbdaef86b24
Jochen Kupperschmidt <homework@nwsnet.de>
2018-02-19 02:17:58 +0100

    Test response code of current user's tickets page

---
+++ tests/blueprints/ticketing/test_views_mytickets.py
@@ -0,0 +1,35 @@
+"""
+:Copyright: 2006-2018 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+from tests.base import AbstractAppTestCase
+
+
+class MyTicketsTestCase(AbstractAppTestCase):
+
+    def setUp(self):
+        super().setUp()
+
+        self.create_brand_and_party()
+
+    def test_when_logged_in(self):
+        user = self.create_user('McFly')
+        self.create_session_token(user.id)
+
+        response = self.send_request(user=user)
+
+        assert response.status_code == 200
+        assert response.mimetype == 'text/html'
+
+    def test_when_not_logged_in(self):
+        response = self.send_request()
+
+        assert response.status_code == 403
+
+    # helpers
+
+    def send_request(self, *, user=None):
+        url = '/tickets/mine'
+        with self.client(user=user) as client:
+            return client.get(url)
