48c9860f506bf08fe733ae2033e0ecbc1472d58e
Jochen Kupperschmidt <homework@nwsnet.de>
2018-02-19 01:59:42 +0100

    Test response code of current user's (HTML) page

---
+++ tests/blueprints/user/test_views_current_user.py
@@ -0,0 +1,36 @@
+"""
+:Copyright: 2006-2018 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+from tests.base import AbstractAppTestCase
+
+
+class CurrentUserTestCase(AbstractAppTestCase):
+
+    def setUp(self):
+        super().setUp()
+
+        self.create_brand_and_party()
+
+    def test_when_logged_in(self):
+        user = self.create_user('McFly')
+        self.create_session_token(user.id)
+
+        response = self.send_request(user=user)
+
+        assert response.status_code == 200
+        assert response.mimetype == 'text/html'
+
+    def test_when_not_logged_in(self):
+        response = self.send_request()
+
+        assert response.status_code == 404
+        assert response.mimetype == 'text/html'
+
+    # helpers
+
+    def send_request(self, *, user=None):
+        url = '/users/me'
+        with self.client(user=user) as client:
+            return client.get(url)
