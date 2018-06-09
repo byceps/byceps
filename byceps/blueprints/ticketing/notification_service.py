cfc7aa26f8565db1d2248ad755edbf8d965f78cb
Jochen Kupperschmidt <homework@nwsnet.de>
2018-06-09 22:54:13 +0200

    Extract method to obtain party title

--- byceps/blueprints/ticketing/notification_service.py
+++ byceps/blueprints/ticketing/notification_service.py
@@ -15,48 +15,48 @@ from ...services.user.models.user import User
 
 
 def notify_appointed_user(ticket: Ticket, user: User, manager: User) -> None:
-    party = party_service.find_party(g.party_id)
+    party_title = _get_party_title()
 
     subject = '{} hat dir Ticket {} zugewiesen.' \
         .format(manager.screen_name, ticket.code)
 
     body = '{} hat dir Ticket {} zugewiesen, was dich zur Teilnahme ' \
         'an der {} berechtigt.' \
-        .format(manager.screen_name, ticket.code, party.title)
+        .format(manager.screen_name, ticket.code, party_title)
 
     _enqueue_email(user, subject, body)
 
 
 def notify_withdrawn_user(ticket: Ticket, user: User, manager: User) -> None:
-    party = party_service.find_party(g.party_id)
+    party_title = _get_party_title()
 
     subject = '{} hat Ticket {} zurückgezogen.' \
         .format(manager.screen_name, ticket.code)
 
     body = '{} hat das dir bisher zugewiesene Ticket {} für die {} ' \
         'zurückgezogen.' \
-        .format(manager.screen_name, ticket.code, party.title)
+        .format(manager.screen_name, ticket.code, party_title)
 
     _enqueue_email(user, subject, body)
 
 
 def notify_appointed_user_manager(ticket: Ticket, user: User, manager: User
                                  ) -> None:
-    party = party_service.find_party(g.party_id)
+    party_title = _get_party_title()
 
     subject = '{} hat dir die Nutzerverwaltung für Ticket {} übertragen.' \
         .format(manager.screen_name, ticket.code)
 
     body = '{} hat dir die Verwaltung des Nutzers von Ticket {} für die {} ' \
         'übertragen.' \
-        .format(manager.screen_name, ticket.code, party.title)
+        .format(manager.screen_name, ticket.code, party_title)
 
     _enqueue_email(user, subject, body)
 
 
 def notify_withdrawn_user_manager(ticket: Ticket, user: User, manager: User
                                  ) -> None:
-    party = party_service.find_party(g.party_id)
+    party_title = _get_party_title()
 
     subject = '{} hat die Übertragung der Nutzerverwaltung für Ticket {} ' \
         'an dich zurückgezogen.' \
@@ -64,28 +64,28 @@ def notify_withdrawn_user_manager(ticket: Ticket, user: User, manager: User
 
     body = '{} hat die dir bisher übertragene Verwaltung des Nutzers ' \
         'von Ticket {} für die {} zurückgezogen.' \
-        .format(manager.screen_name, ticket.code, party.title)
+        .format(manager.screen_name, ticket.code, party_title)
 
     _enqueue_email(user, subject, body)
 
 
 def notify_appointed_seat_manager(ticket: Ticket, user: User, manager: User
                                  ) -> None:
-    party = party_service.find_party(g.party_id)
+    party_title = _get_party_title()
 
     subject = '{} hat dir die Sitzplatzverwaltung für Ticket {} übertragen.' \
         .format(manager.screen_name, ticket.code)
 
     body = '{} hat dir die Verwaltung des Sitzplatzes von Ticket {} ' \
         'für die {} übertragen.' \
-        .format(manager.screen_name, ticket.code, party.title)
+        .format(manager.screen_name, ticket.code, party_title)
 
     _enqueue_email(user, subject, body)
 
 
 def notify_withdrawn_seat_manager(ticket: Ticket, user: User, manager: User
                                  ) -> None:
-    party = party_service.find_party(g.party_id)
+    party_title = _get_party_title()
 
     subject = '{} hat die Übertragung der Sitzplatzverwaltung für Ticket {} ' \
         'an dich zurückgezogen.' \
@@ -93,9 +93,14 @@ def notify_withdrawn_seat_manager(ticket: Ticket, user: User, manager: User
 
     body = '{} hat die dir bisher übertragene Verwaltung des Sitzplatzes ' \
         'von Ticket {} für die {} zurückgezogen.' \
-        .format(manager.screen_name, ticket.code, party.title)
+        .format(manager.screen_name, ticket.code, party_title)
 
     _enqueue_email(user, subject, body)
+
+
+def _get_party_title():
+    party = party_service.find_party(g.party_id)
+    return party.title
 
 
 def _enqueue_email(recipient: User, subject: str, body: str) -> None:
