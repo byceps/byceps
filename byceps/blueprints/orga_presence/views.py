1466d59473126e8d861d098dec7210248d67b144
Jochen Kupperschmidt <homework@nwsnet.de>
2018-06-10 06:25:15 +0200

    Extract method from template

--- byceps/blueprints/orga_presence/views.py
+++ byceps/blueprints/orga_presence/views.py
@@ -6,10 +6,12 @@ byceps.blueprints.orga_presence.views
 :License: Modified BSD, see LICENSE for details.
 """
 
+from datetime import date, datetime
+
 from flask import abort
 
 from ...services.orga_presence import service as orga_presence_service
-from ...services.orga_presence.transfer.models import PartyTimeSlot
+from ...services.orga_presence.transfer.models import PartyTimeSlot, TimeSlot
 from ...services.party import service as party_service
 from ...util.framework.blueprint import create_blueprint
 from ...util.framework.templating import templated
@@ -53,4 +55,10 @@ def view(party_id):
         'party_time_slot': party_time_slot,
         'presences': presences,
         'tasks': tasks,
+        'is_instant_contained_in_time_slot': is_instant_contained_in_time_slot,
     }
+
+
+def is_instant_contained_in_time_slot(instant: datetime, time_slot: TimeSlot
+                                     ) -> bool:
+    return time_slot.range.contains(instant)
