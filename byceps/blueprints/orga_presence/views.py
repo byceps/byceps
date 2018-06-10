889fc5dd08929d9b8dfe1882dd6b95b43041a1b5
Jochen Kupperschmidt <homework@nwsnet.de>
2018-06-10 06:44:45 +0200

    Merge multiple presence time slots of a single orga into a single row

--- byceps/blueprints/orga_presence/views.py
+++ byceps/blueprints/orga_presence/views.py
@@ -6,13 +6,17 @@ byceps.blueprints.orga_presence.views
 :License: Modified BSD, see LICENSE for details.
 """
 
+from collections import defaultdict
 from datetime import date, datetime
+from typing import Dict, Iterable
 
 from flask import abort
 
 from ...services.orga_presence import service as orga_presence_service
-from ...services.orga_presence.transfer.models import PartyTimeSlot, TimeSlot
+from ...services.orga_presence.transfer.models import PartyTimeSlot, \
+    PresenceTimeSlot, TimeSlot
 from ...services.party import service as party_service
+from ...services.user.models.user import UserTuple
 from ...util.framework.blueprint import create_blueprint
 from ...util.framework.templating import templated
 
@@ -42,6 +46,8 @@ def view(party_id):
     presences = orga_presence_service.get_presences(party.id)
     tasks = orga_presence_service.get_tasks(party.id)
 
+    presences_by_orga = _group_presences_by_orga(presences)
+
     time_slots = [party_time_slot] + presences + tasks
     hour_ranges = list(orga_presence_service.get_hour_ranges(time_slots))
 
@@ -53,12 +59,22 @@ def view(party_id):
         'days_and_hour_totals': days_and_hour_totals,
         'hour_ranges': hour_ranges,
         'party_time_slot': party_time_slot,
-        'presences': presences,
+        'presences_by_orga': presences_by_orga,
         'tasks': tasks,
-        'is_instant_contained_in_time_slot': is_instant_contained_in_time_slot,
+        'is_instant_contained_in_time_slots': is_instant_contained_in_time_slots,
     }
 
 
-def is_instant_contained_in_time_slot(instant: datetime, time_slot: TimeSlot
-                                     ) -> bool:
-    return time_slot.range.contains(instant)
+def _group_presences_by_orga(presences: Iterable[PresenceTimeSlot]
+                            ) -> Dict[UserTuple, PresenceTimeSlot]:
+    d = defaultdict(set)
+
+    for presence in presences:
+        d[presence.orga].add(presence)
+
+    return d
+
+
+def is_instant_contained_in_time_slots(instant: datetime,
+                                       time_slots: Iterable[TimeSlot]) -> bool:
+    return any(time_slot.range.contains(instant) for time_slot in time_slots)
