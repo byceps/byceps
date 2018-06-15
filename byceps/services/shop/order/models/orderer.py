216ff1ef2193bc45ad58849c7af08ec3295d605b
Jochen Kupperschmidt <homework@nwsnet.de>
2018-06-15 03:33:58 +0200

    Re-define orderer class via 'attr' package, make it immutable

--- byceps/services/shop/order/models/orderer.py
+++ byceps/services/shop/order/models/orderer.py
@@ -6,18 +6,19 @@ byceps.services.shop.order.models.order.orderer
 :License: Modified BSD, see LICENSE for details.
 """
 
+from attr import attrib, attrs
+
 from .....typing import UserID
 
 
+@attrs(frozen=True, slots=True)
 class Orderer:
     """Someone who orders articles."""
 
-    def __init__(self, user_id: UserID, first_names: str, last_name: str,
-                 country: str, zip_code: str, city: str, street: str) -> None:
-        self.user_id = user_id
-        self.first_names = first_names
-        self.last_name = last_name
-        self.country = country
-        self.zip_code = zip_code
-        self.city = city
-        self.street = street
+    user_id = attrib(type=UserID)
+    first_names = attrib(type=str)
+    last_name = attrib(type=str)
+    country = attrib(type=str)
+    zip_code = attrib(type=str)
+    city = attrib(type=str)
+    street = attrib(type=str)
