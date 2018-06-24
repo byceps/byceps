3eb158ffabaf3dfbabef1bd0e3d98c46d37d7be6
Jochen Kupperschmidt <homework@nwsnet.de>
2018-06-24 18:51:09 +0200

    Add missing function arguments

--- tests/blueprints/shop_order/test_views.py
+++ tests/blueprints/shop_order/test_views.py
@@ -69,17 +69,17 @@ class ShopTestCase(AbstractAppTestCase):
         with self.client(user=self.orderer) as client:
             response = client.post(url, data=form_data)
 
-        assert_response_headers()
+        assert_response_headers(response)
 
         article_afterwards = self.get_article()
         assert article_afterwards.quantity == 2
 
         order = Order.query.filter_by(placed_by=self.orderer).one()
-        assert_order('AEC-01-B00005', 1)
+        assert_order(order, 'AEC-01-B00005', 1)
 
         first_order_item = order.items[0]
-        assert_order_item(self.article_id, article_before.price,
-                          article_before.tax_rate, 3)
+        assert_order_item(first_order_item, self.article_id,
+                          article_before.price, article_before.tax_rate, 3)
 
         order_placed_mock.assert_called_once_with(None, order_id=order.id)
 
@@ -101,17 +101,17 @@ class ShopTestCase(AbstractAppTestCase):
         with self.client(user=self.orderer) as client:
             response = client.post(url, data=form_data)
 
-        assert_response_headers()
+        assert_response_headers(response)
 
         article_afterwards = self.get_article()
         assert article_afterwards.quantity == 4
 
         order = Order.query.filter_by(placed_by=self.orderer).one()
-        assert_order('AEC-01-B00005', 1)
+        assert_order(order, 'AEC-01-B00005', 1)
 
         first_order_item = order.items[0]
-        assert_order_item(self.article_id, article_before.price,
-                          article_before.tax_rate, 1)
+        assert_order_item(first_order_item, self.article_id,
+                          article_before.price, article_before.tax_rate, 1)
 
         order_placed_mock.assert_called_once_with(None, order_id=order.id)
 
@@ -121,17 +121,17 @@ class ShopTestCase(AbstractAppTestCase):
         return Article.query.get(self.article_id)
 
 
-def assert_response_headers():
+def assert_response_headers(response):
     assert response.status_code == 302
     assert response.headers.get('Location') == 'http://example.com/shop/order_placed'
 
 
-def assert_order(order_number, item_quantity):
+def assert_order(order, order_number, item_quantity):
     assert order.order_number == order_number
     assert len(order.items) == item_quantity
 
 
-def assert_order_item(article_id, price, tax_rate, quantity):
+def assert_order_item(order_item, article_id, price, tax_rate, quantity):
     assert order_item.article.id == article_id
     assert order_item.price == price
     assert order_item.tax_rate == tax_rate
