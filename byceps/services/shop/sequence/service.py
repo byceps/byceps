3b3367d94c8bce373343c8c8b24af8a7ad61ea0c
Jochen Kupperschmidt <homework@nwsnet.de>
2018-06-30 20:03:49 +0200

    Return number sequence state instead of just the value to simplify service-internal call sites

--- byceps/services/shop/sequence/service.py
+++ byceps/services/shop/sequence/service.py
@@ -36,37 +36,21 @@ class NumberGenerationFailed(Exception):
 
 def generate_article_number(shop_id: ShopID) -> ArticleNumber:
     """Generate and reserve an unused, unique article number for this shop."""
-    sequence = find_article_number_sequence(shop_id)
+    sequence = _get_next_sequence_step(shop_id, Purpose.article)
 
-    if sequence is None:
-        raise NumberGenerationFailed(
-            'No article number sequence is configured for shop "{}".'
-            .format(shop_id))
-
-    prefix = sequence.prefix
-    article_sequence_number = _get_next_sequence_number(shop_id,
-        Purpose.article)
-
-    return ArticleNumber('{}{:05d}'.format(prefix, article_sequence_number))
+    return ArticleNumber('{}{:05d}'.format(sequence.prefix, sequence.value))
 
 
 def generate_order_number(shop_id: ShopID) -> OrderNumber:
     """Generate and reserve an unused, unique order number for this shop."""
-    sequence = find_order_number_sequence(shop_id)
+    sequence = _get_next_sequence_step(shop_id, Purpose.order)
 
-    if sequence is None:
-        raise NumberGenerationFailed(
-            'No order number sequence is configured for shop "{}".'
-            .format(shop_id))
-
-    prefix = sequence.prefix
-    order_sequence_number = _get_next_sequence_number(shop_id, Purpose.order)
-
-    return OrderNumber('{}{:05d}'.format(prefix, order_sequence_number))
+    return OrderNumber('{}{:05d}'.format(sequence.prefix, sequence.value))
 
 
-def _get_next_sequence_number(shop_id: ShopID, purpose: Purpose) -> int:
-    """Calculate and reserve the next sequence number for the shop and
+def _get_next_sequence_step(shop_id: ShopID, purpose: Purpose
+                           ) -> NumberSequence:
+    """Calculate and reserve the next sequence step for the shop and
     purpose.
     """
     sequence = DbNumberSequence.query \
@@ -83,7 +67,7 @@ def _get_next_sequence_number(shop_id: ShopID, purpose: Purpose) -> int:
     sequence.value = DbNumberSequence.value + 1
     db.session.commit()
 
-    return sequence.value
+    return sequence
 
 
 def find_article_number_sequence(shop_id: ShopID) -> Optional[NumberSequence]:
