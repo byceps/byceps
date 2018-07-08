4c1cdfaba230b78c5f1632ed5327912f2fbf5e83
Jochen Kupperschmidt <homework@nwsnet.de>
2018-07-08 05:03:10 +0200

    Add script to create seating area

---
+++ scripts/create_seating_area.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""Create a seating area.
+
+:Copyright: 2006-2018 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+import click
+
+from byceps.services.seating import area_service
+from byceps.util.system import get_config_filename_from_env_or_exit
+
+from bootstrap.validators import validate_party
+from bootstrap.util import app_context
+
+
+@click.command()
+@click.argument('party', callback=validate_party)
+@click.argument('slug')
+@click.argument('title')
+def execute(party, slug, title):
+    area_service.create_area(party.id, slug, title)
+    click.secho('Done.', fg='green')
+
+
+if __name__ == '__main__':
+    config_filename = get_config_filename_from_env_or_exit()
+    with app_context(config_filename):
+        execute()
