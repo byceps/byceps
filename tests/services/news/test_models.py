f22e2ec7e49b1ea965dc6f1efad700090664625b
Jochen Kupperschmidt <homework@nwsnet.de>
2018-09-30 21:18:01 +0200

    Adjust news test to use channels

--- tests/services/news/test_models.py
+++ tests/services/news/test_models.py
@@ -3,7 +3,8 @@
 :License: Modified BSD, see LICENSE for details.
 """
 
-from byceps.services.news import service as news_service
+from byceps.services.news import channel_service as news_channel_service, \
+    service as news_service
 
 from tests.base import AbstractAppTestCase
 from tests.helpers import current_party_set
@@ -18,6 +19,10 @@ class ItemTestCase(AbstractAppTestCase):
 
         self.create_brand_and_party()
 
+        channel_id = '{}-test'.format(self.brand.id)
+        self.channel = news_channel_service.create_channel(self.brand.id,
+                                                           channel_id)
+
     def test_image_url_with_image(self):
         with current_party_set(self.app, self.party), self.app.app_context():
             item = self.create_item('with-image', 'breaking.png')
@@ -31,9 +36,9 @@ class ItemTestCase(AbstractAppTestCase):
     # helpers
 
     def create_item(self, slug, image_url_path):
-        item = news_service.create_item(self.brand.id, slug, self.editor.id,
+        item = news_service.create_item(self.channel.id, slug, self.editor.id,
                                         'the title', 'the body',
                                         image_url_path=image_url_path)
 
         # Return aggregated version of item.
-        return news_service.find_aggregated_item_by_slug(self.brand.id, slug)
+        return news_service.find_aggregated_item_by_slug(self.channel.id, slug)
