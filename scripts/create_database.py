51ab60836c58c7d0d10c637482919a9acbc31efe
Jochen Kupperschmidt <homework@nwsnet.de>
2018-10-01 02:17:22 +0200

    Add script to create initial database structure

---
+++ scripts/create_database.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""Create the initial database structure.
+
+Existing tables will be ignored, and those not existing will be created.
+
+:Copyright: 2006-2018 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+import click
+
+from byceps.database import db
+from byceps.util.system import get_config_filename_from_env_or_exit
+
+from bootstrap.util import app_context
+
+
+@click.command()
+def execute():
+    click.echo('Creating database ... ', nl=False)
+
+    db.create_all()
+
+    click.secho('done.', fg='green')
+
+
+if __name__ == '__main__':
+    config_filename = get_config_filename_from_env_or_exit()
+    with app_context(config_filename):
+        execute()
