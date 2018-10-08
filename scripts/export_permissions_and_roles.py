bb975b843a2886f550107000428b3f6d502bb353
Jochen Kupperschmidt <homework@nwsnet.de>
2018-10-08 19:06:33 +0200

    Add scripts to import and export permissions, roles, and their relations

---
+++ scripts/export_permissions_and_roles.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+"""Export all permissions, roles, and their relations as JSON to STDOUT.
+
+:Copyright: 2006-2018 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+import json
+import sys
+
+import click
+
+from byceps.database import db
+from byceps.services.authorization.models import Permission, Role
+from byceps.util.system import get_config_filename_from_env_or_exit
+
+from bootstrap.util import app_context
+
+
+@click.command()
+def execute():
+    permissions = list(collect_permissions())
+    roles = list(collect_roles())
+
+    data = {
+        'permissions': permissions,
+        'roles': roles,
+    }
+
+    json.dump(data, sys.stdout, indent=2, sort_keys=True)
+
+
+def collect_permissions():
+    """Collect all permissions, even those not assigned to any role."""
+    permissions = Permission.query \
+        .options(
+            db.undefer('title'),
+        ) \
+        .order_by(Permission.id) \
+        .all()
+
+    for permission in permissions:
+        yield {
+            'id': permission.id,
+            'title': permission.title,
+        }
+
+
+def collect_roles():
+    """Collect all roles and the permissions assigned to them."""
+    roles = Role.query \
+        .options(
+            db.undefer('title'),
+            db.joinedload('role_permissions'),
+        ) \
+        .order_by(Role.id) \
+        .all()
+
+    for role in roles:
+        permission_ids = [permission.id for permission in role.permissions]
+        permission_ids.sort()
+
+        yield {
+            'id': role.id,
+            'title': role.title,
+            'assigned_permissions': permission_ids,
+        }
+
+
+if __name__ == '__main__':
+    config_filename = get_config_filename_from_env_or_exit()
+    with app_context(config_filename):
+        execute()
---
+++ scripts/import_permissions_and_roles.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""Import permissions, roles, and their relations from a JSON file.
+
+:Copyright: 2006-2018 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+import json
+
+import click
+
+from byceps.services.authorization import service as authz_service
+from byceps.util.system import get_config_filename_from_env_or_exit
+
+from bootstrap.util import app_context
+
+
+@click.command()
+@click.argument('json_file', type=click.File())
+def execute(json_file):
+    data = json.load(json_file)
+
+    permissions = data['permissions']
+    roles = data['roles']
+
+    create_permissions(permissions)
+    create_roles(roles)
+
+
+def create_permissions(permissions):
+    for permission in permissions:
+        authz_service.create_permission(permission['id'], permission['title'])
+
+
+def create_roles(roles):
+    for role in roles:
+        role_id = role['id']
+
+        authz_service.create_role(role_id, role['title'])
+
+        for permission_id in role['assigned_permissions']:
+            authz_service.assign_permission_to_role(permission_id, role_id)
+
+
+if __name__ == '__main__':
+    config_filename = get_config_filename_from_env_or_exit()
+    with app_context(config_filename):
+        execute()
