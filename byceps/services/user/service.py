ca07cf3162af84b0392af4b3b8c1bba4fe208b55
Jochen Kupperschmidt <homework@nwsnet.de>
2018-10-13 16:17:30 +0200

    Introduce service function to explicitly obtain a user's e-mail address
    This allows to return user DTOs without the (sensible, and rather
    seldomly accessed) e-mail address in most cases.

--- byceps/services/user/service.py
+++ byceps/services/user/service.py
@@ -99,6 +99,12 @@ def get_anonymous_user() -> AnonymousUser:
     return AnonymousUser()
 
 
+def get_email_address(user_id: UserID) -> str:
+    """Return the user's e-mail address."""
+    user = _get_user(user_id)
+    return user.email_address
+
+
 def index_users_by_id(users: Set[User]) -> Dict[UserID, User]:
     """Map the users' IDs to the corresponding user objects."""
     return {user.id: user for user in users}
