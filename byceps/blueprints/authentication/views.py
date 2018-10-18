cc2292e7edf77196011d4d247144e47a53297f02
Jochen Kupperschmidt <homework@nwsnet.de>
2018-10-18 06:01:41 +0200

    Obtain avatar URL only after admin access check

--- byceps/blueprints/authentication/views.py
+++ byceps/blueprints/authentication/views.py
@@ -57,14 +57,15 @@ def _get_current_user(is_admin_mode: bool) -> CurrentUser:
     if user is None:
         return CurrentUser.create_anonymous()
 
-    is_anonymous = False
-    avatar_url = user_avatar_service.get_avatar_url_for_user(user.id)
     permissions = _get_permissions_for_user(user.id)
 
     if is_admin_mode and not _has_admin_access(permissions):
         # The user lacks the admin access permission which is
         # required to enter the admin area.
         return CurrentUser.create_anonymous()
+
+    is_anonymous = False
+    avatar_url = user_avatar_service.get_avatar_url_for_user(user.id)
 
     return CurrentUser(user, is_anonymous, avatar_url, permissions)
 
