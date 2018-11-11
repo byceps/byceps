998406e24d72e537799a5fbcaf5dfcee17f1e972
Jochen Kupperschmidt <homework@nwsnet.de>
2018-11-11 19:30:12 +0100

    Create orders for test via order service

--- tests/services/shop/order/test_ordered_articles_service.py
+++ tests/services/shop/order/test_ordered_articles_service.py
@@ -3,12 +3,15 @@
 :License: Modified BSD, see LICENSE for details.
 """
 
-from itertools import count
+from unittest.mock import patch
 
+from byceps.services.shop.cart.models import Cart
 from byceps.services.shop.order import ordered_articles_service
-from byceps.services.shop.order.transfer.models import PaymentState
+from byceps.services.shop.order import service as order_service
+from byceps.services.shop.order.transfer.models import PaymentMethod, \
+    PaymentState
 
-from testfixtures.shop_order import create_order, create_order_item
+from testfixtures.shop_order import create_orderer
 
 from tests.services.shop.base import ShopTestBase
 
@@ -18,12 +21,14 @@ class OrderedArticlesServiceTestCase(ShopTestBase):
     def setUp(self):
         super().setUp()
 
-        self.user = self.create_user_with_detail()
+        user = self.create_user_with_detail()
+        self.orderer = create_orderer(user)
 
         self.create_brand_and_party()
 
         self.shop = self.create_shop(self.party.id)
-        self.article = self.create_article(self.shop.id)
+        self.create_order_number_sequence(self.shop.id, 'ABC-01-B')
+        self.article = self.create_article(self.shop.id, quantity=100)
 
     def test_count_ordered_articles(self):
         expected = {
@@ -33,7 +38,6 @@ class OrderedArticlesServiceTestCase(ShopTestBase):
             PaymentState.canceled_after_paid: 6,
         }
 
-        order_number_sequence = count(1)
         for article_quantity, payment_state in [
             (4, PaymentState.open),
             (6, PaymentState.canceled_after_paid),
@@ -43,8 +47,8 @@ class OrderedArticlesServiceTestCase(ShopTestBase):
             (2, PaymentState.canceled_before_paid),
             (7, PaymentState.open),
         ]:
-            order_number = 'XY-01-B{:05d}'.format(next(order_number_sequence))
-            self.create_order(order_number, article_quantity, payment_state)
+            order = self.create_order(article_quantity)
+            self.set_payment_state(order.order_number, payment_state)
 
         totals = ordered_articles_service \
             .count_ordered_articles(self.article.item_number)
@@ -54,12 +58,17 @@ class OrderedArticlesServiceTestCase(ShopTestBase):
     # -------------------------------------------------------------------- #
     # helpers
 
-    def create_order(self, order_number, article_quantity, payment_state):
-        order = create_order(self.shop.id, self.user, order_number=order_number)
+    @patch('byceps.blueprints.shop_order.signals.order_placed.send')
+    def create_order(self, article_quantity, order_placed_mock):
+        payment_method = PaymentMethod.bank_transfer
+
+        cart = Cart()
+        cart.add_item(self.article, article_quantity)
+
+        return order_service.create_order(self.shop.id, self.orderer,
+                                          payment_method, cart)
+
+    def set_payment_state(self, order_number, payment_state):
+        order = order_service.find_order_by_order_number(order_number)
         order.payment_state = payment_state
-        self.db.session.add(order)
-
-        order_item = create_order_item(order, self.article, article_quantity)
-        self.db.session.add(order_item)
-
         self.db.session.commit()
