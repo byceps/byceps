900bbf2ac57fb1e8c51a5ced1ded3176894c1312
Jochen Kupperschmidt <homework@nwsnet.de>
2018-11-13 20:27:56 +0100

    Create orders for test via order service

--- tests/blueprints/shop/order_admin/test_views.py
+++ tests/blueprints/shop/order_admin/test_views.py
@@ -4,11 +4,13 @@
 """
 
 from byceps.services.shop.article.models.article import Article
+from byceps.services.shop.cart.models import Cart
 from byceps.services.shop.order.models.order import Order
+from byceps.services.shop.order import service as order_service
 from byceps.services.shop.order.transfer.models import PaymentMethod, \
     PaymentState
 
-from testfixtures.shop_order import create_order, create_order_item
+from testfixtures.shop_order import create_orderer
 
 from tests.base import CONFIG_FILENAME_TEST_ADMIN
 from tests.helpers import assign_permissions_to_user
@@ -27,6 +29,7 @@ class ShopAdminTestCase(ShopTestBase):
         self.create_brand_and_party()
 
         self.shop = self.create_shop(self.party.id)
+        self.create_order_number_sequence(self.shop.id, 'AEC-05-B')
 
     def create_admin(self):
         admin = self.create_user('Admin')
@@ -43,10 +46,11 @@ class ShopAdminTestCase(ShopTestBase):
         return admin
 
     def test_cancel_before_paid(self):
-        article_before = self.create_article(self.shop.id, quantity=5)
+        article_before = self.create_article(self.shop.id, quantity=8)
 
         quantified_articles_to_order = {(article_before, 3)}
-        order_before = self.place_order(quantified_articles_to_order)
+        placed_order = self.place_order(quantified_articles_to_order)
+        order_before = get_order(placed_order.id)
 
         assert article_before.quantity == 5
 
@@ -57,7 +61,7 @@ class ShopAdminTestCase(ShopTestBase):
         with self.client(user_id=self.admin.id) as client:
             response = client.post(url, data=form_data)
 
-        order_afterwards = Order.query.get(order_before.id)
+        order_afterwards = get_order(order_before.id)
         assert response.status_code == 302
         assert_payment(order_afterwards, PaymentMethod.bank_transfer,
                        PaymentState.canceled_before_paid, self.admin.id)
@@ -66,8 +70,8 @@ class ShopAdminTestCase(ShopTestBase):
         assert article_afterwards.quantity == 8
 
     def test_mark_order_as_paid(self):
-        order_before = self.place_order([])
-        self.db.session.commit()
+        placed_order = self.place_order([])
+        order_before = get_order(placed_order.id)
 
         assert_payment_is_open(order_before)
 
@@ -76,16 +80,17 @@ class ShopAdminTestCase(ShopTestBase):
         with self.client(user_id=self.admin.id) as client:
             response = client.post(url, data=form_data)
 
-        order_afterwards = Order.query.get(order_before.id)
+        order_afterwards = get_order(order_before.id)
         assert response.status_code == 302
         assert_payment(order_afterwards, PaymentMethod.direct_debit,
                        PaymentState.paid, self.admin.id)
 
     def test_cancel_after_paid(self):
-        article_before = self.create_article(self.shop.id, quantity=5)
+        article_before = self.create_article(self.shop.id, quantity=8)
 
         quantified_articles_to_order = {(article_before, 3)}
-        order_before = self.place_order(quantified_articles_to_order)
+        placed_order = self.place_order(quantified_articles_to_order)
+        order_before = get_order(placed_order.id)
 
         assert article_before.quantity == 5
 
@@ -101,7 +106,7 @@ class ShopAdminTestCase(ShopTestBase):
         with self.client(user_id=self.admin.id) as client:
             response = client.post(url, data=form_data)
 
-        order_afterwards = Order.query.get(order_before.id)
+        order_afterwards = get_order(order_before.id)
         assert response.status_code == 302
         assert_payment(order_afterwards, PaymentMethod.bank_transfer,
                        PaymentState.canceled_after_paid, self.admin.id)
@@ -112,16 +117,15 @@ class ShopAdminTestCase(ShopTestBase):
     # helpers
 
     def place_order(self, quantified_articles):
-        order = create_order(self.party.id, self.orderer)
-        self.db.session.add(order)
+        orderer = create_orderer(self.orderer)
+        payment_method = PaymentMethod.bank_transfer
+        cart = Cart()
 
         for article, quantity_to_order in quantified_articles:
-            order_item = create_order_item(order, article, quantity_to_order)
-            self.db.session.add(order_item)
+            cart.add_item(article, quantity_to_order)
 
-        self.db.session.commit()
-
-        return order
+        return order_service.place_order(self.shop.id, orderer, payment_method,
+                                         cart, send_signal=False)
 
 
 def assert_payment_is_open(order):
@@ -136,3 +140,7 @@ def assert_payment(order, method, state, updated_by_id):
     assert order.payment_state == state
     assert order.payment_state_updated_at is not None
     assert order.payment_state_updated_by_id == updated_by_id
+
+
+def get_order(order_id):
+    return Order.query.get(order_id)
