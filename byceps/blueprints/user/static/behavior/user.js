2cdce51b2a25c761e0ee3ff263f5c7c096ee4708
Jochen Kupperschmidt <homework@nwsnet.de>
2018-12-02 20:17:30 +0100

    Inline `user.js` into template

--- byceps/blueprints/user/static/behavior/user.js
+++
@@ -1,8 +0,0 @@
-onDomReady(function() {
-
-  confirmed_delete_on_click_then_reload('[data-action="avatar-delete"]', 'Avatarbild entfernen?');
-
-  post_on_click_then_reload('[data-action="newsletter-subscribe"]');
-  confirmed_delete_on_click_then_reload('[data-action="newsletter-unsubscribe"]', 'Wirklich vom Newsletter abmelden?');
-
-});
