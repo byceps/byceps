6d6658f69b85f042b319d619b1687acb91d6b951
Jochen Kupperschmidt <homework@nwsnet.de>
2018-12-12 16:17:45 +0100

    Mark `build_event` for shop order events as public by removing private prefix

--- byceps/services/shop/order/event_service.py
+++ byceps/services/shop/order/event_service.py
@@ -18,7 +18,7 @@ from .transfer.models import OrderID
 def create_event(event_type: str, order_id: OrderID, data: OrderEventData
                 ) -> None:
     """Create an order event."""
-    event = _build_event(event_type, order_id, data)
+    event = build_event(event_type, order_id, data)
 
     db.session.add(event)
     db.session.commit()
@@ -27,13 +27,13 @@ def create_event(event_type: str, order_id: OrderID, data: OrderEventData
 def create_events(event_type: str, order_id: OrderID,
                   datas: Sequence[OrderEventData]) -> None:
     """Create a sequence of order events."""
-    events = [_build_event(event_type, order_id, data) for data in datas]
+    events = [build_event(event_type, order_id, data) for data in datas]
 
     db.session.add_all(events)
     db.session.commit()
 
 
-def _build_event(event_type: str, order_id: OrderID, data: OrderEventData
+def build_event(event_type: str, order_id: OrderID, data: OrderEventData
                 ) -> OrderEvent:
     """Assemble, but not persist, an order event."""
     now = datetime.utcnow()
