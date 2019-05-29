857330ddd22067b23c8f1ad61192d9bdf45db8f6
Jochen Kupperschmidt <homework@nwsnet.de>
2019-05-29 20:11:09 +0200

    Leave screen name length check to WTForms' length validator

--- byceps/blueprints/user/forms.py
+++ byceps/blueprints/user/forms.py
@@ -17,9 +17,13 @@ from ...util.l10n import LocalizedForm
 
 
 class ScreenNameValidator:
+    """Make sure screen name contains only permitted characters.
+
+    However, do *not* check the length; use WTForms' `Length` for that.
+    """
 
     def __call__(self, form, field):
-        if not screen_name_validator.is_screen_name_valid(field.data):
+        if not screen_name_validator.contains_only_valid_chars(field.data):
             raise ValidationError(
                 'Enthält ungültige Zeichen. Erlaubt sind Buchstaben, '
                 ' Ziffern und diese Sonderzeichen: {}' \
--- byceps/services/user/screen_name_validator.py
+++ byceps/services/user/screen_name_validator.py
@@ -22,8 +22,18 @@ VALID_CHARS = frozenset(chain(
 
 
 def is_screen_name_valid(screen_name: str) -> bool:
-    """Return `True` if the screen name contains only permitted characters."""
-    if not MIN_LENGTH <= len(screen_name) <= MAX_LENGTH:
-        return False
+    """Return `True` if the screen name has a valid length and contains
+    only permitted characters.
+    """
+    return is_length_valid(screen_name) \
+        and contains_only_valid_chars(screen_name)
 
+
+def is_length_valid(screen_name: str) -> bool:
+    """Return `True` if the screen name has a valid length."""
+    return MIN_LENGTH <= len(screen_name) <= MAX_LENGTH
+
+
+def contains_only_valid_chars(screen_name: str) -> bool:
+    """Return `True` if the screen name contains only permitted characters."""
     return all(map(VALID_CHARS.__contains__, screen_name))
