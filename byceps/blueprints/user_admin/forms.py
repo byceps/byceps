d9e421ecc618705a3eed341fca0ddbb844ac328b
Jochen Kupperschmidt <homework@nwsnet.de>
2019-05-31 19:52:52 +0200

    Reuse screen name form validator in admin's user creation form

--- byceps/blueprints/user_admin/forms.py
+++ byceps/blueprints/user_admin/forms.py
@@ -12,24 +12,20 @@ from wtforms.validators import InputRequired, Length, ValidationError
 from ...services.user import screen_name_validator
 from ...util.l10n import LocalizedForm
 
+from ..user.forms import ScreenNameValidator
+
 
 class CreateAccountForm(LocalizedForm):
     screen_name = StringField('Benutzername', [
         InputRequired(),
         Length(min=screen_name_validator.MIN_LENGTH,
                max=screen_name_validator.MAX_LENGTH),
+        ScreenNameValidator(),
     ])
     first_names = StringField('Vorname(n)', [InputRequired(), Length(min=2, max=40)])
     last_name = StringField('Nachname', [InputRequired(), Length(min=2, max=40)])
     email_address = StringField('E-Mail-Adresse', [InputRequired(), Length(min=6)])
     password = PasswordField('Passwort', [InputRequired(), Length(min=8)])
-
-    def validate_screen_name(form, field):
-        if not screen_name_validator.is_screen_name_valid(field.data):
-            raise ValidationError(
-                'Enthält ungültige Zeichen. Erlaubt sind Buchstaben, '
-                ' Ziffern und diese Sonderzeichen: {}' \
-                    .format(' '.join(screen_name_validator.SPECIAL_CHARS)))
 
 
 class DeleteAccountForm(LocalizedForm):
