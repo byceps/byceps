cdae72e147e33d24007e9ac90b8938c672da3303
Jochen Kupperschmidt <homework@nwsnet.de>
2019-06-17 00:43:42 +0200

    Extract test context manager that recreates database

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,12 +3,24 @@
 :License: Modified BSD, see LICENSE for details.
 """
 
+from contextlib import contextmanager
+
 import pytest
 
 from byceps.database import db
 
 from tests.base import CONFIG_FILENAME_TEST_ADMIN, \
     CONFIG_FILENAME_TEST_PARTY, create_app
+
+
+@contextmanager
+def database_recreated(db):
+    db.reflect()
+    db.drop_all()
+
+    db.create_all()
+
+    yield
 
 
 @pytest.fixture(scope='session')
@@ -21,13 +33,8 @@ def admin_app():
 @pytest.fixture
 def admin_app_with_db(admin_app):
     with admin_app.app_context():
-        db.reflect()
-        db.drop_all()
-
-        db.create_all()
-
-        yield admin_app
-
+        with database_recreated(db):
+            yield admin_app
         db.session.remove()
 
 
@@ -47,11 +54,6 @@ def party_app():
 @pytest.fixture
 def party_app_with_db(party_app):
     with party_app.app_context():
-        db.reflect()
-        db.drop_all()
-
-        db.create_all()
-
-        yield party_app
-
+        with database_recreated(db):
+            yield party_app
         db.session.remove()
