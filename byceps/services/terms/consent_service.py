d4cabebd0bbc7971355bb6cc1c6d496e1ff20028
Jochen Kupperschmidt <homework@nwsnet.de>
2019-08-06 07:44:47 +0200

    Do not require consent to terms of service by default

--- byceps/services/terms/consent_service.py
+++ byceps/services/terms/consent_service.py
@@ -23,13 +23,14 @@ def is_consent_required_for_brand(brand_id: BrandID) -> bool:
     """Return `True` if consent to the brand's terms of service is
     required.
 
-    By default, consent is required. It can be disabled by configuring
-    the string `false` for the brand setting `terms_consent_required`.
+    By default, consent is not required. It can be required by
+    configuring the string `true` for the brand setting
+    `terms_consent_required`.
     """
     value = brand_settings_service \
         .find_setting_value(brand_id, 'terms_consent_required')
 
-    return value != 'false'
+    return value == 'true'
 
 
 def count_user_consents_for_versions_of_brand(brand_id: BrandID
