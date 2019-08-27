21460c07b3f040ec399d8cd99bdcf9d05d84b366
Jochen Kupperschmidt <homework@nwsnet.de>
2019-08-27 02:38:51 +0200

    Assign all rules to initial user

--- scripts/create_initial_admin_user.py
+++ scripts/create_initial_admin_user.py
@@ -8,6 +8,7 @@
 
 import click
 
+from byceps.services.authorization import service as authorization_service
 from byceps.services.user import command_service as user_command_service
 from byceps.services.user import creation_service as user_creation_service
 from byceps.util.system import get_config_filename_from_env_or_exit
@@ -26,6 +27,8 @@ def execute(screen_name, email_address, password):
 
     user_command_service.enable_user(user.id, user.id)
 
+    _assign_roles(user.id)
+
     click.secho('done.', fg='green')
 
 
@@ -35,6 +38,12 @@ def _create_user(screen_name, email_address, password):
             .create_basic_user(screen_name, email_address, password)
     except ValueError as e:
         raise click.UsageError(e)
+
+
+def _assign_roles(user_id):
+    roles = authorization_service.get_all_roles_with_titles()
+    for role in roles:
+        authorization_service.assign_role_to_user(user.id, role.id)
 
 
 if __name__ == '__main__':
