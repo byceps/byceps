7832fdd7261751b4a085353cca300cf1d9f5f7d5
Jochen Kupperschmidt <homework@nwsnet.de>
2019-09-03 00:46:18 +0200

    Explicitly import board models for which tables otherwise would not get created on `db.create_all()`

---
+++ byceps/services/board/models/__init__.py
@@ -0,0 +1,9 @@
+"""
+byceps.blueprints.board.models
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Copyright: 2006-2019 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+from . import board_access_grant, last_category_view, last_topic_view
