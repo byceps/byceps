feab4240bc4293cb7c905ca2a190da34c989f6c8
Jochen Kupperschmidt <homework@nwsnet.de>
2019-10-21 21:35:37 +0200

    Port badge awarding tests to pytest fixtures

--- tests/services/user_badge/test_service_awarding.py
+++ tests/services/user_badge/test_service_awarding.py
@@ -3,49 +3,55 @@
 :License: Modified BSD, see LICENSE for details.
 """
 
+import pytest
+
 from byceps.services.user_badge import service as user_badge_service
 from byceps.services.user_badge.transfer.models import QuantifiedBadgeAwarding
 
-from tests.base import AbstractAppTestCase
-from tests.helpers import create_user
+from ...conftest import database_recreated
+from ...helpers import create_user
 
 
-class UserBadgeAwardingServiceTestCase(AbstractAppTestCase):
+@pytest.fixture(scope='module')
+def app(party_app, db):
+    with party_app.app_context():
+        with database_recreated(db):
+            yield
 
-    def test_get_awardings_of_unknown_badge(self):
-        unknown_badge_id = '00000000-0000-0000-0000-000000000000'
 
-        with self.app.app_context():
-            actual = user_badge_service.get_awardings_of_badge(unknown_badge_id)
+def test_get_awardings_of_unknown_badge(app):
+    unknown_badge_id = '00000000-0000-0000-0000-000000000000'
 
-        assert actual == set()
+    actual = user_badge_service.get_awardings_of_badge(unknown_badge_id)
 
-    def test_get_awardings_of_unawarded_badge(self):
-        with self.app.app_context():
-            badge = user_badge_service.create_badge(
-                'awesomeness', 'Certificate of Awesomeness', 'awesomeness.svg'
-            )
+    assert actual == set()
 
-            actual = user_badge_service.get_awardings_of_badge(badge.id)
 
-            assert actual == set()
+def test_get_awardings_of_unawarded_badge(app):
+    badge = user_badge_service.create_badge(
+        'awesomeness', 'Certificate of Awesomeness', 'awesomeness.svg'
+    )
 
-    def test_get_awardings_of_badge(self):
-        user1 = create_user('User1')
-        user2 = create_user('User2')
+    actual = user_badge_service.get_awardings_of_badge(badge.id)
 
-        with self.app.app_context():
-            badge = user_badge_service.create_badge(
-                'awesomeness', 'Certificate of Awesomeness', 'awesomeness.svg'
-            )
+    assert actual == set()
 
-            user_badge_service.award_badge_to_user(badge.id, user1.id)
-            user_badge_service.award_badge_to_user(badge.id, user1.id)
-            user_badge_service.award_badge_to_user(badge.id, user2.id)
 
-            actual = user_badge_service.get_awardings_of_badge(badge.id)
+def test_get_awardings_of_badge():
+    user1 = create_user('User1')
+    user2 = create_user('User2')
 
-            assert actual == {
-                QuantifiedBadgeAwarding(badge.id, user1.id, 2),
-                QuantifiedBadgeAwarding(badge.id, user2.id, 1),
-            }
+    badge = user_badge_service.create_badge(
+        'attendee', 'You were there.', 'attendance.svg'
+    )
+
+    user_badge_service.award_badge_to_user(badge.id, user1.id)
+    user_badge_service.award_badge_to_user(badge.id, user1.id)
+    user_badge_service.award_badge_to_user(badge.id, user2.id)
+
+    actual = user_badge_service.get_awardings_of_badge(badge.id)
+
+    assert actual == {
+        QuantifiedBadgeAwarding(badge.id, user1.id, 2),
+        QuantifiedBadgeAwarding(badge.id, user2.id, 1),
+    }
