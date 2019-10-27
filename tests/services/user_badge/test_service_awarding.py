9186f840d7be4741ff0f423eb3c1f9aa2ac2922e
Jochen Kupperschmidt <homework@nwsnet.de>
2019-10-28 00:26:12 +0100

    Provide badges as module-scope fixtures, delete awardings after each badge test

--- tests/services/user_badge/test_service_awarding.py
+++ tests/services/user_badge/test_service_awarding.py
@@ -11,6 +11,9 @@ from byceps.services.user_badge import (
     command_service as badge_command_service,
     service as badge_service,
 )
+from byceps.services.user_badge.models.awarding import (
+    BadgeAwarding as DbBadgeAwarding,
+)
 from byceps.services.user_badge.transfer.models import QuantifiedBadgeAwarding
 
 from ...conftest import database_recreated
@@ -24,10 +27,34 @@ def app(party_app, db):
             yield
 
 
-def test_award_badge_without_initiator(app):
+@pytest.fixture(scope='module')
+def badge1():
+    return _create_badge('attendance', 'You were there.')
+
+
+@pytest.fixture(scope='module')
+def badge2():
+    return _create_badge('awesomeness', 'Certificate of Awesomeness')
+
+
+@pytest.fixture(scope='module')
+def badge3():
+    return _create_badge('eternal-wisdom', 'Eternal Wisdom')
+
+
+@pytest.fixture
+def awardings_scope(db):
+    yield
+
+    # Remove badge awardings.
+    db.session.query(DbBadgeAwarding).delete()
+    db.session.commit()
+
+
+def test_award_badge_without_initiator(app, badge1, awardings_scope):
     user = create_user('EarlyPoster')
 
-    badge = _create_badge('first-post', 'First Post')
+    badge = badge1
 
     user_events_before = event_service.get_events_for_user(user.id)
     assert len(user_events_before) == 0
@@ -47,10 +74,10 @@ def test_award_badge_without_initiator(app):
     assert user_awarding_event.data == {'badge_id': str(badge.id)}
 
 
-def test_award_badge_with_initiator(app, admin_user):
+def test_award_badge_with_initiator(app, badge2, admin_user, awardings_scope):
     user = create_user('AwesomePerson')
 
-    badge = _create_badge('awesomeness', 'Certificate of Awesomeness')
+    badge = badge2
 
     user_events_before = event_service.get_events_for_user(user.id)
     assert len(user_events_before) == 0
@@ -83,19 +110,19 @@ def test_get_awardings_of_unknown_badge(app):
     assert actual == set()
 
 
-def test_get_awardings_of_unawarded_badge(app):
-    badge = _create_badge('eternal-wisdom', 'Eternal Wisdom')
+def test_get_awardings_of_unawarded_badge(app, badge3):
+    badge = badge3
 
     actual = badge_service.get_awardings_of_badge(badge.id)
 
     assert actual == set()
 
 
-def test_get_awardings_of_badge():
+def test_get_awardings_of_badge(badge1, awardings_scope):
     user1 = create_user('User1')
     user2 = create_user('User2')
 
-    badge = _create_badge('attendance', 'You were there.')
+    badge = badge1
 
     badge_command_service.award_badge_to_user(badge.id, user1.id)
     badge_command_service.award_badge_to_user(badge.id, user1.id)
