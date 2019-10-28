6e18d9efc26b6e10feebf86cce26f0e9af3716a0
Jochen Kupperschmidt <homework@nwsnet.de>
2019-10-29 00:19:31 +0100

    Port tourney match comment test class to pytest fixtures

--- tests/api/tourney/match/test_comment_create.py
+++ tests/api/tourney/match/test_comment_create.py
@@ -5,11 +5,13 @@
 
 from base64 import b64encode
 
+import pytest
+
 from byceps.services.tourney.models.match import MatchComment
 from byceps.services.tourney import match_service
 
 from tests.api.helpers import assemble_authorization_header
-from tests.base import AbstractAppTestCase
+from tests.conftest import database_recreated
 from tests.helpers import (
     create_email_config,
     create_site,
@@ -19,70 +21,73 @@ from tests.helpers import (
 )
 
 
-class MatchCommentCreateTest(AbstractAppTestCase):
+def test_create_comment_on_existent_match(app, site, player, match_id):
+    response = request_comment_creation(app, match_id, user_id=player.id)
 
-    def setUp(self):
-        super().setUp()
+    assert response.status_code == 201
 
-        create_email_config()
-        create_site()
+    assert get_comment_count_for_match(match_id) == 1
 
-    def test_create_comment_on_existent_match(self):
-        player = self.create_player()
 
-        match_id = self.create_match()
+def test_create_comment_on_existent_match_as_anonymous_user(
+    app, site, match_id
+):
+    response = request_comment_creation(app, match_id)
 
-        response = self.request_comment_creation(match_id, user_id=player.id)
+    assert response.status_code == 403
 
-        assert response.status_code == 201
+    assert get_comment_count_for_match(match_id) == 0
 
-        assert get_comment_count_for_match(match_id) == 1
 
-    def test_create_comment_on_existent_match_as_anonymous_user(self):
-        match_id = self.create_match()
+def test_create_comment_on_nonexistent_match(app, site, player):
+    unknown_match_id = '00000000-0000-0000-0000-000000000000'
 
-        response = self.request_comment_creation(match_id)
+    response = request_comment_creation(
+        app, unknown_match_id, user_id=player.id
+    )
 
-        assert response.status_code == 403
+    assert response.status_code == 404
 
-        assert get_comment_count_for_match(match_id) == 0
 
-    def test_create_comment_on_nonexistent_match(self):
-        player = self.create_player()
+# helpers
 
-        unknown_match_id = '00000000-0000-0000-0000-000000000000'
 
-        response = self.request_comment_creation(
-            unknown_match_id, user_id=player.id
-        )
+@pytest.fixture(scope='module')
+def app(db, party_app):
+    with party_app.app_context():
+        with database_recreated(db):
+            yield party_app
 
-        assert response.status_code == 404
 
-    # helpers
+@pytest.fixture(scope='module')
+def site():
+    create_email_config()
+    create_site()
 
-    def create_player(self):
-        player = create_user()
 
-        login_user(player.id)
+@pytest.fixture(scope='module')
+def player(app):
+    player = create_user()
 
-        return player
+    login_user(player.id)
 
-    def create_match(self):
-        return match_service.create_match()
+    return player
 
-    def request_comment_creation(self, match_id, *, user_id=None):
-        url = f'/api/tourney/matches/{match_id}/comments'
 
-        headers = [
-            assemble_authorization_header('just-say-PLEASE'),
-        ]
+@pytest.fixture
+def match_id(app):
+    return match_service.create_match()
 
-        form_data = {
-            'body': 'gg',
-        }
 
-        with http_client(self.app, user_id=user_id) as client:
-            return client.post(url, headers=headers, data=form_data)
+def request_comment_creation(app, match_id, *, user_id=None):
+    url = f'/api/tourney/matches/{match_id}/comments'
+
+    headers = [assemble_authorization_header('just-say-PLEASE')]
+
+    form_data = {'body': 'gg'}
+
+    with http_client(app, user_id=user_id) as client:
+        return client.post(url, headers=headers, data=form_data)
 
 
 def get_comment_count_for_match(match_id):
