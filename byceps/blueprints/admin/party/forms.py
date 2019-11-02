f1e5e2cc7fd35e0446f105d619dc01d3ba837865
Jochen Kupperschmidt <homework@nwsnet.de>
2019-11-03 00:35:16 +0100

    Introduce base party form, limit `archived` flag to update form

--- byceps/blueprints/admin/party/forms.py
+++ byceps/blueprints/admin/party/forms.py
@@ -12,14 +12,17 @@ from wtforms.validators import InputRequired, Length, Optional
 from ....util.l10n import LocalizedForm
 
 
-class UpdateForm(LocalizedForm):
+class _BaseForm(LocalizedForm):
     title = StringField('Titel', validators=[Length(min=1, max=40)])
     starts_at = DateTimeField('Beginn', format='%d.%m.%Y %H:%M', validators=[InputRequired()])
     ends_at = DateTimeField('Ende', format='%d.%m.%Y %H:%M', validators=[InputRequired()])
     max_ticket_quantity = IntegerField('Maximale Anzahl Tickets', validators=[Optional()])
     shop_id = StringField('Shop-ID', validators=[Optional()])
-    archived = BooleanField('archiviert')
 
 
-class CreateForm(UpdateForm):
+class CreateForm(_BaseForm):
     id = StringField('ID', validators=[Length(min=1, max=40)])
+
+
+class UpdateForm(_BaseForm):
+    archived = BooleanField('archiviert')
