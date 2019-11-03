8370c4323426afa24924f81af272ef4fc78fee9e
Jochen Kupperschmidt <homework@nwsnet.de>
2019-11-03 19:58:58 +0100

    Port newsletter admin test to pytest fixtures

---
+++ tests/blueprints/admin/conftest.py
@@ -0,0 +1,25 @@
+"""
+:Copyright: 2006-2019 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+
+Fixtures specific to admin blueprints
+"""
+
+import pytest
+
+from tests.base import CONFIG_FILENAME_TEST_ADMIN, create_app
+from tests.conftest import database_recreated
+
+
+@pytest.fixture(scope='session')
+def admin_app_without_db(db):
+    app = create_app(CONFIG_FILENAME_TEST_ADMIN)
+    with app.app_context():
+        yield app
+
+
+@pytest.fixture(scope='module')
+def app(admin_app_without_db, db):
+    app = admin_app_without_db
+    with database_recreated(db):
+        yield app
--- tests/blueprints/admin/newsletter/test_views.py
+++ tests/blueprints/admin/newsletter/test_views.py
@@ -5,13 +5,14 @@
 
 from datetime import datetime
 
+import pytest
+
 from byceps.services.newsletter.models import (
     SubscriptionUpdate as DbSubscriptionUpdate,
 )
 from byceps.services.newsletter import command_service
 from byceps.services.newsletter.types import SubscriptionState
 
-from tests.base import AbstractAppTestCase, CONFIG_FILENAME_TEST_ADMIN
 from tests.helpers import (
     assign_permissions_to_user,
     create_user,
@@ -20,132 +21,135 @@ from tests.helpers import (
 )
 
 
-class NewsletterAdminTestCase(AbstractAppTestCase):
+def test_export_subscribers(newsletter_list, subscribers, client):
+    expected_data = {
+        'subscribers': [
+            {
+                'screen_name': 'User-1',
+                'email_address': 'user001@example.com',
+            },
 
-    def setUp(self):
-        super().setUp(config_filename=CONFIG_FILENAME_TEST_ADMIN)
+            # User #2 has declined a subscription, and thus should be
+            # excluded.
 
-        self.admin = self.create_admin()
+            # User #3 is not initialized, and thus should be excluded.
 
-        self.list_id = command_service.create_list('example', 'Example').id
+            # User #4 has initially declined, but later requested a
+            # subscription, so it should be included.
+            {
+                'screen_name': 'User-4',
+                'email_address': 'user004@example.com',
+            },
 
-        self.setup_subscribers()
+            # User #5 has initially requested, but later declined a
+            # subscription, so it should be excluded.
 
-    def create_admin(self):
-        admin = create_user('Admin')
+            {
+                'screen_name': 'User-6',
+                'email_address': 'user006@example.com',
+            },
 
-        permission_ids = {'admin.access', 'newsletter.export_subscribers'}
-        assign_permissions_to_user(admin.id, 'admin', permission_ids)
+            # User #7 has been suspended and should be excluded, regardless
+            # of subscription state.
 
-        login_user(admin.id)
+            # User #8 has been deleted and should be excluded, regardless
+            # of subscription state.
+        ],
+    }
 
-        return admin
+    url = f'/admin/newsletter/lists/{newsletter_list.id}/subscriptions/export'
+    response = client.get(url)
 
-    def setup_subscribers(self):
-        for number, initialized, suspended, deleted, states in [
-            (1, True,  False, False, [SubscriptionState.requested                             ]),
-            (2, True,  False, False, [SubscriptionState.declined                              ]),
-            (3, False, False, False, [SubscriptionState.requested                             ]),
-            (4, True,  False, False, [SubscriptionState.declined,  SubscriptionState.requested]),
-            (5, True,  False, False, [SubscriptionState.requested, SubscriptionState.declined ]),
-            (6, True,  False, False, [SubscriptionState.requested                             ]),
-            (7, True,  True , False, [SubscriptionState.requested                             ]),
-            (8, True,  False, True , [SubscriptionState.requested                             ]),
-        ]:
-            user = create_user(
-                screen_name=f'User-{number:d}',
-                email_address=f'user{number:03d}@example.com',
-                initialized=initialized,
-            )
+    assert response.status_code == 200
+    assert response.content_type == 'application/json'
+    assert response.json == expected_data
 
-            if suspended:
-                user.suspended = True
-                self.db.session.commit()
 
-            if deleted:
-                user.deleted = True
-                self.db.session.commit()
+def test_export_subscriber_email_addresses(newsletter_list, subscribers, client):
+    expected_data = '\n'.join([
+        'user001@example.com',
+        # User #2 has declined a subscription.
+        # User #3 is not initialized.
+        # User #4 has initially declined, but later requested a subscription.
+        'user004@example.com',
+        # User #5 has initially requested, but later declined a subscription.
+        'user006@example.com',
+        # User #7 has been suspended, and thus should be excluded.
+        # User #8 has been deleted, and thus should be excluded.
+    ]).encode('utf-8')
 
-            self.add_subscriptions(user, states)
+    url = f'/admin/newsletter/lists/{newsletter_list.id}/subscriptions/email_addresses/export'
+    response = client.get(url)
 
-    def test_export_subscribers(self):
-        expected_data = {
-            'subscribers': [
-                {
-                    'screen_name': 'User-1',
-                    'email_address': 'user001@example.com',
-                },
+    assert response.status_code == 200
+    assert response.content_type == 'text/plain; charset=utf-8'
+    assert response.mimetype == 'text/plain'
+    assert response.get_data() == expected_data
 
-                # User #2 has declined a subscription, and thus should be
-                # excluded.
 
-                # User #3 is not initialized, and thus should be excluded.
+@pytest.fixture(scope='module')
+def newsletter_admin():
+    admin = create_user('NewsletterAdmin')
 
-                # User #4 has initially declined, but later requested a
-                # subscription, so it should be included.
-                {
-                    'screen_name': 'User-4',
-                    'email_address': 'user004@example.com',
-                },
+    permission_ids = {'admin.access', 'newsletter.export_subscribers'}
+    assign_permissions_to_user(admin.id, 'admin', permission_ids)
 
-                # User #5 has initially requested, but later declined a
-                # subscription, so it should be excluded.
+    login_user(admin.id)
 
-                {
-                    'screen_name': 'User-6',
-                    'email_address': 'user006@example.com',
-                },
+    return admin
 
-                # User #7 has been suspended and should be excluded, regardless
-                # of subscription state.
 
-                # User #8 has been deleted and should be excluded, regardless
-                # of subscription state.
-            ],
-        }
+@pytest.fixture(scope='module')
+def newsletter_list(app):
+    return command_service.create_list('example', 'Example')
 
-        url = f'/admin/newsletter/lists/{self.list_id}/subscriptions/export'
-        response = self.get_as_admin(url)
 
-        assert response.status_code == 200
-        assert response.content_type == 'application/json'
-        assert response.json == expected_data
-
-    def test_export_subscriber_email_addresses(self):
-        expected_data = '\n'.join([
-            'user001@example.com',
-            # User #2 has declined a subscription.
-            # User #3 is not initialized.
-            # User #4 has initially declined, but later requested a subscription.
-            'user004@example.com',
-            # User #5 has initially requested, but later declined a subscription.
-            'user006@example.com',
-            # User #7 has been suspended, and thus should be excluded.
-            # User #8 has been deleted, and thus should be excluded.
-        ]).encode('utf-8')
-
-        url = f'/admin/newsletter/lists/{self.list_id}/subscriptions/email_addresses/export'
-        response = self.get_as_admin(url)
-
-        assert response.status_code == 200
-        assert response.content_type == 'text/plain; charset=utf-8'
-        assert response.mimetype == 'text/plain'
-        assert response.get_data() == expected_data
-
-    def add_subscriptions(self, user, states):
-        for state in states:
-            self.add_subscription(user, state)
-
-        self.db.session.commit()
-
-    def add_subscription(self, user, state):
-        expressed_at = datetime.utcnow()
-        subscription_update = DbSubscriptionUpdate(
-            user.id, self.list_id, expressed_at, state
+@pytest.fixture(scope='module')
+def subscribers(db, newsletter_list):
+    for number, initialized, suspended, deleted, states in [
+        (1, True,  False, False, [SubscriptionState.requested                             ]),
+        (2, True,  False, False, [SubscriptionState.declined                              ]),
+        (3, False, False, False, [SubscriptionState.requested                             ]),
+        (4, True,  False, False, [SubscriptionState.declined,  SubscriptionState.requested]),
+        (5, True,  False, False, [SubscriptionState.requested, SubscriptionState.declined ]),
+        (6, True,  False, False, [SubscriptionState.requested                             ]),
+        (7, True,  True , False, [SubscriptionState.requested                             ]),
+        (8, True,  False, True , [SubscriptionState.requested                             ]),
+    ]:
+        user = create_user(
+            screen_name=f'User-{number:d}',
+            email_address=f'user{number:03d}@example.com',
+            initialized=initialized,
         )
-        self.db.session.add(subscription_update)
 
-    def get_as_admin(self, url):
-        """Make a GET request as the admin and return the response."""
-        with http_client(self.app, user_id=self.admin.id) as client:
-            return client.get(url)
+        if suspended:
+            user.suspended = True
+            db.session.commit()
+
+        if deleted:
+            user.deleted = True
+            db.session.commit()
+
+        add_subscriptions(db, user.id, newsletter_list.id, states)
+
+
+def add_subscriptions(db, user_id, list_id, states):
+    for state in states:
+        # Timestamp must not be identical for multiple
+        # `(user_id, list_id)` pairs.
+        expressed_at = datetime.utcnow()
+
+        subscription_update = DbSubscriptionUpdate(
+            user_id, list_id, expressed_at, state
+        )
+
+        db.session.add(subscription_update)
+
+    db.session.commit()
+
+
+@pytest.fixture(scope='module')
+def client(app, newsletter_admin):
+    """Provide a test HTTP client against the API."""
+    with http_client(app, user_id=newsletter_admin.id) as client:
+        yield client
