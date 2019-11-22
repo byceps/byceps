c9feb2d03e2d629fea2c0f52e8265586d665f2c8
Jochen Kupperschmidt <homework@nwsnet.de>
2019-11-22 22:27:13 +0100

    Test event returned from user service on screen name change

--- tests/services/user/test_change_screen_name.py
+++ tests/services/user/test_change_screen_name.py
@@ -5,6 +5,7 @@
 
 import pytest
 
+from byceps.events.user import UserScreenNameChanged
 from byceps.services.user import command_service as user_command_service
 from byceps.services.user import event_service
 
@@ -42,11 +43,17 @@ def test_change_screen_name_with_reason(app):
 
     # -------------------------------- #
 
-    user_command_service.change_screen_name(
+    event = user_command_service.change_screen_name(
         user_id, new_screen_name, admin_id, reason=reason
     )
 
     # -------------------------------- #
+
+    assert isinstance(event, UserScreenNameChanged)
+    assert event.user_id == user_id
+    assert event.initiator_id == admin_id
+    assert event.old_screen_name == old_screen_name
+    assert event.new_screen_name == new_screen_name
 
     user_after = user_command_service._get_user(user_id)
     assert user_after.screen_name == new_screen_name
