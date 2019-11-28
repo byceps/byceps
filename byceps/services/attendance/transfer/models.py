824c46b7d3953e1933a72def4edf058a577487ea
Jochen Kupperschmidt <homework@nwsnet.de>
2019-11-28 23:02:52 +0100

    Use `dataclass` instead of `attr` for attendance model

--- byceps/services/attendance/transfer/models.py
+++ byceps/services/attendance/transfer/models.py
@@ -6,14 +6,14 @@ byceps.services.attendance.transfer.models
 :License: Modified BSD, see LICENSE for details.
 """
 
-from attr import attrib, attrs
+from dataclasses import dataclass
 
 from ....services.seating.models.seat import Seat
 from ....services.user.models.user import User
 
 
-@attrs(slots=True)  # Not yet frozen b/c models are not immutable.
+@dataclass  # Not yet frozen b/c models are not immutable.
 class Attendee:
-    user = attrib(type=User)
-    seat = attrib(type=Seat)
-    checked_in = attrib(type=bool)
+    user: User
+    seat: Seat
+    checked_in: bool
