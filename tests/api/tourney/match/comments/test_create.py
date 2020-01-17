8218a4c0e50767778a95bf8358ae60fdbbf8cdd4
Jochen Kupperschmidt <homework@nwsnet.de>
2020-01-17 23:02:56 +0100

    Test tourney match comment attributes after creation

--- tests/api/tourney/match/comments/test_create.py
+++ tests/api/tourney/match/comments/test_create.py
@@ -5,8 +5,13 @@
 
 import pytest
 
-from byceps.services.tourney.models.match_comment import MatchComment
-from byceps.services.tourney import match_service
+from byceps.services.tourney.models.match_comment import (
+    MatchComment as DbMatchComment,
+)
+from byceps.services.tourney import (
+    match_comment_service as comment_service,
+    match_service,
+)
 from byceps.services.user import command_service as user_command_service
 
 from tests.helpers import create_user
@@ -20,6 +25,18 @@ def test_create_comment(api_client, api_client_authz_header, player, match):
     assert response.status_code == 201
     assert get_comment_count_for_match(match.id) == 1
 
+    comment_id = get_comment_id_from_location_header(response)
+    comment = get_comment(comment_id)
+    assert comment.match_id == match.id
+    assert comment.created_at is not None
+    assert comment.created_by.id == player.id
+    assert comment.body_html == 'gg'
+    assert comment.last_edited_at is None
+    assert comment.last_edited_by is None
+    assert not comment.hidden
+    assert comment.hidden_at is None
+    assert comment.hidden_by is None
+
 
 def test_create_comment_on_nonexistent_match(
     api_client, api_client_authz_header, player
@@ -31,6 +48,7 @@ def test_create_comment_on_nonexistent_match(
     )
 
     assert response.status_code == 400
+    assert get_comment_count_for_match(unknown_match_id) == 0
 
 
 def test_create_comment_by_suspended_user(
@@ -96,5 +114,14 @@ def request_comment_creation(
     return api_client.post(url, headers=headers, json=json_data)
 
 
+def get_comment_id_from_location_header(response):
+    location_header = response.headers['Location']
+    return location_header.rpartition('/')[-1]
+
+
+def get_comment(comment_id):
+    return comment_service.get_comment(comment_id)
+
+
 def get_comment_count_for_match(match_id):
-    return MatchComment.query.for_match(match_id).count()
+    return DbMatchComment.query.for_match(match_id).count()
