5b13cf5dda134f6dd33b4161ded70be684de1323
Jochen Kupperschmidt <homework@nwsnet.de>
2020-02-03 02:48:16 +0100

    Remove unused `PATH_BRAND` from configuration files

--- config/production_party.py
+++ config/production_party.py
@@ -24,7 +24,6 @@ BRAND = 'example-brand'
 PARTY = 'example-party-1'
 
 PATH_DATA = Path('./data')
-PATH_BRAND = PATH_DATA / 'brands' / BRAND
 PATH_PARTY = PATH_DATA / 'parties' / PARTY
 
 MAIL_DEBUG = False
