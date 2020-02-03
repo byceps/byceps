956510176efd4a4002742b5e6d8cff659ebbf279
Jochen Kupperschmidt <homework@nwsnet.de>
2020-02-03 22:04:52 +0100

    Remove unused URL build rules for global and brand static files

--- byceps/application.py
+++ byceps/application.py
@@ -192,8 +192,6 @@ def _get_blueprints_debug() -> Iterator[BlueprintReg]:
 def _add_static_file_url_rules(app: Flask) -> None:
     """Add URL rules to for static files."""
     for rule_prefix, endpoint in [
-        ('/global', 'global_file'),
-        ('/brand', 'brand_file'),
         ('/party', 'party_file'),
         ('/site', 'site_file'),
     ]:
