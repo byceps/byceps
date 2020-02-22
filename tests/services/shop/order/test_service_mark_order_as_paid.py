7428a7cad80cdde3c8af5ab1fdac6202e20245fd
Jochen Kupperschmidt <homework@nwsnet.de>
2020-02-23 00:24:16 +0100

    Compress test

--- tests/services/shop/order/test_service_mark_order_as_paid.py
+++ tests/services/shop/order/test_service_mark_order_as_paid.py
@@ -18,16 +18,16 @@ def order(admin_app_with_db, shop, order_number_sequence, orderer, empty_cart):
     return order
 
 
-def test_mark_order_as_paid(admin_app_with_db, order, admin_user):
-    initiator_id = admin_user.id
-
+def test_mark_order_as_paid(order, admin_user):
     order_before = order
     assert order_before.payment_method is None
     assert order_before.payment_state == PaymentState.open
     assert order_before.is_open
     assert not order_before.is_paid
 
-    order_service.mark_order_as_paid(order.id, PaymentMethod.cash, initiator_id)
+    order_service.mark_order_as_paid(
+        order.id, PaymentMethod.cash, admin_user.id
+    )
 
     order_after = order_service.find_order(order.id)
     assert order_after.payment_method == PaymentMethod.cash
