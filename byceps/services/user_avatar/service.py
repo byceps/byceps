f9ae6347ca37ff4b0a25b76259e96d5cce94c2aa
Jochen Kupperschmidt <homework@nwsnet.de>
2020-02-29 15:19:09 +0100

    Use user service to check user ID on user avatar upload

--- byceps/services/user_avatar/service.py
+++ byceps/services/user_avatar/service.py
@@ -16,8 +16,7 @@ from ...util import upload
 
 from ..image import service as image_service
 from ..image.service import ImageTypeProhibited  # Provide to view functions.
-from ..user.models.user import User as DbUser
-from ..user.service import UserIdRejected
+from ..user import service as user_service
 
 from .models import Avatar, AvatarCreationTuple, AvatarSelection
 
@@ -37,9 +36,9 @@ def update_avatar_image(
     Raise `ImageTypeProhibited` if the stream data is not of one the
     allowed types.
     """
-    user = DbUser.query.get(user_id)
+    user = user_service.find_active_user(user_id)
     if user is None:
-        raise UserIdRejected(user_id)
+        raise user_service.UserIdRejected(user_id)
 
     image_type = image_service.determine_image_type(stream, allowed_types)
     image_dimensions = image_service.determine_dimensions(stream)
