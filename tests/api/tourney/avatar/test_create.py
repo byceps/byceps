4831e649322e6e5acb2fb8af58a0482c3e11aa78
Jochen Kupperschmidt <homework@nwsnet.de>
2020-02-29 16:17:17 +0100

    Test tourney avatar upload with invalid user IDs

--- tests/api/tourney/avatar/test_create.py
+++ tests/api/tourney/avatar/test_create.py
@@ -21,6 +21,61 @@ def test_create(
     assert response.status_code == 201
 
 
+def test_create_fails_with_unknown_user_id(
+    data_path, app, api_client, api_client_authz_header, party
+):
+    app.config['PATH_DATA'] = data_path
+
+    unknown_user_id = '99999999-9999-9999-9999-999999999999'
+
+    response = send_request(
+        api_client, api_client_authz_header, party.id, unknown_user_id
+    )
+
+    assert response.status_code == 400
+
+
+def test_create_fails_with_uninitialized_user(
+    data_path,
+    app,
+    api_client,
+    api_client_authz_header,
+    party,
+    uninitialized_user,
+):
+    app.config['PATH_DATA'] = data_path
+
+    response = send_request(
+        api_client, api_client_authz_header, party.id, uninitialized_user.id
+    )
+
+    assert response.status_code == 400
+
+
+def test_create_fails_with_suspended_user(
+    data_path, app, api_client, api_client_authz_header, party, suspended_user
+):
+    app.config['PATH_DATA'] = data_path
+
+    response = send_request(
+        api_client, api_client_authz_header, party.id, suspended_user.id
+    )
+
+    assert response.status_code == 400
+
+
+def test_create_fails_with_deleted_user(
+    data_path, app, api_client, api_client_authz_header, party, deleted_user
+):
+    app.config['PATH_DATA'] = data_path
+
+    response = send_request(
+        api_client, api_client_authz_header, party.id, deleted_user.id
+    )
+
+    assert response.status_code == 400
+
+
 @pytest.fixture
 def data_path():
     with TemporaryDirectory() as d:
