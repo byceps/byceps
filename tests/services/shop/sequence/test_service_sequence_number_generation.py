e844844fd65c00ea2ffd90309ee1143c0f541af8
Jochen Kupperschmidt <homework@nwsnet.de>
2020-03-08 15:16:58 +0100

    Turn shop sequence number generation tests from class-based to function-based using pytest fixtures
    This results in a significant speed up as well.

--- tests/services/shop/sequence/test_service_sequence_number_generation.py
+++ tests/services/shop/sequence/test_service_sequence_number_generation.py
@@ -3,53 +3,79 @@
 :License: Modified BSD, see LICENSE for details.
 """
 
+import pytest
+
 from byceps.services.shop.sequence import service as sequence_service
+from byceps.services.shop.shop import service as shop_service
 
-from tests.helpers import create_email_config
-from tests.services.shop.base import ShopTestBase
+from ....conftest import database_recreated
 
 
-class SequenceNumberGenerationTestCase(ShopTestBase):
+@pytest.fixture(scope='module')
+def app(admin_app, db):
+    with admin_app.app_context():
+        with database_recreated(db):
+            yield admin_app
 
-    def setUp(self):
-        super().setUp()
 
-        create_email_config()
+@pytest.fixture(scope='module')
+def email_config(make_email_config):
+    return make_email_config()
 
-        self.shop = self.create_shop()
 
-    def test_generate_article_number_default(self):
-        sequence_service.create_article_number_sequence(self.shop.id, 'AEC-01-A')
+@pytest.fixture(scope='module')
+def shop1(email_config):
+    return shop_service.create_shop('shop-01', 'Some Shop', email_config.id)
 
-        actual = sequence_service.generate_article_number(self.shop.id)
 
-        assert actual == 'AEC-01-A00001'
+@pytest.fixture(scope='module')
+def shop2(email_config):
+    return shop_service.create_shop('shop-02', 'Another Shop', email_config.id)
 
-    def test_generate_article_number_custom(self):
-        sequence_service.create_article_number_sequence(
-            self.shop.id,
-            'XYZ-09-A',
-            value=41,
-        )
 
-        actual = sequence_service.generate_article_number(self.shop.id)
+def test_generate_article_number_default(app, shop1):
+    shop = shop1
 
-        assert actual == 'XYZ-09-A00042'
+    sequence_service.create_article_number_sequence(shop.id, 'AEC-01-A')
 
-    def test_generate_order_number_default(self):
-        sequence_service.create_order_number_sequence(self.shop.id, 'AEC-01-B')
+    actual = sequence_service.generate_article_number(shop.id)
 
-        actual = sequence_service.generate_order_number(self.shop.id)
+    assert actual == 'AEC-01-A00001'
 
-        assert actual == 'AEC-01-B00001'
 
-    def test_generate_order_number_custom(self):
-        last_assigned_order_sequence_number = 206
+def test_generate_article_number_custom(app, shop2):
+    shop = shop2
 
-        sequence_service.create_order_number_sequence(
-            self.shop.id, 'LOL-03-B', value=last_assigned_order_sequence_number
-        )
+    sequence_service.create_article_number_sequence(
+        shop.id,
+        'XYZ-09-A',
+        value=41,
+    )
 
-        actual = sequence_service.generate_order_number(self.shop.id)
+    actual = sequence_service.generate_article_number(shop.id)
 
-        assert actual == 'LOL-03-B00207'
+    assert actual == 'XYZ-09-A00042'
+
+
+def test_generate_order_number_default(app, shop1):
+    shop = shop1
+
+    sequence_service.create_order_number_sequence(shop.id, 'AEC-01-B')
+
+    actual = sequence_service.generate_order_number(shop.id)
+
+    assert actual == 'AEC-01-B00001'
+
+
+def test_generate_order_number_custom(app, shop2):
+    shop = shop2
+
+    last_assigned_order_sequence_number = 206
+
+    sequence_service.create_order_number_sequence(
+        shop.id, 'LOL-03-B', value=last_assigned_order_sequence_number
+    )
+
+    actual = sequence_service.generate_order_number(shop.id)
+
+    assert actual == 'LOL-03-B00207'
