ee064f779cf847d414f797cbf9dbc8cebdea1140
Jochen Kupperschmidt <homework@nwsnet.de>
2020-03-13 01:55:19 +0100

    Turn user message tests from class-based to function-based using pytest fixtures

--- tests/blueprints/user_message/test_send.py
+++ tests/blueprints/user_message/test_send.py
@@ -3,9 +3,11 @@
 :License: Modified BSD, see LICENSE for details.
 """
 
+import pytest
+
 from unittest.mock import patch
 
-from tests.base import AbstractAppTestCase
+from tests.conftest import database_recreated
 from tests.helpers import (
     create_email_config,
     create_site,
@@ -15,29 +17,43 @@ from tests.helpers import (
 )
 
 
-class SendUserMessageTest(AbstractAppTestCase):
+@pytest.fixture(scope='module')
+def app(party_app, db):
+    with party_app.app_context():
+        with database_recreated(db):
+            create_email_config(
+                sender_address='noreply@example.com',
+                sender_name='ACME Entertainment Convention',
+            )
+            create_site(server_name='acme.example.com')
+            yield party_app
 
-    def setUp(self):
-        super().setUp()
 
-        create_email_config(
-            sender_address='noreply@example.com',
-            sender_name='ACME Entertainment Convention',
-        )
+@pytest.fixture(scope='module')
+def user_alice(app):
+    return create_user(
+        'Alice',
+        user_id='a4903d8f-0bc6-4af9-aeb9-d7534a0a22e8',
+        email_address='alice@example.com',
+    )
 
-        create_site(server_name='acme.example.com')
 
-    @patch('byceps.email.send')
-    def test_send_when_logged_in_without_brand_contact_address(
-        self, send_email_mock
-    ):
-        sender = create_user(
-            'Alice',
-            user_id='a4903d8f-0bc6-4af9-aeb9-d7534a0a22e8',
-            email_address='alice@example.com',
-        )
-        recipient = create_user('Bob', email_address='bob@example.com')
-        text = '''\
+@pytest.fixture(scope='module')
+def user_bob(app):
+    return create_user(
+        'Bob',
+        user_id='11d72bab-3646-4199-b96c-e5e4c6f972bc',
+        email_address='bob@example.com',
+    )
+
+
+@patch('byceps.email.send')
+def test_send_when_logged_in_without_brand_contact_address(
+    send_email_mock, app, user_alice, user_bob
+):
+    sender = user_alice
+    recipient = user_bob
+    text = '''\
 Hi Bob,
 
 please don't forget to take out the trash.
@@ -46,12 +62,14 @@ kthxbye,
 Alice
 '''
 
-        expected_response_location = f'http://example.com/users/{recipient.id}'
+    expected_response_location = f'http://example.com/users/{recipient.id}'
 
-        expected_email_sender = 'ACME Entertainment Convention <noreply@example.com>'
-        expected_email_recipients = ['Bob <bob@example.com>']
-        expected_email_subject = 'Mitteilung von Alice (über acme.example.com)'
-        expected_email_body = '''\
+    expected_email_sender = (
+        'ACME Entertainment Convention <noreply@example.com>'
+    )
+    expected_email_recipients = ['Bob <bob@example.com>']
+    expected_email_subject = 'Mitteilung von Alice (über acme.example.com)'
+    expected_email_body = '''\
 Hallo Bob,
 
 Alice möchte dir die folgende Mitteilung zukommen lassen.
@@ -75,37 +93,32 @@ Alice
 Diese Mitteilung wurde über die Website acme.example.com gesendet.\
 '''
 
-        response = self.send_request(
-            recipient.id, text, current_user_id=sender.id
-        )
+    response = send_request(app, recipient.id, text, current_user_id=sender.id)
 
-        assert response.status_code == 302
-        assert response.location == expected_response_location
+    assert response.status_code == 302
+    assert response.location == expected_response_location
 
-        send_email_mock.assert_called_once_with(
-            expected_email_sender,
-            expected_email_recipients,
-            expected_email_subject,
-            expected_email_body,
-        )
+    send_email_mock.assert_called_once_with(
+        expected_email_sender,
+        expected_email_recipients,
+        expected_email_subject,
+        expected_email_body,
+    )
 
-    @patch('byceps.email.send')
-    def test_send_when_logged_in_with_brand_contact_address(
-        self, send_email_mock
-    ):
-        create_email_config(
-            sender_address='noreply@example.com',
-            sender_name='ACME Entertainment Convention',
-            contact_address='help@example.com',
-        )
 
-        sender = create_user(
-            'Bob',
-            user_id='11d72bab-3646-4199-b96c-e5e4c6f972bc',
-            email_address='bob@example.com',
-        )
-        recipient = create_user('Alice', email_address='alice@example.com')
-        text = '''\
+@patch('byceps.email.send')
+def test_send_when_logged_in_with_brand_contact_address(
+    send_email_mock, app, user_alice, user_bob
+):
+    create_email_config(
+        sender_address='noreply@example.com',
+        sender_name='ACME Entertainment Convention',
+        contact_address='help@example.com',
+    )
+
+    sender = user_bob
+    recipient = user_alice
+    text = '''\
 Hey Alice,
 
 nice to hear from you.
@@ -114,12 +127,14 @@ Best,
 Bob
 '''
 
-        expected_response_location = f'http://example.com/users/{recipient.id}'
+    expected_response_location = f'http://example.com/users/{recipient.id}'
 
-        expected_email_sender = 'ACME Entertainment Convention <noreply@example.com>'
-        expected_email_recipients = ['Alice <alice@example.com>']
-        expected_email_subject = 'Mitteilung von Bob (über acme.example.com)'
-        expected_email_body = '''\
+    expected_email_sender = (
+        'ACME Entertainment Convention <noreply@example.com>'
+    )
+    expected_email_recipients = ['Alice <alice@example.com>']
+    expected_email_subject = 'Mitteilung von Bob (über acme.example.com)'
+    expected_email_body = '''\
 Hallo Alice,
 
 Bob möchte dir die folgende Mitteilung zukommen lassen.
@@ -144,40 +159,39 @@ Diese Mitteilung wurde über die Website acme.example.com gesendet.
 Bei Fragen kontaktiere uns bitte per E-Mail an: help@example.com\
 '''
 
-        response = self.send_request(
-            recipient.id, text, current_user_id=sender.id
-        )
+    response = send_request(app, recipient.id, text, current_user_id=sender.id)
 
-        assert response.status_code == 302
-        assert response.location == expected_response_location
+    assert response.status_code == 302
+    assert response.location == expected_response_location
 
-        send_email_mock.assert_called_once_with(
-            expected_email_sender,
-            expected_email_recipients,
-            expected_email_subject,
-            expected_email_body,
-        )
+    send_email_mock.assert_called_once_with(
+        expected_email_sender,
+        expected_email_recipients,
+        expected_email_subject,
+        expected_email_body,
+    )
 
-    def test_send_when_not_logged_in(self):
-        recipient_id = '8e5037f6-3ca1-4981-b1e4-1998cbdf58e2'
-        text = 'Hello, Eve!'
 
-        response = self.send_request(recipient_id, text)
+def test_send_when_not_logged_in(app):
+    recipient_id = '8e5037f6-3ca1-4981-b1e4-1998cbdf58e2'
+    text = 'Hello, Eve!'
 
-        assert response.status_code == 302
-        assert response.location == 'http://example.com/authentication/login'
+    response = send_request(app, recipient_id, text)
 
-    # helpers
+    assert response.status_code == 302
+    assert response.location == 'http://example.com/authentication/login'
 
-    def send_request(self, recipient_id, text, *, current_user_id=None):
-        url = f'/user_messages/to/{recipient_id}/create'
 
-        form_data = {
-            'body': text,
-        }
+# helpers
 
-        if current_user_id is not None:
-            login_user(current_user_id)
 
-        with http_client(self.app, user_id=current_user_id) as client:
-            return client.post(url, data=form_data)
+def send_request(app, recipient_id, text, *, current_user_id=None):
+    url = f'/user_messages/to/{recipient_id}/create'
+
+    form_data = {'body': text}
+
+    if current_user_id is not None:
+        login_user(current_user_id)
+
+    with http_client(app, user_id=current_user_id) as client:
+        return client.post(url, data=form_data)
