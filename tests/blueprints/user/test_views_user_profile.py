9c546c94bce24a16b7f6d35cad8943ebefa7fd0a
Jochen Kupperschmidt <homework@nwsnet.de>
2020-03-29 19:59:46 +0200

    Extend user profile tests

--- tests/blueprints/user/test_views_user_profile.py
+++ tests/blueprints/user/test_views_user_profile.py
@@ -3,16 +3,67 @@
 :License: Modified BSD, see LICENSE for details.
 """
 
-from tests.helpers import http_client
+import pytest
+
+from tests.conftest import database_recreated
+from tests.helpers import create_site, create_user, http_client
 
 
-def test_view_profile(party_app_with_db, site, normal_user):
-    user = normal_user
+@pytest.fixture(scope='module')
+def app(party_app, db, make_email_config):
+    with party_app.app_context():
+        with database_recreated(db):
+            make_email_config()
+            create_site()
+            yield party_app
 
-    url = f'/users/{user.id}'
 
-    with http_client(party_app_with_db) as client:
-        response = client.get(url)
+def test_view_profile_of_existing_user(app):
+    user = create_user('ExistingUser')
+
+    response = request_profile(app, user.id)
 
     assert response.status_code == 200
     assert response.mimetype == 'text/html'
+
+
+def test_view_profile_of_uninitialized_user(app):
+    user = create_user('UninitializedUser', initialized=False)
+
+    response = request_profile(app, user.id)
+
+    assert response.status_code == 404
+
+
+def test_view_profile_of_suspended_user(app):
+    user = create_user('SuspendedUser', suspended=True)
+
+    response = request_profile(app, user.id)
+
+    assert response.status_code == 404
+
+
+def test_view_profile_of_deleted_user(app):
+    user = create_user('DeletedUser', deleted=True)
+
+    response = request_profile(app, user.id)
+
+    assert response.status_code == 404
+
+
+def test_view_profile_of_unknown_user(app):
+    unknown_user_id = '00000000-0000-0000-0000-000000000000'
+
+    response = request_profile(app, unknown_user_id)
+
+    assert response.status_code == 404
+
+
+# helpers
+
+
+def request_profile(app, user_id):
+    url = f'/users/{user_id}'
+
+    with http_client(app) as client:
+        return client.get(url)
