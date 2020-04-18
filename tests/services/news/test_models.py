8028b169d62706f6e17b58162ed3f7ea7f654abf
Jochen Kupperschmidt <homework@nwsnet.de>
2020-04-18 14:54:59 +0200

    Provide brand and news channel as fixtures

--- tests/services/news/test_models.py
+++ tests/services/news/test_models.py
@@ -15,14 +15,34 @@ from tests.helpers import create_brand, create_user
 from ...conftest import database_recreated
 
 
+@pytest.fixture(scope='module')
+def app(party_app, db):
+    with party_app.app_context():
+        with database_recreated(db):
+            yield party_app
+
+
 @pytest.fixture
 def editor(make_user):
     yield from make_user('NewsEditor')
 
 
-def test_image_url_with_image(app, editor):
+@pytest.fixture(scope='module')
+def brand(app):
+    return create_brand()
+
+
+@pytest.fixture(scope='module')
+def channel(brand):
+    channel_id = f'{brand.id}-test'
+    url_prefix = 'https://example.com/news/'
+
+    return news_channel_service.create_channel(brand.id, channel_id, url_prefix)
+
+
+def test_image_url_with_image(app, channel, editor):
     item = create_item(
-        app.channel.id,
+        channel.id,
         'with-image',
         editor.id,
         image_url_path='breaking.png',
@@ -33,31 +53,15 @@ def test_image_url_with_image(app, editor):
     tear_down_news_item(item)
 
 
-def test_image_url_without_image(app, editor):
-    item = create_item(app.channel.id, 'without-image', editor.id)
+def test_image_url_without_image(app, channel, editor):
+    item = create_item(channel.id, 'without-image', editor.id)
 
     assert item.image_url_path is None
 
     tear_down_news_item(item)
 
 
-@pytest.fixture(scope='module')
-def app(party_app, db):
-    with party_app.app_context():
-        with database_recreated(db):
-            _app = party_app
-
-            brand = create_brand()
-
-            _app.channel = create_channel(brand.id)
-
-            yield _app
-
-
-def create_channel(brand_id):
-    channel_id = f'{brand_id}-test'
-    url_prefix = 'https://example.com/news/'
-    return news_channel_service.create_channel(brand_id, channel_id, url_prefix)
+# helpers
 
 
 def create_item(channel_id, slug, editor_id, *, image_url_path=None):
