b021f32492a0e4885a3cbecdfa4f695f595f4a2f
Jochen Kupperschmidt <homework@nwsnet.de>
2020-04-19 17:30:21 +0200

    Extract test fixtures

--- tests/services/shop/order/test_service_orders_placed_by_user.py
+++ tests/services/shop/order/test_service_orders_placed_by_user.py
@@ -3,9 +3,12 @@
 :License: Modified BSD, see LICENSE for details.
 """
 
+import pytest
+
 from byceps.services.shop.cart.models import Cart
 from byceps.services.shop.order import service as order_service
 from byceps.services.shop.sequence import service as sequence_service
+from byceps.services.shop.shop import service as shop_service
 
 from testfixtures.shop_order import create_orderer as _create_orderer
 
@@ -13,33 +16,53 @@ from tests.helpers import create_user_with_detail
 from tests.services.shop.helpers import create_shop
 
 
-def test_get_orders_placed_by_user(admin_app_with_db, email_config):
-    shop1_id = create_shop('first-nice-shop').id
-    shop2_id = create_shop('second-nice-shop').id
+@pytest.fixture
+def shop1(email_config):
+    shop = create_shop('first-nice-shop')
+    sequence_service.create_order_number_sequence(shop.id, 'LF-02-B')
+    return shop
 
-    sequence_service.create_order_number_sequence(shop1_id, 'LF-02-B')
-    sequence_service.create_order_number_sequence(shop2_id, 'LF-03-B')
 
-    orderer1 = create_orderer('User1')
-    orderer2 = create_orderer('User2')
+@pytest.fixture
+def shop2(email_config):
+    shop = create_shop('second-nice-shop')
+    sequence_service.create_order_number_sequence(shop.id, 'LF-03-B')
+    return shop
 
-    order1 = place_order(shop1_id, orderer1)
-    order2 = place_order(shop1_id, orderer2)  # different user
-    order3 = place_order(shop1_id, orderer1)
-    order4 = place_order(shop1_id, orderer1)
-    order5 = place_order(shop2_id, orderer1)  # different shop
 
-    orders_orderer1_shop1 = get_orders_by_user(orderer1, shop1_id)
+@pytest.fixture
+def orderer1(admin_app_with_db):
+    return create_orderer('Orderer1')
+
+
+@pytest.fixture
+def orderer2(admin_app_with_db):
+    return create_orderer('Orderer2')
+
+
+def test_get_orders_placed_by_user(
+    admin_app_with_db, shop1, shop2, orderer1, orderer2
+):
+    order1 = place_order(shop1.id, orderer1)
+    order2 = place_order(shop1.id, orderer2)  # different user
+    order3 = place_order(shop1.id, orderer1)
+    order4 = place_order(shop1.id, orderer1)
+    order5 = place_order(shop2.id, orderer1)  # different shop
+
+    orders_orderer1_shop1 = get_orders_by_user(orderer1, shop1.id)
     assert orders_orderer1_shop1 == [order4, order3, order1]
 
-    orders_orderer2_shop1 = get_orders_by_user(orderer2, shop1_id)
+    orders_orderer2_shop1 = get_orders_by_user(orderer2, shop1.id)
     assert orders_orderer2_shop1 == [order2]
 
-    orders_orderer1_shop2 = get_orders_by_user(orderer1, shop2_id)
+    orders_orderer1_shop2 = get_orders_by_user(orderer1, shop2.id)
     assert orders_orderer1_shop2 == [order5]
 
     for order in order1, order2, order3, order4, order5:
         order_service.delete_order(order.id)
+
+
+# helpers
 
 
 def create_orderer(screen_name):
