6b3dd31b1a795a92a00c7dba636a88636018655c
Jochen Kupperschmidt <homework@nwsnet.de>
2020-04-21 00:00:21 +0200

    Use existing `admin_app` fixture for admin tests

--- tests/blueprints/admin/conftest.py
+++ tests/blueprints/admin/conftest.py
@@ -7,19 +7,12 @@ Fixtures specific to admin blueprints
 
 import pytest
 
-from tests.base import create_admin_app
 from tests.conftest import database_recreated
 
 
-@pytest.fixture(scope='session')
-def admin_app_without_db(db):
-    app = create_admin_app()
-    with app.app_context():
-        yield app
-
-
 @pytest.fixture(scope='module')
-def app(admin_app_without_db, db):
-    app = admin_app_without_db
-    with database_recreated(db):
-        yield app
+def app(admin_app, db):
+    app = admin_app
+    with app.app_context():
+        with database_recreated(db):
+            yield app
