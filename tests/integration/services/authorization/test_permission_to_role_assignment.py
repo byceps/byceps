40a6a380cbe5c584029afb5b8133651caee20ed3
Jochen Kupperschmidt <homework@nwsnet.de>
2020-06-09 02:10:44 +0200

    Tear down user role assignments

--- tests/integration/services/authorization/test_permission_to_role_assignment.py
+++ tests/integration/services/authorization/test_permission_to_role_assignment.py
@@ -17,6 +17,9 @@ def test_assign_permission_to_role(admin_app, permission, role):
     role_permission_ids_after = get_permission_ids_for_role(role)
     assert permission.id in role_permission_ids_after
 
+    # Clean up.
+    service.deassign_permission_from_role(permission.id, role.id)
+
 
 def test_deassign_permission_from_role(admin_app, permission, role):
     service.assign_permission_to_role(permission.id, role.id)
