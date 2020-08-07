a2a5d1308cff16a6b154647cfa548e18ac5d517f
Jochen Kupperschmidt <homework@nwsnet.de>
2020-08-07 16:08:34 +0200

    Allow internal creation of a user without a screen name

--- byceps/services/user/creation_service.py
+++ byceps/services/user/creation_service.py
@@ -112,7 +112,7 @@ def create_basic_user(
 
 
 def _create_user(
-    screen_name: str,
+    screen_name: Optional[str],
     email_address: Optional[str],
     *,
     first_names: Optional[str] = None,
@@ -153,9 +153,14 @@ def _create_user(
 
 
 def build_user(
-    created_at: datetime, screen_name: str, email_address: Optional[str]
+    created_at: datetime,
+    screen_name: Optional[str],
+    email_address: Optional[str],
 ) -> DbUser:
-    normalized_screen_name = _normalize_screen_name(screen_name)
+    if screen_name is not None:
+        normalized_screen_name = _normalize_screen_name(screen_name)
+    else:
+        normalized_screen_name = None
 
     if email_address is not None:
         normalized_email_address = _normalize_email_address(email_address)
