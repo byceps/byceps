f60dd27f205e93336100480844a304c2f8c43bb9
Jochen Kupperschmidt <homework@nwsnet.de>
2020-08-21 12:50:41 +0200

    Announce ticket check-in on IRC

---
+++ byceps/announce/irc/ticketing.py
@@ -0,0 +1,41 @@
+"""
+byceps.announce.irc.ticketing
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Announce ticketing events on IRC.
+
+:Copyright: 2006-2020 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+from ...events.ticketing import TicketCheckedIn
+from ...services.user import service as user_service
+from ...signals import ticketing as ticketing_signals
+from ...util.irc import send_message
+from ...util.jobqueue import enqueue
+
+from ..helpers import get_screen_name_or_fallback
+
+from ._config import CHANNEL_ORGA_LOG
+
+
+@ticketing_signals.ticket_checked_in.connect
+def _on_ticket_checked_in(sender, *, event: TicketCheckedIn) -> None:
+    enqueue(announce_ticket_checked_in, event)
+
+
+def announce_ticket_checked_in(event: TicketCheckedIn) -> None:
+    """Announce that a ticket has been checked in."""
+    channels = [CHANNEL_ORGA_LOG]
+
+    initiator_screen_name = get_screen_name_or_fallback(
+        user_service.find_screen_name(event.initiator_id)
+    )
+    user_screen_name = get_screen_name_or_fallback(event.user_screen_name)
+
+    text = (
+        f'{initiator_screen_name} hat Ticket "{event.ticket_code}", '
+        f'benutzt von {user_screen_name}, eingecheckt.'
+    )
+
+    send_message(channels, text)
---
+++ tests/integration/announce/irc/test_ticketing.py
@@ -0,0 +1,40 @@
+"""
+:Copyright: 2006-2020 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+from byceps.announce.irc import ticketing  # Load signal handlers.
+from byceps.events.ticketing import TicketCheckedIn
+from byceps.signals import ticketing as ticketing_signals
+
+from .helpers import (
+    assert_submitted_data,
+    CHANNEL_ORGA_LOG,
+    mocked_irc_bot,
+    now,
+)
+
+
+EXPECTED_CHANNELS = [CHANNEL_ORGA_LOG]
+
+
+def test_ticket_checked_in(app, make_user, admin_user):
+    expected_text = (
+        'Admin hat Ticket "GTFIN", benutzt von Einchecker, eingecheckt.'
+    )
+
+    user = make_user('Einchecker')
+
+    with mocked_irc_bot() as mock:
+        event = TicketCheckedIn(
+            occurred_at=now(),
+            initiator_id=admin_user.id,
+            ticket_id=None,
+            ticket_code='GTFIN',
+            occupied_seat_id=None,
+            user_id=user.id,
+            user_screen_name=user.screen_name,
+        )
+        ticketing_signals.ticket_checked_in.send(None, event=event)
+
+        assert_submitted_data(mock, EXPECTED_CHANNELS, expected_text)
