319daaf9025fc8b23c59cc94c305e894b192ceb0
Jochen Kupperschmidt <homework@nwsnet.de>
2020-08-22 22:21:33 +0200

    Add admin UI to create and update email configurations
    SQL:
    
        INSERT INTO authz_permissions (id, title) VALUES ('email_config.create', 'E-Mail-Konfigurationen erstellen');
        INSERT INTO authz_permissions (id, title) VALUES ('email_config.update', 'E-Mail-Konfigurationen bearbeiten');
        INSERT INTO authz_role_permissions (role_id, permission_id) VALUES ('email_admin', 'email_config.create');
        INSERT INTO authz_role_permissions (role_id, permission_id) VALUES ('email_admin', 'email_config.update');

---
+++ byceps/blueprints/admin/email/forms.py
@@ -0,0 +1,26 @@
+"""
+byceps.blueprints.admin.email.forms
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Copyright: 2006-2020 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+from wtforms import StringField
+from wtforms.validators import InputRequired, Length, Optional
+
+from ....util.l10n import LocalizedForm
+
+
+class _BaseForm(LocalizedForm):
+    sender_address = StringField('Absender-Adresse', validators=[InputRequired()])
+    sender_name = StringField('Absender-Name', validators=[Optional()])
+    contact_address = StringField('Kontaktadresse', validators=[Optional()])
+
+
+class CreateForm(_BaseForm):
+    config_id = StringField('ID', validators=[InputRequired()])
+
+
+class UpdateForm(_BaseForm):
+    config_id = StringField('ID')
--- byceps/blueprints/admin/email/views.py
+++ byceps/blueprints/admin/email/views.py
@@ -6,14 +6,19 @@ byceps.blueprints.admin.email.views
 :License: Modified BSD, see LICENSE for details.
 """
 
+from flask import abort, request
+
 from ....services.email import service as email_service
 from ....util.framework.blueprint import create_blueprint
+from ....util.framework.flash import flash_success
 from ....util.framework.templating import templated
+from ....util.views import redirect_to
 
 from ...common.authorization.decorators import permission_required
 from ...common.authorization.registry import permission_registry
 
 from .authorization import EmailConfigPermission
+from .forms import CreateForm, UpdateForm
 
 
 blueprint = create_blueprint('email_admin', __name__)
@@ -32,3 +37,95 @@ def index():
     return {
         'configs': configs,
     }
+
+
+@blueprint.route('/create')
+@permission_required(EmailConfigPermission.create)
+@templated
+def create_form(erroneous_form=None):
+    """Show form to create an e-mail config."""
+    form = erroneous_form if erroneous_form else CreateForm()
+
+    return {
+        'form': form,
+    }
+
+
+@blueprint.route('/', methods=['POST'])
+@permission_required(EmailConfigPermission.create)
+def create():
+    """Create an e-mail config."""
+    form = CreateForm(request.form)
+
+    if not form.validate():
+        return create_form(form)
+
+    config_id = form.config_id.data.strip()
+    sender_address = form.sender_address.data.strip()
+    sender_name = form.sender_name.data.strip() or None
+    contact_address = form.contact_address.data.strip() or None
+
+    config = email_service.create_config(
+        config_id,
+        sender_address,
+        sender_name=sender_name,
+        contact_address=contact_address,
+    )
+
+    flash_success(f'Die Konfiguration "{config.id}" wurde angelegt.')
+    return redirect_to('.index')
+
+
+@blueprint.route('/configs/<config_id>/update')
+@permission_required(EmailConfigPermission.update)
+@templated
+def update_form(config_id, erroneous_form=None):
+    """Show form to update an e-mail config."""
+    config = _get_config_or_404(config_id)
+
+    form = (
+        erroneous_form
+        if erroneous_form
+        else UpdateForm(
+            config_id=config.id,
+            sender_address=config.sender.address,
+            sender_name=config.sender.name,
+            contact_address=config.contact_address,
+        )
+    )
+
+    return {
+        'config': config,
+        'form': form,
+    }
+
+
+@blueprint.route('/configs/<config_id>', methods=['POST'])
+@permission_required(EmailConfigPermission.update)
+def update(config_id):
+    """Update an e-mail config."""
+    config = _get_config_or_404(config_id)
+
+    form = UpdateForm(request.form)
+    if not form.validate():
+        return update_form(config.id, form)
+
+    sender_address = form.sender_address.data.strip()
+    sender_name = form.sender_name.data.strip()
+    contact_address = form.contact_address.data.strip()
+
+    config = email_service.update_config(
+        config.id, sender_address, sender_name, contact_address
+    )
+
+    flash_success(f'Die Konfiguration "{config.id}" wurde aktualisiert.')
+    return redirect_to('.index')
+
+
+def _get_config_or_404(config_id):
+    config = email_service.find_config(config_id)
+
+    if config is None:
+        abort(404)
+
+    return config
