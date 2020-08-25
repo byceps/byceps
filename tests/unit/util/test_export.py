26c91d49b4aa0a520434d5954a67ae098e9ee913
Jochen Kupperschmidt <homework@nwsnet.de>
2020-08-26 00:35:07 +0200

    Test module `byceps.util.export`

---
+++ tests/unit/util/test_export.py
@@ -0,0 +1,26 @@
+"""
+:Copyright: 2006-2020 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+import pytest
+
+from byceps.util.export import serialize_to_csv
+
+
+def test_serialize_to_csv():
+    field_names = ['name', 'color']
+    rows = [
+        {'name': 'Sonic the Hedgehog', 'color': 'blue'},
+        {'name': 'Pac-Man', 'color': 'yellow'},
+        {'name': 'Ultraman', 'color': 'white/red'},
+    ]
+
+    actual = serialize_to_csv(field_names, rows)
+
+    assert list(actual) == [
+        'name;color\r\n',
+        'Sonic the Hedgehog;blue\r\n',
+        'Pac-Man;yellow\r\n',
+        'Ultraman;white/red\r\n',
+    ]
