b9b091e54b0d6be98a4078af9babad9c4c209741
Jochen Kupperschmidt <homework@nwsnet.de>
2020-08-26 00:08:45 +0200

    Re-sort system util functions in linear order

--- byceps/util/system.py
+++ byceps/util/system.py
@@ -15,19 +15,6 @@ from ..config import ConfigurationError
 CONFIG_VAR_NAME = 'BYCEPS_CONFIG'
 
 
-def get_config_filename_from_env() -> str:
-    """Return the configuration filename set via environment variable.
-
-    Raise an exception if it isn't set.
-    """
-    error_message = (
-        "No configuration file was specified via the "
-        f"'{CONFIG_VAR_NAME}' environment variable."
-    )
-
-    return get_env_value(CONFIG_VAR_NAME, error_message)
-
-
 def get_env_value(name: str, error_message: str) -> str:
     """Return the value of the environment variable.
 
@@ -39,6 +26,19 @@ def get_env_value(name: str, error_message: str) -> str:
         raise ConfigurationError(error_message)
 
     return env
+
+
+def get_config_filename_from_env() -> str:
+    """Return the configuration filename set via environment variable.
+
+    Raise an exception if it isn't set.
+    """
+    error_message = (
+        "No configuration file was specified via the "
+        f"'{CONFIG_VAR_NAME}' environment variable."
+    )
+
+    return get_env_value(CONFIG_VAR_NAME, error_message)
 
 
 def get_config_filename_from_env_or_exit() -> str:
