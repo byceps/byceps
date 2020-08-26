b7d7ced7bdfaa180444341c3a57b32b41b15dd09
Jochen Kupperschmidt <homework@nwsnet.de>
2020-08-26 02:13:12 +0200

    Test `byceps.util.l10n.set_locale`

---
+++ tests/unit/util/test_l10n.py
@@ -0,0 +1,20 @@
+"""
+:Copyright: 2006-2020 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+import pytest
+
+from byceps.util.l10n import set_locale
+
+
+def test_set_locale_with_valid_locale():
+    set_locale('en_US.utf8')
+
+
+def test_set_locale_with_invalid_locale():
+    with pytest.warns(UserWarning) as record:
+        set_locale('invalid')
+
+    assert len(record) == 1
+    assert record[0].message.args[0] == 'Could not set locale to "invalid".'
