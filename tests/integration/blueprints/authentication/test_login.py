0ff00997d3dbe2476376131f1b6f1fe87f2c2e9d
Jochen Kupperschmidt <homework@nwsnet.de>
2020-09-05 00:36:07 +0200

    Test basic login

---
+++ tests/integration/blueprints/authentication/test_login.py
@@ -0,0 +1,55 @@
+"""
+:Copyright: 2006-2020 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+import pytest
+
+from byceps.services.authentication.password import service as password_service
+
+
+@pytest.fixture
+def client(site_app, site):
+    return site_app.test_client()
+
+
+def test_login_form(client):
+    response = client.get('/authentication/login')
+
+    assert response.status_code == 200
+
+
+def test_login_succeeds(client, make_user):
+    screen_name = 'LoginTester'
+    password = 'correct horse battery staple'
+
+    user = make_user(screen_name)
+    password_service.create_password_hash(user.id, password)
+
+    assert not list(client.cookie_jar)
+
+    form_data = {
+        'screen_name': screen_name,
+        'password': password,
+    }
+
+    response = client.post('/authentication/login', data=form_data)
+    assert response.status_code == 204
+
+    cookies = list(client.cookie_jar)
+    assert len(cookies) == 1
+
+    cookie = cookies[0]
+    assert cookie.domain == '.www.acmecon.test'
+    assert cookie.name == 'session'
+    assert cookie.secure
+
+
+def test_login_fails(client):
+    form_data = {
+        'screen_name': 'TotallyUnknownUser',
+        'password': 'TotallyWrongPassword',
+    }
+
+    response = client.post('/authentication/login', data=form_data)
+    assert response.status_code == 403
