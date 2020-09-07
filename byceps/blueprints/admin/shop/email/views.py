28ff1b59f895ab31e4f038da69c3a5c5d7cc6fcc
Jochen Kupperschmidt <homework@nwsnet.de>
2020-09-07 18:19:34 +0200

    Extract example shop order email assembly into service

--- byceps/blueprints/admin/shop/email/views.py
+++ byceps/blueprints/admin/shop/email/views.py
@@ -6,20 +6,11 @@ byceps.blueprints.admin.shop.email.views
 :License: Modified BSD, see LICENSE for details.
 """
 
-from datetime import datetime
-from decimal import Decimal
-
 from flask import abort, current_app, Response
 
-from .....config import ConfigurationError
-from .....database import generate_uuid
 from .....services.email import service as email_service
-from .....services.shop.order.email import service as shop_order_email_service
-from .....services.shop.order.email.service import OrderEmailData
-from .....services.shop.order.transfer.models import (
-    Order,
-    PaymentMethod,
-    PaymentState,
+from .....services.shop.order.email import (
+    example_service as example_order_email_service,
 )
 from .....services.shop.shop import service as shop_service
 from .....util.framework.blueprint import create_blueprint
@@ -60,15 +51,13 @@ def view_example_order_placed(shop_id):
     """Show example of order placed e-mail."""
     shop = _get_shop_or_404(shop_id)
 
-    order = _build_order(shop.id, PaymentState.open, is_open=True)
-
-    data = _build_email_data(order, shop)
-
     try:
-        message = shop_order_email_service._assemble_email_for_incoming_order_to_orderer(
-            data
+        message_text = (
+            example_order_email_service.build_example_placed_order_message_text(
+                shop.id
+            )
         )
-    except Exception as e:
+    except example_order_email_service.EmailAssemblyFailed as e:
         current_app.logger.error(
             f'Could not assemble example email for placed order:\n{e}'
         )
@@ -78,7 +67,6 @@ def view_example_order_placed(shop_id):
         )
         return redirect_to('.view_for_shop', shop_id=shop.id)
 
-    message_text = _render_message(message)
     return to_text_response(message_text)
 
 
@@ -88,15 +76,13 @@ def view_example_order_paid(shop_id):
     """Show example of order paid e-mail."""
     shop = _get_shop_or_404(shop_id)
 
-    order = _build_order(shop.id, PaymentState.paid, is_paid=True)
-
-    data = _build_email_data(order, shop)
-
     try:
-        message = shop_order_email_service._assemble_email_for_paid_order_to_orderer(
-            data
+        message_text = (
+            example_order_email_service.build_example_paid_order_message_text(
+                shop.id
+            )
         )
-    except Exception as e:
+    except example_order_email_service.EmailAssemblyFailed as e:
         current_app.logger.error(
             f'Could not assemble example email for paid order:\n{e}'
         )
@@ -106,7 +92,6 @@ def view_example_order_paid(shop_id):
         )
         return redirect_to('.view_for_shop', shop_id=shop.id)
 
-    message_text = _render_message(message)
     return to_text_response(message_text)
 
 
@@ -116,20 +101,11 @@ def view_example_order_canceled(shop_id):
     """Show example of order canceled e-mail."""
     shop = _get_shop_or_404(shop_id)
 
-    order = _build_order(
-        shop.id,
-        PaymentState.canceled_before_paid,
-        is_canceled=True,
-        cancelation_reason='Kein fristgerechter Geldeingang feststellbar',
-    )
-
-    data = _build_email_data(order, shop)
-
     try:
-        message = shop_order_email_service._assemble_email_for_canceled_order_to_orderer(
-            data
+        message_text = example_order_email_service.build_example_canceled_order_message_text(
+            shop.id
         )
-    except Exception as e:
+    except example_order_email_service.EmailAssemblyFailed as e:
         current_app.logger.error(
             f'Could not assemble example email for canceled order:\n{e}'
         )
@@ -139,7 +115,6 @@ def view_example_order_canceled(shop_id):
         )
         return redirect_to('.view_for_shop', shop_id=shop.id)
 
-    message_text = _render_message(message)
     return to_text_response(message_text)
 
 
@@ -150,75 +125,6 @@ def _get_shop_or_404(shop_id):
         abort(404)
 
     return shop
-
-
-def _build_order(
-    shop_id,
-    payment_state,
-    *,
-    is_open=False,
-    is_canceled=False,
-    is_paid=False,
-    cancelation_reason=None,
-) -> Order:
-    order_id = generate_uuid()
-    order_number = 'AWSM-ORDR-9247'
-
-    created_at = datetime.utcnow()
-    placed_by_id = None
-
-    first_names = 'Bella-Bernadine'
-    last_name = 'Ballerwurm'
-    address = None
-
-    total_amount = Decimal('42.95')
-    items = []
-    payment_method = PaymentMethod.bank_transfer
-
-    return Order(
-        order_id,
-        shop_id,
-        order_number,
-        created_at,
-        placed_by_id,
-        first_names,
-        last_name,
-        address,
-        total_amount,
-        items,
-        payment_method,
-        payment_state,
-        is_open,
-        is_canceled,
-        is_paid,
-        False,  # is_invoiced
-        False,  # is_shipping_required
-        False,  # is_shipped
-        cancelation_reason,
-    )
-
-
-def _build_email_data(order, shop) -> OrderEmailData:
-    return OrderEmailData(
-        order=order,
-        email_config_id=shop.email_config_id,
-        orderer_screen_name='Besteller',
-        orderer_email_address='besteller@example.com',
-    )
-
-
-def _render_message(message) -> str:
-    if not message.sender:
-        raise ConfigurationError(
-            'No e-mail sender address configured for message.'
-        )
-
-    return (
-        f'From: {message.sender}\n'
-        f'To: {message.recipients}\n'
-        f'Subject: {message.subject}\n'
-        f'\n\n{message.body}\n'
-    )
 
 
 def to_text_response(text: str) -> Response:
---
+++ byceps/services/shop/order/email/example_service.py
@@ -0,0 +1,157 @@
+"""
+byceps.services.shop.order.email.example_service
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Copyright: 2006-2020 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+from datetime import datetime
+from decimal import Decimal
+
+from .....config import ConfigurationError
+from .....database import generate_uuid
+
+from ....email.transfer.models import Message
+
+from ...shop import service as shop_service
+from ...shop.transfer.models import ShopID
+
+from ..email.service import OrderEmailData
+from ..transfer.models import Order, PaymentMethod, PaymentState
+
+from . import service as shop_order_email_service
+
+
+class EmailAssemblyFailed(Exception):
+    pass
+
+
+def build_example_placed_order_message_text(shop_id: ShopID) -> str:
+    """Assemble an exemplary e-mail for a placed order."""
+    shop = shop_service.get_shop(shop_id)
+
+    order = _build_order(shop.id, PaymentState.open, is_open=True)
+
+    data = _build_email_data(order, shop.email_config_id)
+
+    try:
+        message = shop_order_email_service._assemble_email_for_incoming_order_to_orderer(
+            data
+        )
+    except Exception as e:
+        raise EmailAssemblyFailed()
+
+    return _render_message(message)
+
+
+def build_example_paid_order_message_text(shop_id: ShopID) -> str:
+    """Assemble an exemplary e-mail for a paid order."""
+    shop = shop_service.get_shop(shop_id)
+
+    order = _build_order(shop.id, PaymentState.paid, is_paid=True)
+
+    data = _build_email_data(order, shop.email_config_id)
+
+    try:
+        message = (
+            shop_order_email_service._assemble_email_for_paid_order_to_orderer(
+                data
+            )
+        )
+    except Exception as e:
+        raise EmailAssemblyFailed()
+
+    return _render_message(message)
+
+
+def build_example_canceled_order_message_text(shop_id: ShopID) -> str:
+    """Assemble an exemplary e-mail for a canceled order."""
+    shop = shop_service.get_shop(shop_id)
+
+    order = _build_order(
+        shop.id,
+        PaymentState.canceled_before_paid,
+        is_canceled=True,
+        cancelation_reason='Kein fristgerechter Geldeingang feststellbar.',
+    )
+
+    data = _build_email_data(order, shop.email_config_id)
+
+    try:
+        message = shop_order_email_service._assemble_email_for_canceled_order_to_orderer(
+            data
+        )
+    except Exception as e:
+        raise EmailAssemblyFailed()
+
+    return _render_message(message)
+
+
+def _build_order(
+    shop_id: ShopID,
+    payment_state: PaymentState,
+    *,
+    is_open: bool = False,
+    is_canceled: bool = False,
+    is_paid: bool = False,
+    cancelation_reason: str = None,
+) -> Order:
+    order_id = generate_uuid()
+    order_number = 'AWSM-ORDR-9247'
+
+    created_at = datetime.utcnow()
+    placed_by_id = None
+
+    first_names = 'Bella-Bernadine'
+    last_name = 'Ballerwurm'
+    address = None
+
+    total_amount = Decimal('42.95')
+    items = []
+    payment_method = PaymentMethod.bank_transfer
+
+    return Order(
+        order_id,
+        shop_id,
+        order_number,
+        created_at,
+        placed_by_id,
+        first_names,
+        last_name,
+        address,
+        total_amount,
+        items,
+        payment_method,
+        payment_state,
+        is_open,
+        is_canceled,
+        is_paid,
+        False,  # is_invoiced
+        False,  # is_shipping_required
+        False,  # is_shipped
+        cancelation_reason,
+    )
+
+
+def _build_email_data(order: Order, email_config_id: str) -> OrderEmailData:
+    return OrderEmailData(
+        order=order,
+        email_config_id=email_config_id,
+        orderer_screen_name='Besteller',
+        orderer_email_address='besteller@example.com',
+    )
+
+
+def _render_message(message: Message) -> str:
+    if not message.sender:
+        raise ConfigurationError(
+            'No e-mail sender address configured for message.'
+        )
+
+    return (
+        f'From: {message.sender}\n'
+        f'To: {message.recipients}\n'
+        f'Subject: {message.subject}\n'
+        f'\n\n{message.body}\n'
+    )
