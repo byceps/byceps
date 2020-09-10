ee5bc5d6d212e2bd8e0d60e6024c5180821f6a35
Jochen Kupperschmidt <homework@nwsnet.de>
2020-09-10 21:18:16 +0200

    Format test according to Black

--- tests/integration/services/ticketing/test_creation.py
+++ tests/integration/services/ticketing/test_creation.py
@@ -23,11 +23,17 @@ def test_create_ticket(admin_app, category, ticket_owner):
     ticket_service.delete_ticket(ticket.id)
 
 
-@patch('byceps.services.ticketing.ticket_creation_service._generate_ticket_code')
-def test_create_ticket_with_existing_code(generate_ticket_code_mock, admin_app, category, ticket_owner):
+@patch(
+    'byceps.services.ticketing.ticket_creation_service._generate_ticket_code'
+)
+def test_create_ticket_with_existing_code(
+    generate_ticket_code_mock, admin_app, category, ticket_owner
+):
     generate_ticket_code_mock.return_value = 'TAKEN'
 
-    existing_ticket = ticket_creation_service.create_ticket(category.id, ticket_owner.id)
+    existing_ticket = ticket_creation_service.create_ticket(
+        category.id, ticket_owner.id
+    )
     assert existing_ticket.code == 'TAKEN'
 
     with raises(ticket_creation_service.TicketCreationFailed):
