64aa501a67ec8f4584fc57ae2da96a0d1d575bc6
Jochen Kupperschmidt <homework@nwsnet.de>
2020-09-19 17:10:04 +0200

    Test orga team view

---
+++ tests/integration/blueprints/site/orga_team/test_orga_team_view.py
@@ -0,0 +1,13 @@
+"""
+:Copyright: 2006-2020 Jochen Kupperschmidt
+:License: Modified BSD, see LICENSE for details.
+"""
+
+from tests.helpers import http_client
+
+
+def test_orga_team_view(site_app, site):
+    with http_client(site_app) as client:
+        response = client.get('/orgas/')
+
+    assert response.status_code == 200
