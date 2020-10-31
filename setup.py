8d76652cd98e30e6d33ccf885cb159f142d607cc
Jochen Kupperschmidt <homework@nwsnet.de>
2020-10-31 18:48:47 +0100

    Move build configuration from `setup.py` to `setup.cfg`
    Dependencies are not specified via `install_requires` for now until some
    things are figured out.
    
    Installing requirements via `pip install -r requirements.txt`, followed
    by `pip install -e .` should have the same overall result as before.

--- setup.py
+++ setup.py
@@ -1,42 +1,5 @@
-import codecs
-
-from setuptools import setup
+import setuptools
 
 
-def read_lines_from_file(filename):
-    with codecs.open(filename, encoding='utf-8') as f:
-        return [line.rstrip('\n') for line in f]
-
-
-requirements = read_lines_from_file('requirements.txt')
-
-
-setup(
-    name='BYCEPS',
-    version='0.0',
-    description='the Bring-Your-Computer Event Processing System',
-    author='Jochen "Y0Gi" Kupperschmidt',
-    author_email='homework@nwsnet.de',
-    url='https://byceps.nwsnet.de/',
-    license='Modified BSD',
-    classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Flask',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Other Audience',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: German',
-        'Programming Language :: JavaScript',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Communications',
-        'Topic :: Games/Entertainment',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Content Management System',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Message Boards',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary',
-    ],
-    packages=['byceps'],
-    install_requires=requirements,
-)
+if __name__ == '__main__':
+    setuptools.setup()
