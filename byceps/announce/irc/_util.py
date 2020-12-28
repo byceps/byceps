f93c70774be94adbd92e737b76cd6c1a4d4516e6
Jochen Kupperschmidt <homework@nwsnet.de>
2020-12-28 06:14:55 +0100

    Move hard-coded IRC channel names to webhook definitions in database

--- byceps/announce/irc/_util.py
+++ byceps/announce/irc/_util.py
@@ -17,21 +17,13 @@ from ...events.base import _BaseEvent
 from ...services.webhooks import service as webhook_service
 from ...services.webhooks.transfer.models import OutgoingWebhook
 
-from ..visibility import get_visibilities, Visibility
-
-from ._config import CHANNEL_ORGA_LOG, CHANNEL_PUBLIC
-
-
-CHANNELS_BY_VISIBILITY = {
-    Visibility.internal: CHANNEL_ORGA_LOG,
-    Visibility.public: CHANNEL_PUBLIC,
-}
+from ..visibility import get_visibilities
 
 
 def send_message(
     event: _BaseEvent, scope: str, scope_id: str, text: str
 ) -> None:
-    """Write the text to the channel(s) appropriate for this event type
+    """Write the text to the channel(s) configured for this event type
     by sending it to the bot via HTTP.
     """
     visibilities = get_visibilities(event)
@@ -44,37 +36,39 @@ def send_message(
     scope_id = None
     format = 'weitersager'
 
-    webhooks_with_channels = []
+    webhooks = []
     for visibility in visibilities:
         scope = visibility.name
 
         webhook = webhook_service.find_enabled_outgoing_webhook(
             scope, scope_id, format
         )
-        if webhook is None:
-            continue
 
-        channel = CHANNELS_BY_VISIBILITY[visibility]
+        if webhook is not None:
+            webhooks.append(webhook)
 
-        webhooks_with_channels.append((webhook, channel))
-
-    if not webhooks_with_channels:
+    if not webhooks:
         current_app.logger.warning(
             f'No enabled IRC webhooks found. Not sending message to IRC.'
         )
         return
 
     # Stable order is easier to test.
-    webhooks_with_channels.sort(key=lambda xs: xs[1])
+    webhooks.sort(key=lambda wh: wh.extra_fields['channel'])
 
-    for webhook, channel in webhooks_with_channels:
-        call_webhook(webhook, channel, text)
+    for webhook in webhooks:
+        call_webhook(webhook, text)
 
 
-def call_webhook(webhook: OutgoingWebhook, channel: str, text: str) -> None:
+def call_webhook(webhook: OutgoingWebhook, text: str) -> None:
     text_prefix = webhook.text_prefix
     if text_prefix:
         text = text_prefix + text
+
+    channel = webhook.extra_fields.get('channel')
+    if not channel:
+        current_app.logger.warning(f'No channel specified with IRC webhook.')
+        return
 
     data = {'channel': channel, 'text': text}
 
--- tests/integration/announce/irc/conftest.py
+++ tests/integration/announce/irc/conftest.py
@@ -7,21 +7,31 @@ import pytest
 
 from byceps.services.webhooks import service as webhook_service
 
+from .helpers import CHANNEL_ORGA_LOG, CHANNEL_PUBLIC
+
 
 @pytest.fixture(scope='module')
 def webhook_settings():
-    scopes = {'internal', 'public'}
+    scopes_and_channels = [
+        ('internal', CHANNEL_ORGA_LOG),
+        ('public', CHANNEL_PUBLIC),
+    ]
     scope_id = None
     format = 'weitersager'
     url = 'http://127.0.0.1:12345/'
     enabled = True
 
-    webhooks = {
+    webhooks = [
         webhook_service.create_outgoing_webhook(
-            scope, scope_id, format, url, enabled
+            scope,
+            scope_id,
+            format,
+            url,
+            enabled,
+            extra_fields={'channel': channel},
         )
-        for scope in scopes
-    }
+        for scope, channel in scopes_and_channels
+    ]
 
     yield
 
