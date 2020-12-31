8913b6dc4eb0760a757c133371a320316f1af147
Jochen Kupperschmidt <homework@nwsnet.de>
2020-12-31 14:06:50 +0100

    Add missing import

--- byceps/blueprints/admin/ticketing/views.py
+++ byceps/blueprints/admin/ticketing/views.py
@@ -21,7 +21,7 @@ from ....signals import ticketing as ticketing_signals
 from ....util.framework.blueprint import create_blueprint
 from ....util.framework.flash import flash_error, flash_notice, flash_success
 from ....util.framework.templating import templated
-from ....util.views import permission_required, respond_no_content
+from ....util.views import permission_required, redirect_to, respond_no_content
 
 from ...common.authorization.registry import permission_registry
 
