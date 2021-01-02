063b43b83dcc8a9e92c3e0ddec32d84cffbd1695
Jochen Kupperschmidt <homework@nwsnet.de>
2021-01-02 15:40:38 +0100

    Move view functions to check in user/revert check-in to ticketing check-in blueprint

--- byceps/blueprints/admin/ticketing/checkin/views.py
+++ byceps/blueprints/admin/ticketing/checkin/views.py
@@ -8,13 +8,19 @@ byceps.blueprints.admin.ticketing.checkin.views
 
 from datetime import date
 
-from flask import abort, request
+from flask import abort, g, request
 
 from .....services.party import service as party_service
-from .....services.ticketing import ticket_service
+from .....services.ticketing import (
+    exceptions as ticket_exceptions,
+    ticket_service,
+    ticket_user_checkin_service,
+)
+from .....signals import ticketing as ticketing_signals
 from .....util.framework.blueprint import create_blueprint
+from .....util.framework.flash import flash_error, flash_notice, flash_success
 from .....util.framework.templating import templated
-from .....util.views import permission_required
+from .....util.views import permission_required, respond_no_content
 
 from ...ticketing.authorization import TicketingPermission
 from ...user import service as user_blueprint_service
@@ -96,3 +102,63 @@ def _get_tickets_for_users(party_id, users):
         yield from ticket_service.find_tickets_related_to_user_for_party(
             user.id, party_id
         )
+
+
+@blueprint.route('/tickets/<uuid:ticket_id>/check_in_user', methods=['POST'])
+@permission_required(TicketingPermission.checkin)
+@respond_no_content
+def check_in_user(ticket_id):
+    """Check the user in."""
+    ticket = _get_ticket_or_404(ticket_id)
+
+    initiator_id = g.current_user.id
+
+    try:
+        event = ticket_user_checkin_service.check_in_user(
+            ticket.id, initiator_id
+        )
+    except ticket_exceptions.UserAccountDeleted:
+        flash_error(
+            'Das dem Ticket zugewiesene Benutzerkonto ist gelöscht worden. '
+            'Der Check-In ist nicht erlaubt.'
+        )
+        return
+    except ticket_exceptions.UserAccountSuspended:
+        flash_error(
+            'Das dem Ticket zugewiesene Benutzerkonto ist gesperrt. '
+            'Der Check-In ist nicht erlaubt.'
+        )
+        return
+
+    ticketing_signals.ticket_checked_in.send(None, event=event)
+
+    flash_success(f"Benutzer '{ticket.used_by.screen_name}' wurde eingecheckt.")
+
+    occupies_seat = ticket.occupied_seat_id is not None
+    if not occupies_seat:
+        flash_notice('Das Ticket belegt noch keinen Sitzplatz.', icon='warning')
+
+
+@blueprint.route(
+    '/tickets/<uuid:ticket_id>/revert_user_check_in', methods=['POST']
+)
+@permission_required(TicketingPermission.checkin)
+@respond_no_content
+def revert_user_check_in(ticket_id):
+    """Revert the user check-in state."""
+    ticket = _get_ticket_or_404(ticket_id)
+
+    initiator_id = g.current_user.id
+
+    ticket_user_checkin_service.revert_user_check_in(ticket.id, initiator_id)
+
+    flash_success('Der Check-In wurde rückgängig gemacht.')
+
+
+def _get_ticket_or_404(ticket_id):
+    ticket = ticket_service.find_ticket(ticket_id)
+
+    if ticket is None:
+        abort(404)
+
+    return ticket
--- byceps/blueprints/admin/ticketing/views.py
+++ byceps/blueprints/admin/ticketing/views.py
@@ -11,17 +11,14 @@ from flask import abort, g, request
 from ....services.party import service as party_service
 from ....services.shop.order import service as order_service
 from ....services.ticketing import (
-    exceptions as ticket_exceptions,
     ticket_bundle_service,
     ticket_service,
-    ticket_user_checkin_service,
     ticket_user_management_service,
 )
-from ....signals import ticketing as ticketing_signals
 from ....util.framework.blueprint import create_blueprint
-from ....util.framework.flash import flash_error, flash_notice, flash_success
+from ....util.framework.flash import flash_success
 from ....util.framework.templating import templated
-from ....util.views import permission_required, redirect_to, respond_no_content
+from ....util.views import permission_required, redirect_to
 
 from ...common.authorization.registry import permission_registry
 
@@ -131,61 +128,6 @@ def appoint_user(ticket_id):
     )
 
     return redirect_to('.view_ticket', ticket_id=ticket.id)
-
-
-# -------------------------------------------------------------------- #
-# checked-in flag
-
-
-@blueprint.route('/tickets/<uuid:ticket_id>/check_in_user', methods=['POST'])
-@permission_required(TicketingPermission.checkin)
-@respond_no_content
-def check_in_user(ticket_id):
-    """Check the user in."""
-    ticket = _get_ticket_or_404(ticket_id)
-
-    initiator_id = g.current_user.id
-
-    try:
-        event = ticket_user_checkin_service.check_in_user(
-            ticket.id, initiator_id
-        )
-    except ticket_exceptions.UserAccountDeleted:
-        flash_error(
-            'Das dem Ticket zugewiesene Benutzerkonto ist gelöscht worden. '
-            'Der Check-In ist nicht erlaubt.'
-        )
-        return
-    except ticket_exceptions.UserAccountSuspended:
-        flash_error(
-            'Das dem Ticket zugewiesene Benutzerkonto ist gesperrt. '
-            'Der Check-In ist nicht erlaubt.'
-        )
-        return
-
-    ticketing_signals.ticket_checked_in.send(None, event=event)
-
-    flash_success(f"Benutzer '{ticket.used_by.screen_name}' wurde eingecheckt.")
-
-    occupies_seat = ticket.occupied_seat_id is not None
-    if not occupies_seat:
-        flash_notice('Das Ticket belegt noch keinen Sitzplatz.', icon='warning')
-
-
-@blueprint.route(
-    '/tickets/<uuid:ticket_id>/revert_user_check_in', methods=['POST']
-)
-@permission_required(TicketingPermission.checkin)
-@respond_no_content
-def revert_user_check_in(ticket_id):
-    """Revert the user check-in state."""
-    ticket = _get_ticket_or_404(ticket_id)
-
-    initiator_id = g.current_user.id
-
-    ticket_user_checkin_service.revert_user_check_in(ticket.id, initiator_id)
-
-    flash_success('Der Check-In wurde rückgängig gemacht.')
 
 
 # -------------------------------------------------------------------- #
