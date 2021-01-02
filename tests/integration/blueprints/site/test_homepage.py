7a04bb7692b4838e0abe9ba586fc4748ed9cd5d4
Jochen Kupperschmidt <homework@nwsnet.de>
2021-01-02 13:19:32 +0100

    Test custom root path redirect

--- tests/integration/blueprints/site/test_homepage.py
+++ tests/integration/blueprints/site/test_homepage.py
@@ -15,3 +15,14 @@ def test_homepage(site_app, site):
     # By default, nothing is mounted on `/`, but at least check that
     # the application boots up and doesn't return a server error.
     assert response.status_code == 404
+    assert response.location is None
+
+
+def test_homepage_with_root_redirect(make_site_app, site):
+    site_app = make_site_app(ROOT_REDIRECT_TARGET='welcome')
+
+    with http_client(site_app) as client:
+        response = client.get('/')
+
+    assert response.status_code == 307
+    assert response.location == 'http://www.acmecon.test/welcome'
