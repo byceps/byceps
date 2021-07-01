78ab2d8e42806d817aab2a847aabdc75b99fce43
Jochen Kupperschmidt <homework@nwsnet.de>
2021-07-01 17:30:03 +0200

    Use `get_locales`, `get_current_user_locale` for CozyLAN locale selector

--- sites/cozylan/extension.py
+++ sites/cozylan/extension.py
@@ -6,14 +6,26 @@ from __future__ import annotations
 from typing import Any
 
 from flask import g
+from flask_babel import get_locale
 
 from byceps.services.seating import seat_service
 from byceps.services.ticketing import ticket_service
+from byceps.util.l10n import get_locales
+
+
+_LOCALE_ICONS_BY_CODE = {
+    'en': '🇬🇧',
+    'de': '🇩🇪',
+}
 
 
 def template_context_processor() -> dict[str, Any]:
     """Extend template context."""
-    context = {}
+    context = {
+        'current_locale': get_locale(),
+        'locales': get_locales(),
+        'locale_icons_by_code': _LOCALE_ICONS_BY_CODE,
+    }
 
     if g.party_id is not None:
         sale_stats = ticket_service.get_ticket_sale_stats(g.party_id)
