869e7eb7153848ed5530ab6e6c078e446516c354
Jochen Kupperschmidt <homework@nwsnet.de>
2021-07-02 04:56:44 +0200

    Add admin UI to create consent subject, remove script

---
+++ byceps/blueprints/admin/consent/forms.py
@@ -0,0 +1,20 @@
+"""
+byceps.blueprints.admin.consent.forms
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Copyright: 2006-2021 Jochen Kupperschmidt
+:License: Revised BSD (see `LICENSE` file for details)
+"""
+
+from flask_babel import lazy_gettext
+from wtforms import StringField
+from wtforms.validators import InputRequired, Length
+
+from ....util.l10n import LocalizedForm
+
+
+class SubjectCreateForm(LocalizedForm):
+    subject_name = StringField(lazy_gettext('Internal name'), [InputRequired(), Length(max=40)])
+    subject_title = StringField(lazy_gettext('Internal title'), [InputRequired(), Length(max=40)])
+    checkbox_label = StringField(lazy_gettext('Checkbox label'), [InputRequired(), Length(max=200)])
+    checkbox_link_target = StringField(lazy_gettext('Checkbox label link target'), [InputRequired(), Length(max=200)])
--- byceps/blueprints/admin/consent/views.py
+++ byceps/blueprints/admin/consent/views.py
@@ -6,13 +6,18 @@ byceps.blueprints.admin.consent.views
 :License: Revised BSD (see `LICENSE` file for details)
 """
 
+from flask import request
+from flask_babel import gettext
+
 from ....services.consent import subject_service
 from ....util.authorization import register_permission_enum
 from ....util.framework.blueprint import create_blueprint
+from ....util.framework.flash import flash_success
 from ....util.framework.templating import templated
-from ....util.views import permission_required
+from ....util.views import permission_required, redirect_to
 
 from .authorization import ConsentPermission
+from .forms import SubjectCreateForm
 
 
 blueprint = create_blueprint('consent_admin', __name__)
@@ -35,3 +40,41 @@ def index():
     return {
         'subjects_with_consent_counts': subjects_with_consent_counts,
     }
+
+
+@blueprint.get('/consents/create')
+@permission_required(ConsentPermission.administrate)
+@templated
+def subject_create_form(erroneous_form=None):
+    """Show form to create a consent subject."""
+    form = erroneous_form if erroneous_form else SubjectCreateForm()
+
+    return {
+        'form': form,
+    }
+
+
+@blueprint.post('/consents')
+@permission_required(ConsentPermission.administrate)
+def subject_create():
+    """Create a consent subject."""
+    form = SubjectCreateForm(request.form)
+    if not form.validate():
+        return subject_create_form(form)
+
+    subject_name = form.subject_name.data.strip()
+    subject_title = form.subject_title.data.strip()
+    checkbox_label = form.checkbox_label.data.strip()
+    checkbox_link_target = form.checkbox_link_target.data.strip()
+
+    subject = subject_service.create_subject(
+        subject_name, subject_title, checkbox_label, checkbox_link_target
+    )
+
+    flash_success(
+        gettext(
+            'Consent subject "%(title)s" has been created.', title=subject.title
+        )
+    )
+
+    return redirect_to('.index')
