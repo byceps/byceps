b92028c8b0861557171e1f54a9be1b45fb0dbc85
Jochen Kupperschmidt <homework@nwsnet.de>
2021-08-09 18:27:11 +0200

    Introduce seating area transfer object

--- byceps/services/seating/area_service.py
+++ byceps/services/seating/area_service.py
@@ -6,25 +6,27 @@ byceps.services.seating.area_service
 :License: Revised BSD (see `LICENSE` file for details)
 """
 
+from __future__ import annotations
 from typing import Optional
 
-from ...database import db, Pagination
+from ...database import db, paginate, Pagination
 from ...typing import PartyID
 
 from ..ticketing.dbmodels.ticket import Ticket as DbTicket
 
 from .dbmodels.area import Area as DbArea
 from .dbmodels.seat import Seat as DbSeat
+from .transfer.models import Area
 
 
-def create_area(party_id: PartyID, slug: str, title: str) -> DbArea:
+def create_area(party_id: PartyID, slug: str, title: str) -> Area:
     """Create an area."""
     area = DbArea(party_id, slug, title)
 
     db.session.add(area)
     db.session.commit()
 
-    return area
+    return _db_entity_to_area(area)
 
 
 def delete_area(area_id: str) -> None:
@@ -42,21 +44,26 @@ def count_areas_for_party(party_id: PartyID) -> int:
         .count()
 
 
-def find_area_for_party_by_slug(
-    party_id: PartyID, slug: str
-) -> Optional[DbArea]:
+def find_area_for_party_by_slug(party_id: PartyID, slug: str) -> Optional[Area]:
     """Return the area for that party with that slug, or `None` if not found."""
-    return DbArea.query \
+    area = DbArea.query \
         .for_party(party_id) \
         .filter_by(slug=slug) \
         .first()
 
+    if area is None:
+        return None
 
-def get_areas_for_party(party_id: PartyID) -> DbArea:
+    return _db_entity_to_area(area)
+
+
+def get_areas_for_party(party_id: PartyID) -> Area:
     """Return all areas for that party."""
-    return DbArea.query \
+    areas = DbArea.query \
         .for_party(party_id) \
         .all()
+
+    return [_db_entity_to_area(area) for area in areas]
 
 
 def get_areas_for_party_paginated(
@@ -75,11 +82,30 @@ def get_areas_for_party_paginated(
         .filter(DbSeat.area_id == area.id) \
         .scalar_subquery()
 
-    return db.session \
+    query = db.session \
         .query(
             area,
             subquery
         ) \
         .filter(area.party_id == party_id) \
-        .group_by(area.id) \
-        .paginate(page, per_page)
+        .group_by(area.id)
+
+    def item_mapper(
+        area_and_ticket_count: tuple[DbArea, int]
+    ) -> tuple[Area, int]:
+        area, ticket_count = area_and_ticket_count
+        return _db_entity_to_area(area), ticket_count
+
+    return paginate(query, page, per_page, item_mapper=item_mapper)
+
+
+def _db_entity_to_area(area: DbArea) -> Area:
+    return Area(
+        id=area.id,
+        party_id=area.party_id,
+        slug=area.slug,
+        title=area.title,
+        image_filename=area.image_filename,
+        image_width=area.image_width,
+        image_height=area.image_height,
+    )
--- byceps/services/seating/transfer/models.py
+++ byceps/services/seating/transfer/models.py
@@ -7,11 +7,24 @@ byceps.services.seating.transfer.models
 """
 
 from dataclasses import dataclass
-from typing import NewType
+from typing import NewType, Optional
 from uuid import UUID
+
+from ....typing import PartyID
 
 
 AreaID = NewType('AreaID', UUID)
+
+
+@dataclass(frozen=True)
+class Area:
+    id: AreaID
+    party_id: PartyID
+    slug: str
+    title: str
+    image_filename: Optional[str]
+    image_width: Optional[int]
+    image_height: Optional[int]
 
 
 SeatID = NewType('SeatID', UUID)
