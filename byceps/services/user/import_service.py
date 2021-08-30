b6b949b816f337fd700b8e362cf38e1a9e93dd1b
Jochen Kupperschmidt <homework@nwsnet.de>
2021-08-30 13:19:48 +0200

    Add basic user import

---
+++ byceps/services/user/import_service.py
@@ -0,0 +1,54 @@
+"""
+byceps.services.user.import_service
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Copyright: 2006-2021 Jochen Kupperschmidt
+:License: Revised BSD (see `LICENSE` file for details)
+"""
+
+from __future__ import annotations
+import json
+from io import TextIOBase
+import secrets
+from typing import Iterator
+
+from marshmallow import fields, Schema, ValidationError
+
+from . import creation_service
+from .transfer.models import User
+
+
+class UserImportSchema(Schema):
+    screen_name = fields.Str(required=True)
+    email_address = fields.Str(required=False)
+    first_names = fields.Str(required=False)
+    last_name = fields.Str(required=False)
+
+
+def parse_lines(lines: TextIOBase) -> Iterator[str]:
+    for line in lines:
+        yield line.strip()
+
+
+def parse_user_json(json_data: str) -> dict[str, str]:
+    data_dict = json.loads(json_data)
+
+    schema = UserImportSchema()
+    try:
+        return schema.load(data_dict)
+    except ValidationError as e:
+        raise Exception(str(e.normalized_messages()))
+
+
+def create_user(user_dict) -> User:
+    password = secrets.token_urlsafe(24)
+
+    user, _ = creation_service.create_basic_user(
+        user_dict['screen_name'],
+        user_dict.get('email_address'),
+        password,
+        first_names=user_dict.get('first_names'),
+        last_name=user_dict.get('last_name'),
+    )
+
+    return user
---
+++ scripts/import_users.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+"""Import users from JSON lines.
+
+:Copyright: 2006-2021 Jochen Kupperschmidt
+:License: Revised BSD (see `LICENSE` file for details)
+"""
+
+import click
+
+from byceps.services.user import import_service
+
+from _util import call_with_app_context
+
+
+@click.command()
+@click.argument('data_file', type=click.File())
+def execute(data_file) -> None:
+    lines = import_service.parse_lines(data_file)
+    for line_number, line in enumerate(lines, start=1):
+        try:
+            user_dict = import_service.parse_user_json(line)
+            user = import_service.create_user(user_dict)
+            click.secho(
+                f'[line {line_number}] Imported user {user.screen_name}.',
+                fg='green',
+            )
+        except Exception as e:
+            click.secho(
+                f'[line {line_number}] Cound not import user: {e}', fg='red'
+            )
+
+
+if __name__ == '__main__':
+    call_with_app_context(execute)
