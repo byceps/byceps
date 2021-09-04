4f64b77154b838b24ac0eff8f6291c6025f32344
Jochen Kupperschmidt <homework@nwsnet.de>
2021-09-04 18:25:55 +0200

    Fix user creation with site ID for admins

--- byceps/blueprints/admin/user/views.py
+++ byceps/blueprints/admin/user/views.py
@@ -220,7 +220,7 @@ def create_account():
 
     if site_for_email:
         user_creation_service.request_email_address_confirmation(
-            user, email_address, site_id_for_email.id
+            user, email_address, site_for_email.id
         )
         flash_success(
             gettext('An email has been sent to the corresponding address.'),
