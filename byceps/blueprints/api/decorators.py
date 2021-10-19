f844f88a89d5a0196d21ad5990598a9cfc9c2389
Jochen Kupperschmidt <homework@nwsnet.de>
2021-10-19 05:38:26 +0200

    Remove legacy API token check

--- byceps/blueprints/api/decorators.py
+++ byceps/blueprints/api/decorators.py
@@ -6,13 +6,10 @@ byceps.blueprints.api.decorators
 :License: Revised BSD (see `LICENSE` file for details)
 """
 
-import base64
-import binascii
 from functools import wraps
-import hmac
 from typing import Optional
 
-from flask import abort, current_app, request
+from flask import abort, request
 
 from ...services.authentication.api import service as api_service
 
@@ -34,9 +31,8 @@ def _has_valid_api_token() -> bool:
     if request_token is None:
         return False
 
-    return _matches_legacy_token(request_token) or _matches_api_token(
-        request_token
-    )
+    api_token = api_service.find_api_token_by_token(request_token)
+    return api_token is not None
 
 
 def _extract_token_from_request() -> Optional[str]:
@@ -45,27 +41,3 @@ def _extract_token_from_request() -> Optional[str]:
         return None
 
     return header_value.replace('Bearer ', '', 1)
-
-
-def _matches_legacy_token(request_token: str) -> bool:
-    configured_token = current_app.config.get('API_TOKEN')
-    if configured_token is None:
-        return False
-
-    token_base64decoded = _decode_base64(request_token)
-    if token_base64decoded is None:
-        return False
-
-    return hmac.compare_digest(token_base64decoded, configured_token.encode())
-
-
-def _decode_base64(value: str) -> Optional[bytes]:
-    try:
-        return base64.b64decode(value)
-    except (TypeError, ValueError, binascii.Error):
-        return None
-
-
-def _matches_api_token(request_token: str) -> bool:
-    api_token = api_service.find_api_token_by_token(request_token)
-    return api_token is not None
