594d82b1416b2877500b904d2a19ce6212166f06
Jochen Kupperschmidt <homework@nwsnet.de>
2021-12-02 03:27:44 +0100

    Use Flask-Babel's number formatting template filter

--- byceps/util/templatefunctions.py
+++ byceps/util/templatefunctions.py
@@ -8,21 +8,15 @@ Provide and register custom template global functions.
 :License: Revised BSD (see `LICENSE` file for details)
 """
 
-import babel
-from flask_babel import get_locale, npgettext, pgettext
+from flask_babel import npgettext, pgettext
 
 
 def register(app):
     """Make functions available as global functions in templates."""
     functions = [
-        (_format_number, 'format_number'),
         (npgettext, 'npgettext'),
         (pgettext, 'pgettext'),
     ]
 
     for f, name in functions:
         app.add_template_global(f, name)
-
-
-def _format_number(*args) -> str:
-    return babel.numbers.format_decimal(*args, locale=get_locale())
