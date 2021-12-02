cd41fdbdb53008c9701213d4f223bb8df0514ecb
Jochen Kupperschmidt <homework@nwsnet.de>
2021-12-03 00:25:47 +0100

    Remove unused custom functions `local_tz_to_utc`, `utc_to_local_tz`

--- byceps/util/datetime/timezone.py
+++ byceps/util/datetime/timezone.py
@@ -8,28 +8,7 @@ Timezone helpers
 :License: Revised BSD (see `LICENSE` file for details)
 """
 
-from datetime import datetime
-
 from flask import current_app
-import pendulum
-
-
-
-def local_tz_to_utc(dt: datetime):
-    """Convert date/time object from configured default local time to UTC."""
-    tz_str = get_timezone_string()
-
-    return (pendulum.instance(dt)
-        .set(tz=tz_str)
-        .in_tz(pendulum.UTC)
-        # Keep SQLAlchemy from converting it to another zone.
-        .replace(tzinfo=None))
-
-
-def utc_to_local_tz(dt: datetime) -> datetime:
-    """Convert naive date/time object from UTC to configured time zone."""
-    tz_str = get_timezone_string()
-    return pendulum.instance(dt).in_tz(tz_str)
 
 
 def get_timezone_string() -> str:
