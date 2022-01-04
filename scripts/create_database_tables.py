3ad86a5475f688bdee4c4d291e8660a449b8443b
Jochen Kupperschmidt <homework@nwsnet.de>
2022-01-04 01:50:11 +0100

    Load database model modules before creating tables
    This prevents tables from being created because their corresponding
    modules are unknown to the app - especially with `APP_MODE=base` (which
    is the implicit default).

--- scripts/create_database_tables.py
+++ scripts/create_database_tables.py
@@ -8,6 +8,10 @@ Existing tables will be ignored, and those not existing will be created.
 :License: Revised BSD (see `LICENSE` file for details)
 """
 
+from importlib import import_module
+from pathlib import Path
+from typing import Iterator
+
 import click
 
 from byceps.database import db
@@ -19,9 +23,34 @@ from _util import call_with_app_context
 def execute() -> None:
     click.echo('Creating database tables ... ', nl=False)
 
+    _load_dbmodels()
+
     db.create_all()
 
     click.secho('done.', fg='green')
+
+
+def _load_dbmodels() -> None:
+    paths = _collect_dbmodel_paths()
+    module_names = map(_get_module_name_for_path, paths)
+    for module_name in module_names:
+        import_module(module_name)
+
+
+def _collect_dbmodel_paths() -> Iterator[Path]:
+    path = Path('byceps') / 'services'
+
+    yield from path.glob('**/dbmodels.py')
+    yield from path.glob('**/dbmodels/*.py')
+
+
+def _get_module_name_for_path(path: Path) -> str:
+    path = path.with_suffix('')
+
+    if path.stem == '__init__':
+        path = path.parent
+
+    return str(path).replace('/', '.')
 
 
 if __name__ == '__main__':
