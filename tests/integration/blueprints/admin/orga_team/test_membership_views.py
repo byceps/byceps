abe288ebab37b0661e097e2ceec831f43b29ebbd
Jochen Kupperschmidt <homework@nwsnet.de>
2022-01-24 03:47:03 +0100

    Do not delete orga flag after test

--- tests/integration/blueprints/admin/orga_team/test_membership_views.py
+++ tests/integration/blueprints/admin/orga_team/test_membership_views.py
@@ -48,7 +48,6 @@ def test_membership_create(
 
     # Clean up.
     orga_team_service.delete_membership(membership.id)
-    orga_service.remove_orga_flag(party.brand_id, user.id, user.id)
 
 
 def test_membership_update_form(
