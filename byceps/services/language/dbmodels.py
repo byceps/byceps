643c8896b23cc1d008ce1e2a278d4379fb3b9b08
Jochen Kupperschmidt <homework@nwsnet.de>
2022-05-10 23:54:58 +0200

    Fix name of languages table

--- byceps/services/language/dbmodels.py
+++ byceps/services/language/dbmodels.py
@@ -15,7 +15,7 @@ class Language(db.Model):
     The code can be just `en` or `de`, but also `en-gb` or `de-de`.
     """
 
-    __tablename__ = 'locales'
+    __tablename__ = 'languages'
 
     code = db.Column(db.UnicodeText, primary_key=True)
 
