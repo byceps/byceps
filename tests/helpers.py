623f18ee666787692f3bfe5f68e78158e5fcd9b5
Jochen Kupperschmidt <homework@nwsnet.de>
2022-10-02 02:57:58 +0200

    Use ID from created role

--- tests/helpers.py
+++ tests/helpers.py
@@ -166,7 +166,7 @@ def create_role_with_permissions_assigned(
     role = authz_service.create_role(role_id, role_id, ignore_if_exists=True)
 
     for permission_id in permission_ids:
-        authz_service.assign_permission_to_role(permission_id, role_id)
+        authz_service.assign_permission_to_role(permission_id, role.id)
 
 
 def create_party(
