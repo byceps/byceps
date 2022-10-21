d40e254ac9027f37321bf2b337693f0781aab5cc
Jochen Kupperschmidt <homework@nwsnet.de>
2022-10-21 04:25:25 +0200

    Fix set comprehension

--- byceps/blueprints/admin/shop/shipping/views.py
+++ byceps/blueprints/admin/shop/shipping/views.py
@@ -32,7 +32,7 @@ def view_for_shop(shop_id):
     articles_to_ship = shipping_service.get_articles_to_ship(shop.id)
     article_ids = {ats.article_id for ats in articles_to_ship}
     articles = article_service.get_articles(article_ids)
-    articles_by_id = {article.id: article in articles}
+    articles_by_id = {article.id: article for article in articles}
 
     return {
         'shop': shop,
