e69b4f3a98db39d060066afdb796867d26dd4897
Jochen Kupperschmidt <homework@nwsnet.de>
2022-12-11 20:41:32 +0100

    Use what `click.Path` has to offer

--- byceps/cli/commands/import_roles.py
+++ byceps/cli/commands/import_roles.py
@@ -4,6 +4,8 @@
 :License: Revised BSD (see `LICENSE` file for details)
 """
 
+from pathlib import Path
+
 import click
 from flask.cli import with_appcontext
 
@@ -11,9 +13,11 @@ from ...services.authorization import impex_service
 
 
 @click.command()
-@click.argument('data_file', type=click.File())
+@click.argument(
+    'data_file', type=click.Path(exists=True, dir_okay=False, path_type=Path)
+)
 @with_appcontext
-def import_roles(data_file) -> None:
+def import_roles(data_file: Path) -> None:
     """Import authorization roles."""
     click.echo('Importing roles ... ', nl=False)
     role_count = impex_service.import_from_file(data_file)
--- byceps/cli/commands/import_users.py
+++ byceps/cli/commands/import_users.py
@@ -4,6 +4,8 @@
 :License: Revised BSD (see `LICENSE` file for details)
 """
 
+from pathlib import Path
+
 import click
 from flask.cli import with_appcontext
 
@@ -11,20 +13,23 @@ from ...services.user import user_import_service
 
 
 @click.command()
-@click.argument('data_file', type=click.File())
+@click.argument(
+    'data_file', type=click.Path(exists=True, dir_okay=False, path_type=Path)
+)
 @with_appcontext
-def import_users(data_file) -> None:
+def import_users(data_file: Path) -> None:
     """Import user accounts."""
-    lines = user_import_service.parse_lines(data_file)
-    for line_number, line in enumerate(lines, start=1):
-        try:
-            user_to_import = user_import_service.parse_user_json(line)
-            user = user_import_service.import_user(user_to_import)
-            click.secho(
-                f'[line {line_number}] Imported user {user.screen_name}.',
-                fg='green',
-            )
-        except Exception as e:
-            click.secho(
-                f'[line {line_number}] Could not import user: {e}', fg='red'
-            )
+    with data_file.open() as f:
+        lines = user_import_service.parse_lines(f)
+        for line_number, line in enumerate(lines, start=1):
+            try:
+                user_to_import = user_import_service.parse_user_json(line)
+                user = user_import_service.import_user(user_to_import)
+                click.secho(
+                    f'[line {line_number}] Imported user {user.screen_name}.',
+                    fg='green',
+                )
+            except Exception as e:
+                click.secho(
+                    f'[line {line_number}] Could not import user: {e}', fg='red'
+                )
