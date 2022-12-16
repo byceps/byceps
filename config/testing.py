f0afa81599c68ccc13202894c8d4f78281c75f13
Jochen Kupperschmidt <homework@nwsnet.de>
2022-12-16 11:29:53 +0100

    Change secret key for testing

--- config/testing.py
+++ config/testing.py
@@ -1,6 +1,6 @@
 # a configuration file to be used in tests
 
-SECRET_KEY = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+SECRET_KEY = 'secret-key-for-testing-ONLY'
 TESTING = True
 
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://byceps_test:test@127.0.0.1/byceps_test'
