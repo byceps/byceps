81f82068f3acc68ec2b3de116e01f4df6d7f4209
Jochen Kupperschmidt <homework@nwsnet.de>
2023-02-09 16:34:09 +0100

    Do not explicitly add database-related locals to Flask shell
    They are now automatically provided by Flask-SQLAlchemy as of v3.0.0.

--- app.py
+++ app.py
@@ -9,19 +9,11 @@ application instance
 from warnings import warn
 
 from byceps.application import create_app
-from byceps.database import db
-from byceps.services.brand.dbmodels.brand import DbBrand
-from byceps.services.party.dbmodels.party import DbParty
-from byceps.services.shop.article.dbmodels.article import DbArticle
-from byceps.services.shop.order.dbmodels.line_item import DbLineItem
-from byceps.services.shop.order.dbmodels.order import DbOrder
 from byceps.services.shop.order.models.order import (
     PaymentState as OrderPaymentState,
 )
 from byceps.services.shop.order import order_service
 from byceps.services.ticketing.ticket_service import find_ticket_by_code
-from byceps.services.user.dbmodels.detail import DbUserDetail
-from byceps.services.user.dbmodels.user import DbUser
 from byceps.services.user import user_service
 
 
@@ -46,16 +38,8 @@ def extend_shell_context():
     """Provide common objects to make available in the application shell."""
     return {
         'app': app,
-        'db': db,
-        'DbArticle': DbArticle,
-        'DbBrand': DbBrand,
         'find_order_by_order_number': order_service.find_order_by_order_number,
-        'DbOrder': DbOrder,
-        'DbLineItem': DbLineItem,
         'OrderPaymentState': OrderPaymentState,
-        'DbParty': DbParty,
         'find_ticket_by_code': find_ticket_by_code,
-        'DbUser': DbUser,
-        'DbUserDetail': DbUserDetail,
         'find_db_user_by_screen_name': user_service.find_db_user_by_screen_name,
     }
