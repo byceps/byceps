3976af639454ea0782393dd7a927f2963ec02b4c
Jochen Kupperschmidt <homework@nwsnet.de>
2023-03-09 00:26:30 +0100

    Ensure result is actual user object

--- tests/integration/services/authentication/test_authenticate.py
+++ tests/integration/services/authentication/test_authenticate.py
@@ -57,23 +57,19 @@ def test_active_user_with_screen_name_and_correct_password_is_accepted(
 ):
     user = create_user(make_user)
 
-    authenticated_user = authn_service.authenticate(
-        user.screen_name, CORRECT_PASSWORD
-    )
+    actual = authn_service.authenticate(user.screen_name, CORRECT_PASSWORD)
 
-    assert authenticated_user is not None
+    assert actual == user
 
 
 def test_active_user_with_email_address_and_correct_password_is_accepted(
     make_user,
 ):
-    create_user(make_user, email_address='ehrenmann@mail.test')
+    user = create_user(make_user, email_address='ehrenmann@mail.test')
 
-    authenticated_user = authn_service.authenticate(
-        'ehrenmann@mail.test', CORRECT_PASSWORD
-    )
+    actual = authn_service.authenticate('ehrenmann@mail.test', CORRECT_PASSWORD)
 
-    assert authenticated_user is not None
+    assert actual == user
 
 
 def create_user(make_user, **kwargs):
