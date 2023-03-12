64619734bb188a81cee052636126638c8b61028d
Jochen Kupperschmidt <homework@nwsnet.de>
2023-03-12 06:35:33 +0100

    Extract functions

--- byceps/cli/commands/import_seats.py
+++ byceps/cli/commands/import_seats.py
@@ -5,13 +5,18 @@
 """
 
 from pathlib import Path
+from typing import Iterator
 
 import click
 from flask.cli import with_appcontext
 
+from ...services.seating.models import SeatingAreaID
+from ...services.seating.seat_import_service import SeatToImport
 from ...services.seating import seat_import_service, seating_area_service
+from ...services.ticketing.models.ticket import TicketCategoryID
 from ...services.ticketing import ticket_category_service
 from ...typing import PartyID
+from ...util.result import Result
 
 
 @click.command()
@@ -22,35 +27,50 @@ from ...typing import PartyID
 @with_appcontext
 def import_seats(party_id: PartyID, data_file: Path) -> None:
     """Import seats."""
+    area_ids_by_title = get_area_ids_by_title(party_id)
+    category_ids_by_title = get_category_ids_by_title(party_id)
+
+    line_numbers_and_parse_results = parse_seats_json(data_file)
+    for line_number, parse_result in line_numbers_and_parse_results:
+        if parse_result.is_err():
+            error_str = parse_result.unwrap_err()
+            click.secho(
+                f'[line {line_number}] Could not parse seat: {error_str}',
+                fg='red',
+            )
+            continue
+
+        seat_to_import = parse_result.unwrap()
+
+        seat = seat_import_service.import_seat(
+            seat_to_import, area_ids_by_title, category_ids_by_title
+        )
+
+        click.secho(
+            f'[line {line_number}] Imported seat '
+            f'(area="{seat_to_import.area_title}", x={seat.coord_x}, y={seat.coord_y}, category="{seat_to_import.category_title}").',
+            fg='green',
+        )
+
+
+def get_area_ids_by_title(party_id: PartyID) -> dict[str, SeatingAreaID]:
+    """Get the party's seating areas as a mapping from title to ID."""
     areas = seating_area_service.get_areas_for_party(party_id)
-    area_ids_by_title = {area.title: area.id for area in areas}
+    return {area.title: area.id for area in areas}
 
+
+def get_category_ids_by_title(party_id: PartyID) -> dict[str, TicketCategoryID]:
+    """Get the party's ticket categories as a mapping from title to ID."""
     categories = ticket_category_service.get_categories_for_party(party_id)
-    category_ids_by_title = {
-        category.title: category.id for category in categories
-    }
+    return {category.title: category.id for category in categories}
 
+
+def parse_seats_json(
+    data_file: Path,
+) -> Iterator[tuple[int, Result[SeatToImport, str]]]:
+    """Parse one seat per line."""
     with data_file.open() as f:
         lines = seat_import_service.parse_lines(f)
         for line_number, line in enumerate(lines, start=1):
-            import_result = seat_import_service.parse_seat_json(line)
-
-            if import_result.is_err():
-                error_str = import_result.unwrap_err()
-                click.secho(
-                    f'[line {line_number}] Could not import seat: {error_str}',
-                    fg='red',
-                )
-                continue
-
-            seat_to_import = import_result.unwrap()
-
-            seat = seat_import_service.import_seat(
-                seat_to_import, area_ids_by_title, category_ids_by_title
-            )
-
-            click.secho(
-                f'[line {line_number}] Imported seat '
-                f'(area="{seat_to_import.area_title}", x={seat.coord_x}, y={seat.coord_y}, category="{seat_to_import.category_title}").',
-                fg='green',
-            )
+            parse_result = seat_import_service.parse_seat_json(line)
+            yield line_number, parse_result
