247e4ee822870b8eaa161fa9be91332660a6d445
Jochen Kupperschmidt <homework@nwsnet.de>
2023-03-14 03:20:37 +0100

    Require URL path in page admin form to begin with slash

--- byceps/blueprints/admin/page/forms.py
+++ byceps/blueprints/admin/page/forms.py
@@ -6,9 +6,9 @@ byceps.blueprints.admin.page.forms
 :License: Revised BSD (see `LICENSE` file for details)
 """
 
-from flask_babel import lazy_gettext
+from flask_babel import gettext, lazy_gettext
 from wtforms import SelectField, StringField, TextAreaField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, ValidationError
 
 from ....services.language import language_service
 from ....services.site.models import SiteID
@@ -32,6 +32,13 @@ class _PageBaseForm(LocalizedForm):
         choices = [(language.code, language.code) for language in languages]
         choices.sort()
         self.language_code.choices = choices
+
+    @staticmethod
+    def validate_url_path(form, field):
+        if not field.data.startswith('/'):
+            raise ValidationError(
+                gettext('URL path must begin with a forward slash.')
+            )
 
 
 class CreateForm(_PageBaseForm):
