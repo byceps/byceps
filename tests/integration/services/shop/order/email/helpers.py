fa23854deb28195fd87f8ae426ff7d58f0808526
Jochen Kupperschmidt <homework@nwsnet.de>
2023-06-09 19:57:35 +0200

    Remove non-exemplary shop order email tests
    The recently added unit tests cover the basic assembly; that has to suffice.

--- tests/integration/services/shop/order/email/helpers.py
+++ tests/integration/services/shop/order/email/helpers.py
@@ -3,12 +3,8 @@
 :License: Revised BSD (see `LICENSE` file for details)
 """
 
-from moneyed import EUR
-
 from byceps.services.authentication.session import authn_session_service
 from byceps.services.authentication.session.models import CurrentUser
-from byceps.services.shop.cart.models import Cart
-from byceps.services.shop.order import order_checkout_service
 from byceps.services.user.models.user import User
 
 
@@ -16,19 +12,3 @@ def get_current_user_for_user(user: User, locale: str) -> CurrentUser:
     return authn_session_service.get_authenticated_current_user(
         user, locale=locale, permissions=frozenset()
     )
-
-
-def place_order_with_items(
-    storefront_id, orderer, created_at=None, items_with_quantity=None
-):
-    cart = Cart(EUR)
-
-    if items_with_quantity is not None:
-        for article, quantity in items_with_quantity:
-            cart.add_item(article, quantity)
-
-    order, _ = order_checkout_service.place_order(
-        storefront_id, orderer, cart, created_at=created_at
-    ).unwrap()
-
-    return order
