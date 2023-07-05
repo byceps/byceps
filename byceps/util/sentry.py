bb949daff325f8070ea3e17acb28d01675cba577
Jochen Kupperschmidt <homework@nwsnet.de>
2023-07-05 09:15:45 +0200

    Extract Sentry SDK initialization

--- byceps/util/sentry.py
+++ byceps/util/sentry.py
@@ -17,22 +17,7 @@ def configure_sentry_for_webapp(dsn: str, environment: str, app: Flask) -> None:
     """Initialize and configure the Sentry SDK for the Flask-based web
     application (both in 'admin' and 'site' modes).
     """
-    import sentry_sdk
-    from sentry_sdk.integrations.flask import FlaskIntegration
-    from sentry_sdk.integrations.redis import RedisIntegration
-    from sentry_sdk.integrations.rq import RqIntegration
-    from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
-
-    sentry_sdk.init(
-        dsn=dsn,
-        environment=environment,
-        integrations=[
-            FlaskIntegration(),
-            RedisIntegration(),
-            RqIntegration(),
-            SqlalchemyIntegration(),
-        ],
-    )
+    sentry_sdk = _init_sentry_sdk(dsn, environment)
 
     app_mode = app.config.get('APP_MODE')
     sentry_sdk.set_tag('app_mode', app_mode)
@@ -43,6 +28,12 @@ def configure_sentry_for_webapp(dsn: str, environment: str, app: Flask) -> None:
 
 def configure_sentry_for_worker(dsn: str, environment: str) -> None:
     """Initialize and configure the Sentry SDK for the RQ worker."""
+    sentry_sdk = _init_sentry_sdk(dsn, environment)
+
+    sentry_sdk.set_tag('app_mode', 'worker')
+
+
+def _init_sentry_sdk(dsn: str, environment: str):
     import sentry_sdk
     from sentry_sdk.integrations.flask import FlaskIntegration
     from sentry_sdk.integrations.redis import RedisIntegration
@@ -60,4 +51,4 @@ def configure_sentry_for_worker(dsn: str, environment: str) -> None:
         ],
     )
 
-    sentry_sdk.set_tag('app_mode', 'worker')
+    return sentry_sdk
