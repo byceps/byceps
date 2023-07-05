9bd90b7cbe535938bd473d23405a4b62b37eaf62
Jochen Kupperschmidt <homework@nwsnet.de>
2023-07-05 09:21:54 +0200

    Separate Sentry webapp initialization function for admin, site apps

--- byceps/util/sentry.py
+++ byceps/util/sentry.py
@@ -18,17 +18,25 @@ from sentry_sdk.integrations.rq import RqIntegration
 from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 
 
-def configure_sentry_for_webapp(dsn: str, environment: str, app: Flask) -> None:
-    """Initialize and configure the Sentry SDK for the Flask-based web
-    application (both in 'admin' and 'site' modes).
+def configure_sentry_for_admin_app(dsn: str, environment: str) -> None:
+    """Initialize and configure the Sentry SDK for the Flask-based admin
+    web application.
     """
     sentry_sdk = _init_sentry_sdk(dsn, environment)
 
-    app_mode = app.config.get('APP_MODE')
-    sentry_sdk.set_tag('app_mode', app_mode)
+    sentry_sdk.set_tag('app_mode', 'admin')
 
-    if app_mode == 'site':
-        sentry_sdk.set_tag('site_id', app.config.get('SITE_ID'))
+
+def configure_sentry_for_site_app(
+    dsn: str, environment: str, app: Flask
+) -> None:
+    """Initialize and configure the Sentry SDK for the Flask-based site
+    web application.
+    """
+    sentry_sdk = _init_sentry_sdk(dsn, environment)
+
+    sentry_sdk.set_tag('app_mode', 'site')
+    sentry_sdk.set_tag('site_id', app.config.get('SITE_ID'))
 
 
 def configure_sentry_for_worker(dsn: str, environment: str) -> None:
