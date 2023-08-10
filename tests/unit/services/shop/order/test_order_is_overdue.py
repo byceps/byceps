d06143c3a24ce82b2ab0d6707a39974d13667275
Jochen Kupperschmidt <homework@nwsnet.de>
2023-08-10 18:42:08 +0200

    Keep original test data formatting

--- tests/unit/services/shop/order/test_order_is_overdue.py
+++ tests/unit/services/shop/order/test_order_is_overdue.py
@@ -15,20 +15,12 @@ from byceps.services.shop.order.models.order import PaymentState
 @pytest.mark.parametrize(
     ('checked_at', 'payment_state', 'expected'),
     [
-        (datetime(2021, 6, 25, 11, 59, 59), PaymentState.open, False),
-        (datetime(2021, 6, 25, 12, 0, 0), PaymentState.open, True),
-        (
-            datetime(2021, 6, 25, 12, 0, 0),
-            PaymentState.canceled_before_paid,
-            False,
-        ),
-        (datetime(2021, 6, 25, 12, 0, 0), PaymentState.paid, False),
-        (
-            datetime(2021, 6, 25, 12, 0, 0),
-            PaymentState.canceled_after_paid,
-            False,
-        ),
-        (datetime(2021, 6, 25, 12, 0, 1), PaymentState.open, True),
+        (datetime(2021, 6, 25, 11, 59, 59), PaymentState.open,                 False),
+        (datetime(2021, 6, 25, 12,  0,  0), PaymentState.open,                 True ),
+        (datetime(2021, 6, 25, 12,  0,  0), PaymentState.canceled_before_paid, False),
+        (datetime(2021, 6, 25, 12,  0,  0), PaymentState.paid,                 False),
+        (datetime(2021, 6, 25, 12,  0,  0), PaymentState.canceled_after_paid,  False),
+        (datetime(2021, 6, 25, 12,  0,  1), PaymentState.open,                 True ),
     ],
 )
 def test_is_overdue(
