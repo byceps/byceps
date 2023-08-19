81923060780a7ee19dd3a75e2089cd35f56c6092
Jochen Kupperschmidt <homework@nwsnet.de>
2023-08-19 13:04:54 +0200

    Remove check-in event, log entry assertions covered in unit tests from integration tests

--- tests/integration/services/ticketing/test_user_check_in.py
+++ tests/integration/services/ticketing/test_user_check_in.py
@@ -6,10 +6,8 @@
 import pytest
 
 from byceps.database import db
-from byceps.events.ticketing import TicketCheckedInEvent
 from byceps.services.ticketing import (
     ticket_creation_service,
-    ticket_log_service,
     ticket_service,
     ticket_user_checkin_service,
 )
@@ -33,11 +31,6 @@ def test_check_in_user(admin_app, party, ticket, ticketing_admin, make_user):
 
     assert not ticket_before.user_checked_in
 
-    log_entries_before = ticket_log_service.get_entries_for_ticket(
-        ticket_before.id
-    )
-    assert len(log_entries_before) == 0
-
     # -------------------------------- #
 
     ticket_id = ticket_before.id
@@ -47,36 +40,14 @@ def test_check_in_user(admin_app, party, ticket, ticketing_admin, make_user):
     )
     assert check_in_result.is_ok()
 
-    event = check_in_result.unwrap()
+    check_in_result.unwrap()
 
     # -------------------------------- #
 
     ticket_after = ticket_service.get_ticket(ticket_id)
     assert ticket_after.user_checked_in
 
-    assert event.__class__ is TicketCheckedInEvent
-    assert event.occurred_at is not None
-    assert event.initiator_id == initiator.id
-    assert event.initiator_screen_name == initiator.screen_name
-    assert event.ticket_id == ticket.id
-    assert event.ticket_code == ticket.code
-    assert event.occupied_seat_id is None
-    assert event.user_id == ticket_user.id
-    assert event.user_screen_name == ticket_user.screen_name
-
     check_in = ticket_user_checkin_service.find_check_in_for_ticket(ticket_id)
     assert check_in is not None
     assert check_in.occurred_at is not None
     assert check_in.initiator_id == initiator.id
-
-    log_entries_after = ticket_log_service.get_entries_for_ticket(
-        ticket_after.id
-    )
-    assert len(log_entries_after) == 1
-
-    ticket_checked_in_log_entry = log_entries_after[0]
-    assert ticket_checked_in_log_entry.event_type == 'user-checked-in'
-    assert ticket_checked_in_log_entry.data == {
-        'checked_in_user_id': str(ticket_user.id),
-        'initiator_id': str(initiator.id),
-    }
