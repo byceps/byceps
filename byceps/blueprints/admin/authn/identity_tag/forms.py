1ca8d111b0ecdfb857fa4f3ea7a50e4886650049
Jochen Kupperschmidt <homework@nwsnet.de>
2023-10-05 01:35:15 +0200

    Add admin UI to add identity tags

---
+++ byceps/blueprints/admin/authn/identity_tag/forms.py
@@ -0,0 +1,50 @@
+"""
+byceps.blueprints.admin.authn.identity_tag.forms
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Copyright: 2022-2023 Jochen Kupperschmidt
+:License: Revised BSD (see `LICENSE` file for details)
+"""
+
+from flask_babel import lazy_gettext
+from wtforms import BooleanField, StringField
+from wtforms.validators import InputRequired, Optional, ValidationError
+
+from byceps.services.user import user_service
+from byceps.services.authn.identity_tag import authn_identity_tag_service
+from byceps.util.l10n import LocalizedForm
+
+
+def validate_user_screen_name(form, field):
+    screen_name = field.data.strip()
+
+    user = user_service.find_user_by_screen_name(
+        screen_name, case_insensitive=True
+    )
+
+    if user is None:
+        raise ValidationError(lazy_gettext('Unknown username'))
+
+    field.data = user
+
+
+class CreateForm(LocalizedForm):
+    identifier = StringField(lazy_gettext('Identifier'), [InputRequired()])
+    user = StringField(
+        lazy_gettext('Username'),
+        [InputRequired(), validate_user_screen_name],
+    )
+    note = StringField(lazy_gettext('Note'), [Optional()])
+    suspended = BooleanField(lazy_gettext('suspended'))
+
+    @staticmethod
+    def validate_identifier(form, field):
+        identifier = field.data.strip()
+
+        tag = authn_identity_tag_service.find_tag_by_identifier(identifier)
+        if tag:
+            raise ValidationError(
+                lazy_gettext(
+                    'This value is not available. Please choose another.'
+                )
+            )
--- byceps/blueprints/admin/authn/identity_tag/views.py
+++ byceps/blueprints/admin/authn/identity_tag/views.py
@@ -6,10 +6,16 @@ byceps.blueprints.admin.authn.identity_tag.views
 :License: Revised BSD (see `LICENSE` file for details)
 """
 
+from flask import g, request
+from flask_babel import gettext
+
 from byceps.services.authn.identity_tag import authn_identity_tag_service
 from byceps.util.framework.blueprint import create_blueprint
+from byceps.util.framework.flash import flash_success
 from byceps.util.framework.templating import templated
-from byceps.util.views import permission_required
+from byceps.util.views import permission_required, redirect_to
+
+from .forms import CreateForm
 
 
 blueprint = create_blueprint('authn_identity_tag_admin', __name__)
@@ -23,3 +29,38 @@ def index():
     tags = authn_identity_tag_service.get_all_tags()
 
     return {'tags': tags}
+
+
+@blueprint.get('/tags/create')
+@permission_required('authn_identity_tag.administrate')
+@templated
+def create_form(erroneous_form=None):
+    """Show form to add an identity tag."""
+    form = erroneous_form if erroneous_form else CreateForm()
+
+    return {
+        'form': form,
+    }
+
+
+@blueprint.post('/tags')
+@permission_required('authn_identity_tag.administrate')
+def create():
+    """Add an identity tag."""
+    form = CreateForm(request.form)
+    if not form.validate():
+        return create_form(form)
+
+    creator = g.user
+    identifier = form.identifier.data.strip()
+    user = form.user.data
+    note = form.note.data.strip() or None
+    suspended = form.suspended.data
+
+    authn_identity_tag_service.create_tag(
+        creator, identifier, user, note=note, suspended=suspended
+    )
+
+    flash_success(gettext('The object has been created.'))
+
+    return redirect_to('.index')
