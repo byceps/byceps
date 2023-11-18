86044538662c004bd66efe950ac96f93927d2f6f
Jochen Kupperschmidt <homework@nwsnet.de>
2023-11-18 04:46:32 +0100

    Render specific error messages in example shop email admin UI

--- byceps/blueprints/admin/shop/email/views.py
+++ byceps/blueprints/admin/shop/email/views.py
@@ -8,7 +8,7 @@ byceps.blueprints.admin.shop.email.views
 
 from __future__ import annotations
 
-from flask import abort, current_app, g
+from flask import abort, g
 
 from byceps.services.brand import brand_service
 from byceps.services.brand.models import Brand
@@ -19,6 +19,7 @@ from byceps.services.shop.shop import shop_service
 from byceps.services.shop.shop.models import Shop
 from byceps.util.framework.blueprint import create_blueprint
 from byceps.util.framework.templating import templated
+from byceps.util.result import Result
 from byceps.util.views import permission_required
 
 
@@ -51,70 +52,36 @@ def view_for_shop(shop_id):
         'shop': shop,
         'brand': brand,
         'email_config': email_config,
-        'placed_order_message_text': example_placed_order_message_text,
-        'paid_order_message_text': example_paid_order_message_text,
-        'canceled_order_message_text': example_canceled_order_message_text,
+        'placed_order_message_text_result': example_placed_order_message_text,
+        'paid_order_message_text_result': example_paid_order_message_text,
+        'canceled_order_message_text_result': example_canceled_order_message_text,
     }
 
 
 def _get_example_placed_order_message_text(
     shop: Shop, sender: NameAndAddress, brand: Brand
-) -> str | None:
-    message_text_result = (
-        order_email_example_service.build_example_placed_order_message_text(
-            shop, sender, brand, g.user.locale
-        )
+) -> Result[str, str]:
+    return order_email_example_service.build_example_placed_order_message_text(
+        shop, sender, brand, g.user.locale
     )
-
-    if message_text_result.is_err():
-        error_message = message_text_result.unwrap_err()
-        current_app.logger.error(
-            'Could not assemble example email for placed order:\n%s',
-            error_message,
-        )
-        return None
-
-    return message_text_result.unwrap()
 
 
 def _get_example_paid_order_message_text(
     shop: Shop, sender: NameAndAddress, brand: Brand
-) -> str | None:
-    message_text_result = (
-        order_email_example_service.build_example_paid_order_message_text(
-            shop, sender, brand, g.user.locale
-        )
+) -> Result[str, str]:
+    return order_email_example_service.build_example_paid_order_message_text(
+        shop, sender, brand, g.user.locale
     )
-
-    if message_text_result.is_err():
-        error_message = message_text_result.unwrap_err()
-        current_app.logger.error(
-            'Could not assemble example email for paid order:\n%s',
-            error_message,
-        )
-        return None
-
-    return message_text_result.unwrap()
 
 
 def _get_example_canceled_order_message_text(
     shop: Shop, sender: NameAndAddress, brand: Brand
-) -> str | None:
-    message_text_result = (
+) -> Result[str, str]:
+    return (
         order_email_example_service.build_example_canceled_order_message_text(
             shop, sender, brand, g.user.locale
         )
     )
-
-    if message_text_result.is_err():
-        error_message = message_text_result.unwrap_err()
-        current_app.logger.error(
-            'Could not assemble example email for canceled order:\n%s',
-            error_message,
-        )
-        return None
-
-    return message_text_result.unwrap()
 
 
 def _get_shop_or_404(shop_id):
