da340f561f4c2d9b602001ee8e002ec7a5130da8
Jochen Kupperschmidt <homework@nwsnet.de>
2023-11-28 01:24:03 +0100

    Provide test fixture factories for event property models to all tests

--- tests/unit/announce/conftest.py
+++ tests/unit/announce/conftest.py
@@ -7,14 +7,6 @@ from datetime import datetime
 
 import pytest
 
-from byceps.events.base import EventBrand, EventParty, EventSite, EventUser
-from byceps.services.brand.models import BrandID
-from byceps.services.party.models import PartyID
-from byceps.services.site.models import SiteID
-from byceps.services.user.models.user import UserID
-
-from tests.helpers import generate_token, generate_uuid
-
 
 @pytest.fixture(scope='package')
 def app(make_app):
@@ -26,83 +18,3 @@ def app(make_app):
 @pytest.fixture(scope='module')
 def now() -> datetime:
     return datetime.utcnow()
-
-
-@pytest.fixture(scope='session')
-def make_event_brand():
-    def _wrapper(
-        *,
-        id: str | None = None,
-        title: str | None = None,
-    ) -> EventBrand:
-        if id is None:
-            id = generate_token()
-
-        if title is None:
-            title = generate_token()
-
-        return EventBrand(
-            id=BrandID(id),
-            title=title,
-        )
-
-    return _wrapper
-
-
-@pytest.fixture(scope='session')
-def make_event_party():
-    def _wrapper(
-        *,
-        id: str | None = None,
-        title: str | None = None,
-    ) -> EventParty:
-        if id is None:
-            id = generate_token()
-
-        if title is None:
-            title = generate_token()
-
-        return EventParty(
-            id=PartyID(id),
-            title=title,
-        )
-
-    return _wrapper
-
-
-@pytest.fixture(scope='session')
-def make_event_site():
-    def _wrapper(
-        *,
-        id: str | None = None,
-        title: str | None = None,
-    ) -> EventSite:
-        if id is None:
-            id = generate_token()
-
-        if title is None:
-            title = generate_token()
-
-        return EventSite(
-            id=SiteID(id),
-            title=title,
-        )
-
-    return _wrapper
-
-
-@pytest.fixture(scope='session')
-def make_event_user():
-    def _wrapper(
-        *,
-        screen_name: str | None = '__random__',
-    ) -> EventUser:
-        if screen_name == '__random__':
-            screen_name = generate_token()
-
-        return EventUser(
-            id=UserID(generate_uuid()),
-            screen_name=screen_name,
-        )
-
-    return _wrapper
