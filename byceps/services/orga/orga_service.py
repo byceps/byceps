08ad76e7de767df330762f0c8b4dcb0c553a9fbb
Jochen Kupperschmidt <homework@nwsnet.de>
2024-04-19 00:24:30 +0200

    Remove unused function `count_orgas_for_brand`

--- byceps/services/orga/orga_service.py
+++ byceps/services/orga/orga_service.py
@@ -52,19 +52,6 @@ def get_orgas_for_brand(brand_id: BrandID) -> Sequence[DbUser]:
     )
 
 
-def count_orgas_for_brand(brand_id: BrandID) -> int:
-    """Return the number of users with organizer status for the brand."""
-    return (
-        db.session.scalar(
-            select(db.func.count(DbUser.id))
-            .distinct(DbUser.id)
-            .join(DbOrgaFlag)
-            .filter(DbOrgaFlag.brand_id == brand_id)
-        )
-        or 0
-    )
-
-
 def grant_orga_status(
     user: User, brand: Brand, initiator: User
 ) -> OrgaStatusGrantedEvent:
