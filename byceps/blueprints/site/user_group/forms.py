199af217de189ede3d8ed40fec599a8225995eb7
Jochen Kupperschmidt <homework@nwsnet.de>
2024-04-19 13:25:43 +0200

    Limit length of user group title, description via form validation

--- byceps/blueprints/site/user_group/forms.py
+++ byceps/blueprints/site/user_group/forms.py
@@ -8,11 +8,15 @@ byceps.blueprints.site.user_group.forms
 
 from flask_babel import lazy_gettext
 from wtforms import StringField, TextAreaField
-from wtforms.validators import InputRequired, Optional
+from wtforms.validators import InputRequired, Length, Optional
 
 from byceps.util.l10n import LocalizedForm
 
 
 class CreateForm(LocalizedForm):
-    title = StringField(lazy_gettext('Title'), [InputRequired()])
-    description = TextAreaField(lazy_gettext('Description'), [Optional()])
+    title = StringField(
+        lazy_gettext('Title'), [InputRequired(), Length(max=40)]
+    )
+    description = TextAreaField(
+        lazy_gettext('Description'), [Optional(), Length(max=200)]
+    )
