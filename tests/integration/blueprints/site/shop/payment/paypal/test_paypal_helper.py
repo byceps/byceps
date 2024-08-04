45462a7a5149b2951e923ef76f252d2ee3fa16e8
Jan Korneffel <mail@ove.cc>
2024-08-04 18:06:57 +0200

    Add PayPal payment integration
    Co-authored-by: Micha Ober <git@ober-mail.de>
    Co-authored-by: Jochen Kupperschmidt <homework@nwsnet.de>

---
+++ tests/integration/blueprints/site/shop/payment/paypal/test_paypal_helper.py
@@ -0,0 +1,94 @@
+from string import Template
+
+import pytest
+
+from byceps.blueprints.site.shop.payment.paypal.views import (
+    _check_transaction_against_order,
+    _extract_transaction_id,
+)
+
+from .helpers import json_to_obj
+
+
+@pytest.mark.parametrize(
+    'status, currency_code, total_amount, invoice_id, expected',
+    [
+        ('COMPLETED', 'EUR', '47.11', 'order-001', True),
+        ('COMPLETED', 'EUR', '57.11', 'order-001', False),
+        ('DENIED', 'EUR', '47.11', 'order-001', False),
+        ('COMPLETED', 'USD', '47.11', 'order-001', False),
+        ('COMPLETED', 'EUR', '47.11', 'order-002', False),
+    ],
+)
+def test_paypal_check_transaction(
+    status, currency_code, total_amount, invoice_id, expected
+):
+    order = json_to_obj(
+        """
+        {
+            "total_amount": "47.11",
+            "order_number": "order-001"
+        }
+        """
+    )
+
+    response_json_template = Template(
+        """
+        {
+            "result": {
+                "status": "$status",
+                "purchase_units": [
+                    {
+                        "amount": {
+                            "currency_code": "$currency_code",
+                            "value": "$total_amount"
+                        },
+                        "invoice_id": "$invoice_id"
+                    }
+                ]
+            }
+        }
+        """
+    )
+
+    response_json = response_json_template.substitute(
+        {
+            'status': status,
+            'currency_code': currency_code,
+            'total_amount': total_amount,
+            'invoice_id': invoice_id,
+        }
+    )
+    response = json_to_obj(response_json)
+
+    assert _check_transaction_against_order(response, order) == expected
+
+
+def test_paypal_extract_transaction_id():
+    response = json_to_obj(
+        """
+        {
+            "result": {
+                "status": "COMPLETED",
+                "purchase_units": [
+                    {
+                        "payments": {
+                            "captures": [
+                                {
+                                    "id": "transaction-id-denied",
+                                    "status": "DENIED"
+                                },
+                                {
+                                    "id": "transaction-id-completed",
+                                    "status": "COMPLETED"
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+        """
+    )
+
+    assert _extract_transaction_id(response) == 'transaction-id-completed'
---
+++ tests/integration/blueprints/site/shop/payment/paypal/test_paypal_script.py
@@ -0,0 +1,174 @@
+from datetime import datetime
+from decimal import Decimal
+from string import Template
+
+from moneyed import EUR, Money
+import pytest
+
+from byceps.services.party.models import Party
+from byceps.services.shop.cart.models import Cart
+from byceps.services.shop.order import order_checkout_service
+from byceps.services.shop.payment import payment_gateway_service
+from byceps.services.shop.payment.models import PaymentGateway
+from byceps.services.shop.shop.models import Shop, ShopID
+from byceps.services.shop.storefront.models import Storefront
+from byceps.services.site.models import Site, SiteID
+from byceps.services.snippet import snippet_service
+
+from tests.helpers import (
+    create_site,
+    http_client,
+    log_in_user,
+)
+from tests.helpers.shop import (
+    create_article as _create_article,
+    create_orderer,
+    create_shop_snippet,
+)
+
+
+@pytest.fixture(scope='module')
+def shop(make_brand, make_shop, admin_user):
+    brand = make_brand()
+    shop = make_shop(brand.id, title='NorthCon 2020')
+    snippet_id = create_shop_snippet(
+        shop.id, admin_user, 'payment_instructions', 'de', ''
+    )
+
+    yield shop
+
+    snippet_service.delete_snippet(snippet_id)
+
+
+@pytest.fixture(scope='module')
+def storefront(
+    shop: Shop, make_order_number_sequence, make_storefront
+) -> Storefront:
+    order_number_sequence = make_order_number_sequence(
+        shop.id, prefix='PP-99-B'
+    )
+
+    return make_storefront(shop.id, order_number_sequence.id)
+
+
+@pytest.fixture(scope='module')
+def payment_gateway(make_payment_gateway, storefront: Storefront) -> None:
+    payment_gateway = make_payment_gateway(payment_gateway_id='paypal')
+    payment_gateway_service.enable_payment_gateway_for_storefront(
+        payment_gateway.id, storefront.id
+    )
+
+
+@pytest.fixture(scope='module')
+def site(party: Party, storefront: Storefront) -> Site:
+    return create_site(
+        SiteID('paypal-enabled-site'),
+        party.brand_id,
+        party_id=party.id,
+        storefront_id=storefront.id,
+    )
+
+
+@pytest.fixture(scope='module')
+def site_app(site, make_site_app):
+    server_name = f'{site.id}.acmecon.test'
+    app = make_site_app(server_name, site.id)
+    with app.app_context():
+        yield app
+
+
+@pytest.fixture(scope='module')
+def app(site_app):
+    site_app.config['PAYPAL_CLIENT_ID'] = 'dummy-paypal-client-id'
+    yield site_app
+
+
+@pytest.fixture(scope='module')
+def orderer(make_user):
+    user = make_user()
+    log_in_user(user.id)
+    orderer = create_orderer(user)
+
+    yield orderer
+
+
+@pytest.fixture()
+def cart(shop: Shop) -> Cart:
+    tax_rate = Decimal('0.19')
+
+    article1 = create_article(
+        shop.id,
+        'PP-A01',
+        'Sample Article 1',
+        Decimal('10.00'),
+        tax_rate,
+    )
+    article2 = create_article(
+        shop.id,
+        'PP-A02',
+        'Sample Article 2',
+        Decimal('1.99'),
+        tax_rate,
+    )
+
+    cart = Cart(shop.currency)
+    cart.add_item(article1, 1)
+    cart.add_item(article2, 3)
+
+    return cart
+
+
+@pytest.fixture()
+def order(
+    site: Site,
+    storefront: Storefront,
+    orderer,
+    cart: Cart,
+    payment_gateway: PaymentGateway,
+):
+    created_at = datetime(2015, 2, 26, 12, 26, 24)
+
+    order, _ = order_checkout_service.place_order(
+        storefront, orderer, cart, created_at=created_at
+    ).unwrap()
+
+    return order
+
+
+def test_render_paypal_script(request, app, order):
+    url = f'/shop/orders/{order.id!s}'
+
+    with http_client(app, user_id=order.placed_by.id) as client:
+        response = client.get(url)
+
+    filename = request.fspath.dirpath('paypal_script.html')
+    template_source = filename.read_text('utf-8')
+    expected_script_content_template = Template(template_source)
+
+    expected_script_content = expected_script_content_template.substitute(
+        client_id=app.config['PAYPAL_CLIENT_ID'], order_id=order.id
+    )
+
+    body = response.get_data(as_text=True)
+
+    assert expected_script_content in body
+
+
+# helpers
+
+
+def create_article(
+    shop_id: ShopID,
+    item_number,
+    name: str,
+    price: Decimal,
+    tax_rate: Decimal,
+):
+    return _create_article(
+        shop_id,
+        item_number=item_number,
+        name=name,
+        price=Money(price, EUR),
+        tax_rate=tax_rate,
+        total_quantity=10,
+    )
