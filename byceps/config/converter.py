856428faa0e9fd7bbfe503bcb4187edf723bc532
Jochen Kupperschmidt <homework@nwsnet.de>
2024-11-27 03:04:58 +0100

    Add converter for new configuration format

---
+++ byceps/config/converter.py
@@ -0,0 +1,62 @@
+"""
+byceps.config.converter
+~~~~~~~~~~~~~~~~~~~~~~~
+
+Configuration converter
+
+:Copyright: 2014-2024 Jochen Kupperschmidt
+:License: Revised BSD (see `LICENSE` file for details)
+"""
+
+from collections.abc import Iterator
+from typing import Any
+
+from .models import BycepsConfig, DatabaseConfig
+
+
+def convert_config(config: BycepsConfig) -> dict[str, Any]:
+    """Convert configuration to dictionary accepted by Flask."""
+    return dict(_generate_entries(config))
+
+
+def _generate_entries(config: BycepsConfig) -> Iterator[tuple[str, Any]]:
+    yield 'DEBUG', config.debug.enabled
+    yield 'DEBUG_TOOLBAR_ENABLED', config.debug.toolbar_enabled
+
+    locale = config.locale
+    yield 'BABEL_DEFAULT_LOCALE', locale
+    yield 'LOCALE', locale
+    yield 'LOCALES_FORMS', [locale]
+
+    timezone = config.timezone
+    yield 'BABEL_DEFAULT_TIMEZONE', timezone
+    yield 'SHOP_ORDER_EXPORT_TIMEZONE', timezone
+    yield 'TIMEZONE', timezone
+
+    yield 'JOBS_ASYNC', config.jobs.asynchronous
+
+    yield 'MAIL_HOST', config.smtp.host
+    yield 'MAIL_PASSWORD', config.smtp.password
+    yield 'MAIL_PORT', config.smtp.port
+    yield 'MAIL_STARTTLS', config.smtp.starttls
+    yield 'MAIL_SUPPRESS_SEND', config.smtp.suppress_send
+    yield 'MAIL_USE_SSL', config.smtp.use_ssl
+    yield 'MAIL_USERNAME', config.smtp.username
+
+    yield 'METRICS_ENABLED', config.metrics.enabled
+
+    yield 'PROPAGATE_EXCEPTIONS', config.propagate_exceptions
+
+    yield 'REDIS_URL', config.redis.url
+
+    yield 'SECRET_KEY', config.secret_key
+
+    yield 'SQLALCHEMY_DATABASE_URI', _build_database_url(config.database)
+
+    yield 'STYLE_GUIDE_ENABLED', config.styleguide.enabled
+
+
+def _build_database_url(db_config: DatabaseConfig) -> str:
+    """Assemble SQLAlchemy database URL."""
+    scheme = 'postgresql+psycopg'
+    return f'{scheme}://{db_config.username}:{db_config.password}@{db_config.host}:{db_config.port}/{db_config.database}'
---
+++ tests/unit/config/test_convert_config.py
@@ -0,0 +1,92 @@
+"""
+:Copyright: 2014-2024 Jochen Kupperschmidt
+:License: Revised BSD (see `LICENSE` file for details)
+"""
+
+from byceps.config.converter import convert_config
+from byceps.config.models import (
+    BycepsConfig,
+    DatabaseConfig,
+    DebugConfig,
+    DiscordConfig,
+    JobsConfig,
+    MetricsConfig,
+    PaypalConfig,
+    RedisConfig,
+    SmtpConfig,
+    StripeConfig,
+    StyleguideConfig,
+)
+
+
+def test_convert_config():
+    expected = {
+        'BABEL_DEFAULT_LOCALE': 'de',
+        'BABEL_DEFAULT_TIMEZONE': 'Europe/Berlin',
+        'DEBUG': True,
+        'DEBUG_TOOLBAR_ENABLED': True,
+        'JOBS_ASYNC': True,
+        'LOCALE': 'de',
+        'LOCALES_FORMS': ['de'],
+        'MAIL_HOST': 'localhost',
+        'MAIL_PASSWORD': 'smtppass',
+        'MAIL_PORT': 25,
+        'MAIL_STARTTLS': True,
+        'MAIL_SUPPRESS_SEND': False,
+        'MAIL_USE_SSL': False,
+        'MAIL_USERNAME': 'smtpuser',
+        'METRICS_ENABLED': True,
+        'PROPAGATE_EXCEPTIONS': True,
+        'REDIS_URL': 'redis://127.0.0.1:6379/0',
+        'SECRET_KEY': '<RANDOM-BYTES>',
+        'SHOP_ORDER_EXPORT_TIMEZONE': 'Europe/Berlin',
+        'SQLALCHEMY_DATABASE_URI': 'postgresql+psycopg://dbuser:dbpass@127.0.0.1:5432/db',
+        'STYLE_GUIDE_ENABLED': True,
+        'TIMEZONE': 'Europe/Berlin',
+    }
+
+    config = BycepsConfig(
+        locale='de',
+        propagate_exceptions=True,
+        secret_key='<RANDOM-BYTES>',
+        timezone='Europe/Berlin',
+        database=DatabaseConfig(
+            host='127.0.0.1',
+            port=5432,
+            username='dbuser',
+            password='dbpass',
+            database='db',
+        ),
+        debug=DebugConfig(
+            enabled=True,
+            toolbar_enabled=True,
+        ),
+        discord=DiscordConfig(),
+        jobs=JobsConfig(
+            asynchronous=True,
+        ),
+        metrics=MetricsConfig(
+            enabled=True,
+        ),
+        paypal=PaypalConfig(),
+        redis=RedisConfig(
+            url='redis://127.0.0.1:6379/0',
+        ),
+        smtp=SmtpConfig(
+            host='localhost',
+            port=25,
+            starttls=True,
+            use_ssl=False,
+            username='smtpuser',
+            password='smtppass',
+            suppress_send=False,
+        ),
+        stripe=StripeConfig(),
+        styleguide=StyleguideConfig(
+            enabled=True,
+        ),
+    )
+
+    actual = convert_config(config)
+
+    assert actual == expected
