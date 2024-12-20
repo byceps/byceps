9417f58ce528c32e144cd194b3455f7ec63168cc
Jochen Kupperschmidt <homework@nwsnet.de>
2024-12-20 03:57:16 +0100

    Add newsletter service function to unsubscribe user from all lists

--- byceps/services/newsletter/newsletter_command_service.py
+++ byceps/services/newsletter/newsletter_command_service.py
@@ -10,6 +10,7 @@ from datetime import datetime
 
 from sqlalchemy import delete
 from sqlalchemy.dialects.postgresql import insert
+import structlog
 
 from byceps.database import db
 from byceps.events.newsletter import (
@@ -23,6 +24,9 @@ from . import newsletter_domain_service, newsletter_service
 from .dbmodels import DbList, DbSubscription, DbSubscriptionUpdate
 from .errors import UnknownListIdError
 from .models import List, ListID, SubscriptionUpdate
+
+
+log = structlog.get_logger()
 
 
 def create_list(list_id: ListID, title: str) -> List:
@@ -103,6 +107,27 @@ def unsubscribe_user_from_list(
     db.session.commit()
 
     return Ok((subscription_update, event))
+
+
+def unsubscribe_user_from_lists(
+    user: User, expressed_at: datetime
+) -> tuple[SubscriptionUpdate, UnsubscribedFromNewsletterEvent]:
+    """Unsubscribe the user from the lists they are subscribed to."""
+    events = []
+
+    for list_ in newsletter_service.get_lists_user_is_subscribed_to(user.id):
+        match unsubscribe_user_from_list(user, list_, expressed_at):
+            case Ok((_, event)):
+                events.append(event)
+            case Err(e):
+                log.error(
+                    'Could not unsubscribe user from list.',
+                    user_id=user.id,
+                    list_id=list_.id,
+                    error=e,
+                )
+
+    return events
 
 
 def _update_subscription_state(
