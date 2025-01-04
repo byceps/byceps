30c74042aaa604d9a8f8699156a284b1f23dd3ae
Jochen Kupperschmidt <homework@nwsnet.de>
2025-01-05 00:09:53 +0100

    Configure Sentry in multi-app serve entrypoint, too

--- serve_apps.py
+++ serve_apps.py
@@ -11,10 +11,13 @@ import structlog
 
 from byceps.app_dispatcher import create_dispatcher_app, get_apps_config
 from byceps.util.result import Err, Ok
+from byceps.util.sentry import configure_sentry_from_env
 
 
 log = structlog.get_logger()
 
+
+configure_sentry_from_env()
 
 match get_apps_config():
     case Ok(apps_config):
