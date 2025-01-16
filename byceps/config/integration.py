c420b18c62a4872c5ff558b360a1280730424750
Jochen Kupperschmidt <homework@nwsnet.de>
2025-01-16 01:40:17 +0100

    Allow using modern configuration file format through environment variable `BYCEPS_CONFIG_FILE`
    Not to be confused with `BYCEPS_CONFIG`.

--- byceps/config/integration.py
+++ byceps/config/integration.py
@@ -12,10 +12,17 @@ from pathlib import Path
 from typing import Any
 
 import rtoml
+import structlog
 
 from byceps.byceps_app import BycepsApp
+from byceps.util.result import Err, Ok
 
+from .converter import convert_config
 from .errors import ConfigurationError
+from .parser import parse_config
+
+
+log = structlog.get_logger()
 
 
 def init_app(app: BycepsApp) -> None:
@@ -41,12 +48,17 @@ def parse_value_from_environment(
 
 def read_configuration_from_file_given_in_env_var() -> dict[str, Any]:
     """Load configuration from file specified via environment variable."""
-    filename_str = os.environ.get('BYCEPS_CONFIG')
-    if not filename_str:
-        return {}
+    filename_str = os.environ.get('BYCEPS_CONFIG_FILE')
+    if filename_str:
+        filename = Path(filename_str)
+        return _read_modern_configuration_from_file(filename)
+    else:
+        filename_str = os.environ.get('BYCEPS_CONFIG')
+        if not filename_str:
+            return {}
 
-    filename = Path(filename_str)
-    return _read_configuration_from_file(filename)
+        filename = Path(filename_str)
+        return _read_configuration_from_file(filename)
 
 
 def _read_configuration_from_file(filename: Path) -> dict[str, Any]:
@@ -57,3 +69,17 @@ def _read_configuration_from_file(filename: Path) -> dict[str, Any]:
     except OSError as e:
         e.strerror = f'Unable to load configuration file ({e.strerror})'
         raise
+
+
+def _read_modern_configuration_from_file(filename: Path) -> dict[str, Any]:
+    """Load modern configuration from file."""
+    match parse_config(filename.read_text()):
+        case Ok(config):
+            return convert_config(config)
+        case Err(errors):
+            log.error(
+                'Could not parse configuration file.',
+                filename=filename,
+                errors=errors,
+            )
+            raise ConfigurationError('Errors found in configuration file')
