47d805058a39e0c3d7775af56399dce5f4e8d911
Jochen Kupperschmidt <homework@nwsnet.de>
2025-03-26 05:15:10 +0100

    Move orga admin blueprint into corresponding service package

--- byceps/blueprints/admin/blueprints.py
+++ byceps/blueprints/admin/blueprints.py
@@ -42,7 +42,7 @@ def register_admin_blueprints(
         ('blueprints.admin.more', '/more'),
         ('services.news.blueprints.admin', '/news'),
         ('services.newsletter.blueprints.admin', '/newsletter'),
-        ('blueprints.admin.orga', '/orgas'),
+        ('services.orga.blueprints.admin', '/orgas'),
         ('blueprints.admin.orga_presence', '/presence'),
         ('blueprints.admin.orga_team', '/orga_teams'),
         ('blueprints.admin.page', '/pages'),
