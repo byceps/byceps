aa1a03f9dae7c79d2c846cda6e18183d0611f127
Jochen Kupperschmidt <homework@nwsnet.de>
2025-04-05 18:39:25 +0200

    Fix module docstring

--- byceps/services/board/dbmodels/__init__.py
+++ byceps/services/board/dbmodels/__init__.py
@@ -1,6 +1,6 @@
 """
-byceps.blueprints.site.board.models
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+byceps.services.board.dbmodels
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 :Copyright: 2014-2025 Jochen Kupperschmidt
 :License: Revised BSD (see `LICENSE` file for details)
