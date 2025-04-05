22160f4c0861d0885ff0bb2340268acf4abdbe0e
Jochen Kupperschmidt <homework@nwsnet.de>
2025-04-05 21:44:17 +0200

    Order API blueprint registrations

--- byceps/blueprints/api/blueprints.py
+++ byceps/blueprints/api/blueprints.py
@@ -15,12 +15,12 @@ def register_api_blueprints(app: Flask) -> None:
     api_v1 = get_blueprint('blueprints.api.v1')
 
     blueprints = [
-        ('blueprints.api.v1.authn', '/authentication'),
         ('blueprints.api.v1.attendance', '/attendances'),
+        ('blueprints.api.v1.authn', '/authentication'),
         ('blueprints.api.v1.snippet', '/snippets'),
+        ('blueprints.api.v1.ticketing', '/ticketing'),
         ('blueprints.api.v1.tourney.avatar', '/tourney/avatars'),
         ('blueprints.api.v1.tourney.match.comments', '/tourney'),
-        ('blueprints.api.v1.ticketing', '/ticketing'),
         ('blueprints.api.v1.user', '/users'),
         ('blueprints.api.v1.user_avatar', '/user_avatars'),
         ('blueprints.api.v1.user_badge', '/user_badges'),
