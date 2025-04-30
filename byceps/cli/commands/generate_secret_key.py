b00d36c66db5bc612bdc93f9d1ba460b47e6d3b0
Jochen Kupperschmidt <homework@nwsnet.de>
2025-05-01 00:23:40 +0200

    Update docstring regarding spelling of secret key configuration property

--- byceps/cli/commands/generate_secret_key.py
+++ byceps/cli/commands/generate_secret_key.py
@@ -4,7 +4,7 @@ byceps.cli.command.generate_secret_key
 
 Generate a secure secret key.
 
-Suitable as a value for ``SECRET_KEY`` in a BYCEPS configuration file.
+Suitable as a value for ``secret_key`` in a BYCEPS configuration file.
 
 :Copyright: 2014-2025 Jochen Kupperschmidt
 :License: Revised BSD (see `LICENSE` file for details)
