95db7eafa22fa3f9731a604592bc56f40c0a8a8b
Jochen Kupperschmidt <homework@nwsnet.de>
2025-06-21 13:41:04 +0200

    Extract seating area repository

---
+++ byceps/services/seating/seating_area_repository.py
@@ -0,0 +1,128 @@
+"""
+byceps.services.seating.seating_area_repository
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Copyright: 2014-2025 Jochen Kupperschmidt
+:License: Revised BSD (see `LICENSE` file for details)
+"""
+
+from sqlalchemy import delete, select
+
+from byceps.database import db
+from byceps.services.party.models import PartyID
+from byceps.services.ticketing.dbmodels.ticket import DbTicket
+
+from .dbmodels.area import DbSeatingArea
+from .dbmodels.seat import DbSeat
+from .models import SeatingArea, SeatingAreaID, SeatUtilization
+
+
+def create_area(area: SeatingArea) -> None:
+    """Create an area."""
+    db_area = DbSeatingArea(
+        area.id,
+        area.party_id,
+        area.slug,
+        area.title,
+        image_filename=area.image_filename,
+        image_width=area.image_width,
+        image_height=area.image_height,
+    )
+
+    db.session.add(db_area)
+    db.session.commit()
+
+
+def update_area(area: SeatingArea) -> None:
+    """Update an area."""
+    db_area = find_area(area.id)
+    if db_area is None:
+        raise ValueError(f'Unknown seating area ID "{area_id}"')
+
+    db_area.slug = area.slug
+    db_area.title = area.title
+    db_area.image_filename = area.image_filename
+    db_area.image_width = area.image_width
+    db_area.image_height = area.image_height
+
+    db.session.commit()
+
+
+def delete_area(area_id: SeatingAreaID) -> None:
+    """Delete an area."""
+    db.session.execute(delete(DbSeatingArea).filter_by(id=area_id))
+    db.session.commit()
+
+
+def find_area(area_id: SeatingAreaID) -> DbSeatingArea | None:
+    """Return the area, or `None` if not found."""
+    return db.session.get(DbSeatingArea, area_id)
+
+
+def find_area_for_party_by_slug(
+    party_id: PartyID, slug: str
+) -> DbSeatingArea | None:
+    """Return the area for that party with that slug, or `None` if not found."""
+    return db.session.scalars(
+        select(DbSeatingArea).filter_by(party_id=party_id).filter_by(slug=slug)
+    ).first()
+
+
+def get_areas_for_party(party_id: PartyID) -> list[DbSeatingArea]:
+    """Return all areas for that party."""
+    return db.session.scalars(
+        select(DbSeatingArea).filter_by(party_id=party_id)
+    ).all()
+
+
+def get_areas_with_seat_utilization(
+    party_id: PartyID,
+) -> list[tuple[DbSeatingArea, SeatUtilization]]:
+    """Return all areas and their seat utilization for that party."""
+    area = db.aliased(DbSeatingArea)
+
+    subquery_occupied_seat_count = (
+        select(db.func.count(DbTicket.id))
+        .filter(DbTicket.revoked == False)  # noqa: E712
+        .filter(DbTicket.occupied_seat_id.is_not(None))
+        .join(DbSeat)
+        .filter(DbSeat.area_id == area.id)
+        .scalar_subquery()
+    )
+
+    subquery_total_seat_count = (
+        select(db.func.count(DbSeat.id))
+        .filter_by(area_id=area.id)
+        .scalar_subquery()
+    )
+
+    rows = db.session.execute(
+        select(
+            area,
+            subquery_occupied_seat_count,
+            subquery_total_seat_count,
+        )
+        .filter(area.party_id == party_id)
+        .group_by(area.id)
+        .order_by(area.title)
+    ).all()
+
+    return [
+        (
+            db_area,
+            SeatUtilization(
+                occupied=occupied_seat_count, total=total_seat_count
+            ),
+        )
+        for db_area, occupied_seat_count, total_seat_count in rows
+    ]
+
+
+def count_areas_for_party(party_id: PartyID) -> int:
+    """Return the number of seating areas for that party."""
+    return (
+        db.session.scalar(
+            select(db.func.count(DbSeatingArea.id)).filter_by(party_id=party_id)
+        )
+        or 0
+    )
--- byceps/services/seating/seating_area_service.py
+++ byceps/services/seating/seating_area_service.py
@@ -6,15 +6,10 @@ byceps.services.seating.seating_area_service
 :License: Revised BSD (see `LICENSE` file for details)
 """
 
-from sqlalchemy import delete, select
-
-from byceps.database import db
 from byceps.services.party.models import PartyID
-from byceps.services.ticketing.dbmodels.ticket import DbTicket
 
-from . import seating_area_domain_service
+from . import seating_area_domain_service, seating_area_repository
 from .dbmodels.area import DbSeatingArea
-from .dbmodels.seat import DbSeat
 from .models import SeatingArea, SeatingAreaID, SeatUtilization
 
 
@@ -37,18 +32,7 @@ def create_area(
         image_height=image_height,
     )
 
-    db_area = DbSeatingArea(
-        area.id,
-        area.party_id,
-        area.slug,
-        area.title,
-        image_filename=area.image_filename,
-        image_width=area.image_width,
-        image_height=area.image_height,
-    )
-
-    db.session.add(db_area)
-    db.session.commit()
+    seating_area_repository.create_area(area)
 
     return area
 
@@ -66,40 +50,19 @@ def update_area(
         area, slug, title, image_filename, image_width, image_height
     )
 
-    db_area = _find_db_area(updated_area.id)
-    if db_area is None:
-        raise ValueError(f'Unknown seating area ID "{area_id}"')
-
-    db_area.slug = updated_area.slug
-    db_area.title = updated_area.title
-    db_area.image_filename = updated_area.image_filename
-    db_area.image_width = updated_area.image_width
-    db_area.image_height = updated_area.image_height
-
-    db.session.commit()
+    seating_area_repository.update_area(updated_area)
 
     return updated_area
 
 
 def delete_area(area_id: SeatingAreaID) -> None:
     """Delete an area."""
-    db.session.execute(delete(DbSeatingArea).filter_by(id=area_id))
-    db.session.commit()
-
-
-def count_areas_for_party(party_id: PartyID) -> int:
-    """Return the number of seating areas for that party."""
-    return (
-        db.session.scalar(
-            select(db.func.count(DbSeatingArea.id)).filter_by(party_id=party_id)
-        )
-        or 0
-    )
+    seating_area_repository.delete_area(area_id)
 
 
 def find_area(area_id: SeatingAreaID) -> SeatingArea | None:
     """Return the area, or `None` if not found."""
-    db_area = _find_db_area(area_id)
+    db_area = seating_area_repository.find_area(area_id)
 
     if db_area is None:
         return None
@@ -107,17 +70,13 @@ def find_area(area_id: SeatingAreaID) -> SeatingArea | None:
     return _db_entity_to_area(db_area)
 
 
-def _find_db_area(area_id: SeatingAreaID) -> DbSeatingArea | None:
-    return db.session.get(DbSeatingArea, area_id)
-
-
 def find_area_for_party_by_slug(
     party_id: PartyID, slug: str
 ) -> SeatingArea | None:
     """Return the area for that party with that slug, or `None` if not found."""
-    db_area = db.session.scalars(
-        select(DbSeatingArea).filter_by(party_id=party_id).filter_by(slug=slug)
-    ).first()
+    db_area = seating_area_repository.find_area_for_party_by_slug(
+        party_id, slug
+    )
 
     if db_area is None:
         return None
@@ -127,9 +86,7 @@ def find_area_for_party_by_slug(
 
 def get_areas_for_party(party_id: PartyID) -> list[SeatingArea]:
     """Return all areas for that party."""
-    db_areas = db.session.scalars(
-        select(DbSeatingArea).filter_by(party_id=party_id)
-    ).all()
+    db_areas = seating_area_repository.get_areas_for_party(party_id)
 
     return [_db_entity_to_area(db_area) for db_area in db_areas]
 
@@ -138,43 +95,19 @@ def get_areas_with_seat_utilization(
     party_id: PartyID,
 ) -> list[tuple[SeatingArea, SeatUtilization]]:
     """Return all areas and their seat utilization for that party."""
-    area = db.aliased(DbSeatingArea)
-
-    subquery_occupied_seat_count = (
-        select(db.func.count(DbTicket.id))
-        .filter(DbTicket.revoked == False)  # noqa: E712
-        .filter(DbTicket.occupied_seat_id.is_not(None))
-        .join(DbSeat)
-        .filter(DbSeat.area_id == area.id)
-        .scalar_subquery()
+    db_areas_with_seat_utilization = (
+        seating_area_repository.get_areas_with_seat_utilization(party_id)
     )
-
-    subquery_total_seat_count = (
-        select(db.func.count(DbSeat.id))
-        .filter_by(area_id=area.id)
-        .scalar_subquery()
-    )
-
-    rows = db.session.execute(
-        select(
-            area,
-            subquery_occupied_seat_count,
-            subquery_total_seat_count,
-        )
-        .filter(area.party_id == party_id)
-        .group_by(area.id)
-        .order_by(area.title)
-    ).all()
 
     return [
-        (
-            _db_entity_to_area(db_area),
-            SeatUtilization(
-                occupied=occupied_seat_count, total=total_seat_count
-            ),
-        )
-        for db_area, occupied_seat_count, total_seat_count in rows
+        (_db_entity_to_area(db_area), seat_utilization)
+        for db_area, seat_utilization in db_areas_with_seat_utilization
     ]
+
+
+def count_areas_for_party(party_id: PartyID) -> int:
+    """Return the number of seating areas for that party."""
+    return seating_area_repository.count_areas_for_party(party_id)
 
 
 def _db_entity_to_area(db_area: DbSeatingArea) -> SeatingArea:
