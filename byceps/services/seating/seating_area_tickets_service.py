acb15ea2beea0d5b30915538a6fb43f7e0b7bb61
Jochen Kupperschmidt <homework@nwsnet.de>
2025-06-28 15:29:20 +0200

    Prefix database entity variable names

--- byceps/services/seating/seating_area_tickets_service.py
+++ byceps/services/seating/seating_area_tickets_service.py
@@ -35,37 +35,37 @@ class ManagedTicket:
 
 
 def get_users(
-    seats_with_tickets: Iterable[tuple[Seat, DbTicket]],
-    managed_tickets: Iterable[DbTicket],
+    seats_with_db_tickets: Iterable[tuple[Seat, DbTicket]],
+    managed_db_tickets: Iterable[DbTicket],
 ) -> dict[UserID, User]:
-    seat_tickets = _get_seat_tickets(seats_with_tickets)
-    tickets = chain(seat_tickets, managed_tickets)
-    user_ids = set(_get_ticket_user_ids(tickets))
+    seat_db_tickets = _get_seat_tickets(seats_with_db_tickets)
+    db_tickets = chain(seat_db_tickets, managed_db_tickets)
+    user_ids = set(_get_ticket_user_ids(db_tickets))
     return user_service.get_users_indexed_by_id(user_ids, include_avatars=True)
 
 
 def _get_seat_tickets(
-    seats_with_tickets: Iterable[tuple[Seat, DbTicket]],
+    seats_with_db_tickets: Iterable[tuple[Seat, DbTicket]],
 ) -> Iterator[DbTicket]:
-    for _, ticket in seats_with_tickets:
-        if (ticket is not None) and (ticket.used_by_id is not None):
-            yield ticket
+    for _, db_ticket in seats_with_db_tickets:
+        if (db_ticket is not None) and (db_ticket.used_by_id is not None):
+            yield db_ticket
 
 
-def _get_ticket_user_ids(tickets: Iterable[DbTicket]) -> Iterator[UserID]:
-    for ticket in tickets:
-        user_id = ticket.used_by_id
+def _get_ticket_user_ids(db_tickets: Iterable[DbTicket]) -> Iterator[UserID]:
+    for db_ticket in db_tickets:
+        user_id = db_ticket.used_by_id
         if user_id is not None:
             yield user_id
 
 
 def get_seats_and_tickets(
-    seats_with_tickets: Iterable[tuple[Seat, DbTicket]],
+    seats_with_db_tickets: Iterable[tuple[Seat, DbTicket]],
     users_by_id: dict[UserID, User],
 ) -> Iterator[tuple[Seat, SeatTicket | None]]:
-    for seat, ticket in seats_with_tickets:
-        if ticket is not None:
-            seat_ticket = _build_seat_ticket(ticket, users_by_id)
+    for seat, db_ticket in seats_with_db_tickets:
+        if db_ticket is not None:
+            seat_ticket = _build_seat_ticket(db_ticket, users_by_id)
         else:
             seat_ticket = None
 
@@ -73,48 +73,48 @@ def get_seats_and_tickets(
 
 
 def _build_seat_ticket(
-    ticket: DbTicket, users_by_id: dict[UserID, User]
+    db_ticket: DbTicket, users_by_id: dict[UserID, User]
 ) -> SeatTicket:
     user: User | None
-    if ticket.used_by_id is not None:
-        user = users_by_id[ticket.used_by_id]
+    if db_ticket.used_by_id is not None:
+        user = users_by_id[db_ticket.used_by_id]
     else:
         user = None
 
     return SeatTicket(
-        id=ticket.id,
+        id=db_ticket.id,
         user=user,
     )
 
 
 def get_managed_tickets(
-    tickets: Iterable[DbTicket], users_by_id: dict[UserID, User]
+    db_tickets: Iterable[DbTicket], users_by_id: dict[UserID, User]
 ) -> Iterator[ManagedTicket]:
-    for ticket in tickets:
-        yield _build_managed_ticket(ticket, users_by_id)
+    for db_ticket in db_tickets:
+        yield _build_managed_ticket(db_ticket, users_by_id)
 
 
 def _build_managed_ticket(
-    ticket: DbTicket, users_by_id: dict[UserID, User]
+    db_ticket: DbTicket, users_by_id: dict[UserID, User]
 ) -> ManagedTicket:
-    occupies_seat = ticket.occupied_seat is not None
+    occupies_seat = db_ticket.occupied_seat is not None
 
     seat_label = (
-        ticket.occupied_seat.label
-        if (ticket.occupied_seat is not None)
+        db_ticket.occupied_seat.label
+        if (db_ticket.occupied_seat is not None)
         else None
     )
 
     user: User | None
-    if ticket.used_by_id is not None:
-        user = users_by_id[ticket.used_by_id]
+    if db_ticket.used_by_id is not None:
+        user = users_by_id[db_ticket.used_by_id]
     else:
         user = None
 
     return ManagedTicket(
-        id=ticket.id,
-        code=TicketCode(ticket.code),
-        category_label=ticket.category.title,
+        id=db_ticket.id,
+        code=TicketCode(db_ticket.code),
+        category_label=db_ticket.category.title,
         occupies_seat=occupies_seat,
         seat_label=seat_label,
         user=user,
