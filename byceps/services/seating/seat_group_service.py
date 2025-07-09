d9c5132cfec4db3ed16a0bf87172354e8f1c28a4
Jochen Kupperschmidt <homework@nwsnet.de>
2025-07-09 09:26:43 +0200

    Fetch ticket bundle via service

--- byceps/services/seating/seat_group_service.py
+++ byceps/services/seating/seat_group_service.py
@@ -123,8 +123,11 @@ def switch_group(
     db_occupancy: DbSeatGroupOccupancy, target_group: SeatGroup
 ) -> Result[None, SeatingError]:
     """Switch ticket bundle to another seat group."""
+    db_ticket_bundle = ticket_bundle_service.get_bundle(
+        db_occupancy.ticket_bundle_id
+    )
     ticket_bundle = ticket_bundle_service.db_entity_to_ticket_bundle(
-        db_occupancy.ticket_bundle
+        db_ticket_bundle
     )
 
     db_tickets = ticket_bundle_service.get_tickets_for_bundle(ticket_bundle.id)
