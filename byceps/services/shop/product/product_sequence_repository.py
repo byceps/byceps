ed609bc8de1bf809b08bf29cd691d01d93f0875e
Jochen Kupperschmidt <homework@nwsnet.de>
2025-07-13 16:31:52 +0200

    Extract product sequence repository

---
+++ byceps/services/shop/product/product_sequence_repository.py
@@ -0,0 +1,87 @@
+"""
+byceps.services.shop.product.product_sequence_repository
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Copyright: 2014-2025 Jochen Kupperschmidt
+:License: Revised BSD (see `LICENSE` file for details)
+"""
+
+from collections.abc import Sequence
+
+from sqlalchemy import delete, select, update
+from sqlalchemy.exc import IntegrityError
+
+from byceps.database import db
+from byceps.services.shop.shop.models import ShopID
+from byceps.util.result import Err, Ok, Result
+from byceps.util.uuid import generate_uuid7
+
+from .dbmodels.number_sequence import DbProductNumberSequence
+from .models import ProductNumberSequenceID
+
+
+def create_product_number_sequence(
+    shop_id: ShopID, prefix: str, value: int
+) -> Result[DbProductNumberSequence, None]:
+    """Create a product number sequence."""
+    sequence_id = ProductNumberSequenceID(generate_uuid7())
+
+    db_sequence = DbProductNumberSequence(sequence_id, shop_id, prefix, value)
+
+    db.session.add(db_sequence)
+
+    try:
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+        return Err(None)
+
+    return Ok(db_sequence)
+
+
+def delete_product_number_sequence(
+    sequence_id: ProductNumberSequenceID,
+) -> None:
+    """Delete the product number sequence."""
+    db.session.execute(
+        delete(DbProductNumberSequence).filter_by(id=sequence_id)
+    )
+    db.session.commit()
+
+
+def find_product_number_sequence(
+    sequence_id: ProductNumberSequenceID,
+) -> DbProductNumberSequence | None:
+    """Return the product number sequence, if found."""
+    return db.session.get(DbProductNumberSequence, sequence_id)
+
+
+def get_product_number_sequences_for_shop(
+    shop_id: ShopID,
+) -> Sequence[DbProductNumberSequence]:
+    """Return the product number sequences defined for that shop."""
+    return db.session.scalars(
+        select(DbProductNumberSequence).filter_by(shop_id=shop_id)
+    ).all()
+
+
+def generate_product_number(
+    sequence_id: ProductNumberSequenceID,
+) -> Result[tuple[str, int], str]:
+    """Generate and reserve the next product number from this sequence."""
+    row = db.session.execute(
+        update(DbProductNumberSequence)
+        .filter_by(id=sequence_id)
+        .values(value=DbProductNumberSequence.value + 1)
+        .returning(
+            DbProductNumberSequence.prefix, DbProductNumberSequence.value
+        )
+    ).one_or_none()
+    db.session.commit()
+
+    if row is None:
+        return Err(f'No product number sequence found for ID "{sequence_id}".')
+
+    prefix, value = row
+
+    return Ok((prefix, value))
--- byceps/services/shop/product/product_sequence_service.py
+++ byceps/services/shop/product/product_sequence_service.py
@@ -6,14 +6,10 @@ byceps.services.shop.product.product_sequence_service
 :License: Revised BSD (see `LICENSE` file for details)
 """
 
-from sqlalchemy import delete, select, update
-from sqlalchemy.exc import IntegrityError
-
-from byceps.database import db
 from byceps.services.shop.shop.models import ShopID
 from byceps.util.result import Err, Ok, Result
-from byceps.util.uuid import generate_uuid7
 
+from . import product_sequence_repository
 from .dbmodels.number_sequence import DbProductNumberSequence
 from .models import (
     ProductNumber,
@@ -26,38 +22,30 @@ def create_product_number_sequence(
     shop_id: ShopID, prefix: str, *, value: int = 0
 ) -> Result[ProductNumberSequence, None]:
     """Create a product number sequence."""
-    sequence_id = ProductNumberSequenceID(generate_uuid7())
-
-    db_sequence = DbProductNumberSequence(sequence_id, shop_id, prefix, value)
-
-    db.session.add(db_sequence)
-
-    try:
-        db.session.commit()
-    except IntegrityError:
-        db.session.rollback()
-        return Err(None)
-
-    sequence = _db_entity_to_product_number_sequence(db_sequence)
-
-    return Ok(sequence)
+    match product_sequence_repository.create_product_number_sequence(
+        shop_id, prefix, value
+    ):
+        case Ok(db_sequence):
+            sequence = _db_entity_to_product_number_sequence(db_sequence)
+            return Ok(sequence)
+        case Err(e):
+            return Err(e)
 
 
 def delete_product_number_sequence(
     sequence_id: ProductNumberSequenceID,
 ) -> None:
     """Delete the product number sequence."""
-    db.session.execute(
-        delete(DbProductNumberSequence).filter_by(id=sequence_id)
-    )
-    db.session.commit()
+    product_sequence_repository.delete_product_number_sequence(sequence_id)
 
 
 def get_product_number_sequence(
     sequence_id: ProductNumberSequenceID,
 ) -> ProductNumberSequence:
     """Return the product number sequence, or raise an exception."""
-    db_sequence = db.session.get(DbProductNumberSequence, sequence_id)
+    db_sequence = product_sequence_repository.find_product_number_sequence(
+        sequence_id
+    )
 
     if db_sequence is None:
         raise ValueError(f'Unknown product number sequence ID "{sequence_id}"')
@@ -69,9 +57,11 @@ def get_product_number_sequences_for_shop(
     shop_id: ShopID,
 ) -> list[ProductNumberSequence]:
     """Return the product number sequences defined for that shop."""
-    db_sequences = db.session.scalars(
-        select(DbProductNumberSequence).filter_by(shop_id=shop_id)
-    ).all()
+    db_sequences = (
+        product_sequence_repository.get_product_number_sequences_for_shop(
+            shop_id
+        )
+    )
 
     return [
         _db_entity_to_product_number_sequence(db_sequence)
@@ -83,23 +73,13 @@ def generate_product_number(
     sequence_id: ProductNumberSequenceID,
 ) -> Result[ProductNumber, str]:
     """Generate and reserve the next product number from this sequence."""
-    row = db.session.execute(
-        update(DbProductNumberSequence)
-        .filter_by(id=sequence_id)
-        .values(value=DbProductNumberSequence.value + 1)
-        .returning(
-            DbProductNumberSequence.prefix, DbProductNumberSequence.value
-        )
-    ).one_or_none()
-    db.session.commit()
-
-    if row is None:
-        return Err(f'No product number sequence found for ID "{sequence_id}".')
-
-    prefix, value = row
-    product_number = ProductNumber(f'{prefix}{value:05d}')
-
-    return Ok(product_number)
+    match product_sequence_repository.generate_product_number(sequence_id):
+        case Ok(prefix_and_value):
+            prefix, value = prefix_and_value
+            product_number = ProductNumber(f'{prefix}{value:05d}')
+            return Ok(product_number)
+        case Err(e):
+            return Err(e)
 
 
 def _db_entity_to_product_number_sequence(
