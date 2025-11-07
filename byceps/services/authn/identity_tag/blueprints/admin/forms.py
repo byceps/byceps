9e788ba7ff05bbeea84aa619e0b4832a485ab233
Jochen Kupperschmidt <homework@nwsnet.de>
2025-11-07 18:08:43 +0100

    Move user name validation function for identity tag creation form into form class

--- byceps/services/authn/identity_tag/blueprints/admin/forms.py
+++ byceps/services/authn/identity_tag/blueprints/admin/forms.py
@@ -15,23 +15,9 @@ from byceps.services.authn.identity_tag import authn_identity_tag_service
 from byceps.util.l10n import LocalizedForm
 
 
-def validate_user_screen_name(form, field):
-    screen_name = field.data.strip()
-
-    user = user_service.find_user_by_screen_name(screen_name)
-
-    if user is None:
-        raise ValidationError(lazy_gettext('Unknown username'))
-
-    field.data = user
-
-
 class CreateForm(LocalizedForm):
     identifier = StringField(lazy_gettext('Identifier'), [InputRequired()])
-    user = StringField(
-        lazy_gettext('Username'),
-        [InputRequired(), validate_user_screen_name],
-    )
+    user = StringField(lazy_gettext('Username'), [InputRequired()])
     note = StringField(lazy_gettext('Note'), [Optional()])
     suspended = BooleanField(lazy_gettext('suspended'))
 
@@ -46,3 +32,14 @@ class CreateForm(LocalizedForm):
                     'This value is not available. Please choose another.'
                 )
             )
+
+    @staticmethod
+    def validate_user(form, field):
+        screen_name = field.data.strip()
+
+        user = user_service.find_user_by_screen_name(screen_name)
+
+        if user is None:
+            raise ValidationError(lazy_gettext('Unknown username'))
+
+        field.data = user
