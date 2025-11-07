d33d881f5722793ce79d2b1f3c95fc5624272b7a
Jochen Kupperschmidt <homework@nwsnet.de>
2025-11-07 18:06:45 +0100

    Move user name validation function for orga status grant form into form class

--- byceps/services/orga/blueprints/admin/forms.py
+++ byceps/services/orga/blueprints/admin/forms.py
@@ -15,30 +15,27 @@ from byceps.services.user import user_service
 from byceps.util.l10n import LocalizedForm
 
 
-def validate_user_screen_name(form, field):
-    screen_name = field.data.strip()
-
-    user = user_service.find_user_by_screen_name(screen_name)
-
-    if user is None:
-        raise ValidationError(lazy_gettext('Unknown username'))
-
-    if orga_service.has_orga_status(user.id, form.brand_id):
-        raise ValidationError(
-            lazy_gettext(
-                'The user already has organizer status for this brand.'
-            )
-        )
-
-    field.data = user
-
-
 class GrantOrgaStatusForm(LocalizedForm):
     def __init__(self, *args, brand_id=None, **kwargs):
         super().__init__(*args, **kwargs)
         self.brand_id = brand_id
 
-    user = StringField(
-        lazy_gettext('Username'),
-        [InputRequired(), validate_user_screen_name],
-    )
+    user = StringField(lazy_gettext('Username'), [InputRequired()])
+
+    @staticmethod
+    def validate_user(form, field):
+        screen_name = field.data.strip()
+
+        user = user_service.find_user_by_screen_name(screen_name)
+
+        if user is None:
+            raise ValidationError(lazy_gettext('Unknown username'))
+
+        if orga_service.has_orga_status(user.id, form.brand_id):
+            raise ValidationError(
+                lazy_gettext(
+                    'The user already has organizer status for this brand.'
+                )
+            )
+
+        field.data = user
