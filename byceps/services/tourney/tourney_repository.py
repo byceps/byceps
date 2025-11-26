d9620448f6a5c5f1131596134e21f9eebbbeb6ec
Jochen Kupperschmidt <homework@nwsnet.de>
2025-11-26 03:35:58 +0100

    Extract tourney repository

---
+++ byceps/services/tourney/tourney_repository.py
@@ -0,0 +1,141 @@
+"""
+byceps.services.tourney.tourney_repository
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Copyright: 2014-2025 Jochen Kupperschmidt
+:License: Revised BSD (see `LICENSE` file for details)
+"""
+
+from collections.abc import Sequence
+from datetime import datetime
+
+from sqlalchemy import delete, select
+
+from byceps.database import db
+from byceps.services.party.models import PartyID
+
+from .dbmodels.participant import DbParticipant
+from .dbmodels.tourney import DbTourney
+from .dbmodels.tourney_category import DbTourneyCategory
+from .models import Tourney, TourneyCategoryID, TourneyID
+
+
+def create_tourney(tourney: Tourney) -> None:
+    """Create a tourney."""
+    db_tourney = DbTourney(
+        tourney.id,
+        tourney.party_id,
+        tourney.title,
+        tourney.category_id,
+        tourney.team_size,
+        tourney.max_participant_count,
+        tourney.starts_at,
+        subtitle=tourney.subtitle,
+        logo_url=tourney.logo_url,
+        registration_open=tourney.registration_open,
+    )
+
+    db.session.add(db_tourney)
+    db.session.commit()
+
+
+def update_tourney(
+    tourney_id: TourneyID,
+    title: str,
+    subtitle: str | None,
+    logo_url: str | None,
+    category_id: TourneyCategoryID,
+    max_participant_count: int,
+    starts_at: datetime,
+) -> DbTourney:
+    """Update tourney."""
+    db_tourney = get_tourney(tourney_id)
+
+    db_tourney.title = title
+    db_tourney.subtitle = subtitle
+    db_tourney.logo_url = logo_url
+    db_tourney.category_id = category_id
+    db_tourney.max_participant_count = max_participant_count
+    db_tourney.starts_at = starts_at
+
+    db.session.commit()
+
+    return db_tourney
+
+
+def open_registration(tourney: Tourney) -> None:
+    """Open the registration for a tourney."""
+    db_tourney = get_tourney(tourney.id)
+    db_tourney.registration_open = tourney.registration_open
+    db.session.commit()
+
+
+def close_registration(tourney: Tourney) -> None:
+    """Close the registration for a tourney."""
+    db_tourney = get_tourney(tourney.id)
+    db_tourney.registration_open = tourney.registration_open
+    db.session.commit()
+
+
+def delete_tourney(tourney_id: TourneyID) -> None:
+    """Delete a tourney."""
+    db.session.execute(delete(DbTourney).filter_by(id=tourney_id))
+    db.session.commit()
+
+
+def find_tourney(tourney_id: TourneyID) -> DbTourney | None:
+    """Return the tourney with that id, or `None` if not found."""
+    return db.session.get(DbTourney, tourney_id)
+
+
+def get_tourney(tourney_id: TourneyID) -> DbTourney:
+    """Return the tourney with that ID.
+
+    Raise an exception if not found.
+    """
+    db_tourney = find_tourney(tourney_id)
+
+    if db_tourney is None:
+        raise ValueError(f'Unknown tourney ID "{tourney_id}"')
+
+    return db_tourney
+
+
+def find_tourney_with_category(
+    tourney_id: TourneyID,
+) -> tuple[DbTourney, DbTourneyCategory, int] | None:
+    """Return the tourney with that id, with categories and participant
+    counts, or `None` if not found.
+    """
+    return (
+        db.session.execute(
+            select(
+                DbTourney, DbTourneyCategory, db.func.count(DbParticipant.id)
+            )
+            .join(DbTourneyCategory)
+            .join(DbParticipant, isouter=True)
+            .filter(DbTourney.id == tourney_id)
+            .group_by(DbTourney.id, DbTourneyCategory)
+        )
+        .tuples()
+        .one_or_none()
+    )
+
+
+def get_tourneys_for_party(
+    party_id: PartyID,
+) -> Sequence[tuple[DbTourney, DbTourneyCategory, int]]:
+    """Return the tourneys for that party, with categories and participant counts."""
+    return (
+        db.session.execute(
+            select(
+                DbTourney, DbTourneyCategory, db.func.count(DbParticipant.id)
+            )
+            .join(DbTourneyCategory)
+            .join(DbParticipant, isouter=True)
+            .filter(DbTourney.party_id == party_id)
+            .group_by(DbTourney.id, DbTourneyCategory)
+        )
+        .tuples()
+        .all()
+    )
--- byceps/services/tourney/tourney_service.py
+++ byceps/services/tourney/tourney_service.py
@@ -8,16 +8,16 @@ byceps.services.tourney.tourney_service
 
 from datetime import datetime
 
-from sqlalchemy import delete, select
-
-from byceps.database import db
 from byceps.services.party.models import Party, PartyID
 from byceps.services.tourney.log import tourney_log_service
 from byceps.services.user.models.user import User
 from byceps.util.result import Err, Ok, Result
 
-from . import tourney_category_service, tourney_domain_service
-from .dbmodels.participant import DbParticipant
+from . import (
+    tourney_category_service,
+    tourney_domain_service,
+    tourney_repository,
+)
 from .dbmodels.tourney import DbTourney
 from .dbmodels.tourney_category import DbTourneyCategory
 from .events import (
@@ -61,29 +61,11 @@ def create_tourney(
         registration_open=registration_open,
     )
 
-    _persist_tourney_creation(tourney)
+    tourney_repository.create_tourney(tourney)
 
     tourney_log_service.persist_entry(log_entry)
 
     return tourney, event
-
-
-def _persist_tourney_creation(tourney: Tourney) -> None:
-    db_tourney = DbTourney(
-        tourney.id,
-        tourney.party_id,
-        tourney.title,
-        tourney.category_id,
-        tourney.team_size,
-        tourney.max_participant_count,
-        tourney.starts_at,
-        subtitle=tourney.subtitle,
-        logo_url=tourney.logo_url,
-        registration_open=tourney.registration_open,
-    )
-
-    db.session.add(db_tourney)
-    db.session.commit()
 
 
 def update_tourney(
@@ -96,16 +78,15 @@ def update_tourney(
     starts_at: datetime,
 ) -> Tourney:
     """Update tourney."""
-    db_tourney = _get_db_tourney(tourney_id)
-
-    db_tourney.title = title
-    db_tourney.subtitle = subtitle
-    db_tourney.logo_url = logo_url
-    db_tourney.category_id = category_id
-    db_tourney.max_participant_count = max_participant_count
-    db_tourney.starts_at = starts_at
-
-    db.session.commit()
+    db_tourney = tourney_repository.update_tourney(
+        tourney_id,
+        title,
+        subtitle,
+        logo_url,
+        category_id,
+        max_participant_count,
+        starts_at,
+    )
 
     return _db_entity_to_tourney(db_tourney)
 
@@ -114,8 +95,6 @@ def open_registration(
     tourney: Tourney, initiator: User
 ) -> Result[tuple[Tourney, TourneyRegistrationOpenedEvent], str]:
     """Open the registration for a tourney."""
-    db_tourney = _get_db_tourney(tourney.id)
-
     result = tourney_domain_service.open_registration(tourney, initiator)
     match result:
         case Err(e):
@@ -123,8 +102,7 @@ def open_registration(
 
     updated_tourney, event, log_entry = result.unwrap()
 
-    db_tourney.registration_open = updated_tourney.registration_open
-    db.session.commit()
+    tourney_repository.open_registration(updated_tourney)
 
     tourney_log_service.persist_entry(log_entry)
 
@@ -135,8 +113,6 @@ def close_registration(
     tourney: Tourney, initiator: User
 ) -> Result[tuple[Tourney, TourneyRegistrationClosedEvent], str]:
     """Close the registration for a tourney."""
-    db_tourney = _get_db_tourney(tourney.id)
-
     result = tourney_domain_service.close_registration(tourney, initiator)
     match result:
         case Err(e):
@@ -144,8 +120,7 @@ def close_registration(
 
     updated_tourney, event, log_entry = result.unwrap()
 
-    db_tourney.registration_open = updated_tourney.registration_open
-    db.session.commit()
+    tourney_repository.close_registration(updated_tourney)
 
     tourney_log_service.persist_entry(log_entry)
 
@@ -156,22 +131,17 @@ def delete_tourney(tourney_id: TourneyID) -> None:
     """Delete a tourney."""
     tourney = get_tourney(tourney_id)
 
-    db.session.execute(delete(DbTourney).filter_by(id=tourney.id))
-    db.session.commit()
+    tourney_repository.delete_tourney(tourney.id)
 
 
 def find_tourney(tourney_id: TourneyID) -> Tourney | None:
     """Return the tourney with that id, or `None` if not found."""
-    db_tourney = _find_db_tourney(tourney_id)
+    db_tourney = tourney_repository.find_tourney(tourney_id)
 
     if db_tourney is None:
         return None
 
     return _db_entity_to_tourney(db_tourney)
-
-
-def _find_db_tourney(tourney_id: TourneyID) -> DbTourney | None:
-    return db.session.get(DbTourney, tourney_id)
 
 
 def get_tourney(tourney_id: TourneyID) -> Tourney:
@@ -187,20 +157,11 @@ def get_tourney(tourney_id: TourneyID) -> Tourney:
     return tourney
 
 
-def _get_db_tourney(tourney_id: TourneyID) -> DbTourney:
-    db_tourney = _find_db_tourney(tourney_id)
-
-    if db_tourney is None:
-        raise ValueError(f'Unknown tourney ID "{tourney_id}"')
-
-    return db_tourney
-
-
 def find_tourney_with_category(
     tourney_id: TourneyID,
 ) -> TourneyWithCategory | None:
     """Return the tourney with that id, or `None` if not found."""
-    row = _find_db_tourney_with_category(tourney_id)
+    row = tourney_repository.find_tourney_with_category(tourney_id)
 
     if row is None:
         return None
@@ -210,33 +171,9 @@ def find_tourney_with_category(
     return _to_tourney_with_category(db_tourney, db_category, participant_count)
 
 
-def _find_db_tourney_with_category(
-    tourney_id: TourneyID,
-) -> tuple[DbTourney, DbTourneyCategory, int] | None:
-    return (
-        db.session.execute(
-            select(
-                DbTourney, DbTourneyCategory, db.func.count(DbParticipant.id)
-            )
-            .join(DbTourneyCategory)
-            .join(DbParticipant, isouter=True)
-            .filter(DbTourney.id == tourney_id)
-            .group_by(DbTourney.id, DbTourneyCategory)
-        )
-        .tuples()
-        .one_or_none()
-    )
-
-
 def get_tourneys_for_party(party_id: PartyID) -> list[TourneyWithCategory]:
-    """Return the tourneys for that party."""
-    rows = db.session.execute(
-        select(DbTourney, DbTourneyCategory, db.func.count(DbParticipant.id))
-        .join(DbTourneyCategory)
-        .join(DbParticipant, isouter=True)
-        .filter(DbTourney.party_id == party_id)
-        .group_by(DbTourney.id, DbTourneyCategory)
-    ).all()
+    """Return the tourneys for that party, with categories and participant counts."""
+    rows = tourney_repository.get_tourneys_for_party(party_id)
 
     return [
         _to_tourney_with_category(db_tourney, db_category, participant_count)
