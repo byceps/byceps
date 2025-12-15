eb805c9f5b6d15a3b5a95adbc7d6adee47a51c1e
Jochen Kupperschmidt <homework@nwsnet.de>
2025-12-15 02:31:19 +0100

    Remove import from `byceps.services.board.dbmodels.__init__`

--- byceps/services/board/dbmodels/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-byceps.services.board.dbmodels
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-:Copyright: 2014-2025 Jochen Kupperschmidt
-:License: Revised BSD (see `LICENSE` file for details)
-"""
-
-from . import board_access_grant  # noqa: F401
