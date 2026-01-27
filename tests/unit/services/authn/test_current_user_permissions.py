cb90c4a1d61ca44fb1fe1ae27602d851f2162ba5
Jochen Kupperschmidt <homework@nwsnet.de>
2026-01-27 03:13:50 +0100

    Turn integration tests for `has_current_user_[any_]permission` into unit tests for `CurrentUser.has_[any_]permission`

--- tests/unit/services/authn/test_current_user_permissions.py
+++ tests/unit/services/authn/test_current_user_permissions.py
@@ -3,30 +3,12 @@
 :License: Revised BSD (see `LICENSE` file for details)
 """
 
-from flask import g
-from flask_babel import lazy_gettext
+from uuid import UUID
+
 import pytest
 
-from byceps.util.authz import (
-    has_current_user_any_permission,
-    has_current_user_permission,
-    register_permissions,
-)
-
-
-register_permissions(
-    'chill',
-    [
-        ('browse_the_web', lazy_gettext('Browse the web')),
-        ('play_video_games', lazy_gettext('Play video games')),
-        ('watch_movies', lazy_gettext('Watch movies')),
-    ],
-)
-
-
-class CurrentUserMock:
-    def __init__(self, permissions: set[str]) -> None:
-        self.permissions = permissions
+from byceps.services.authn.session.models import CurrentUser
+from byceps.services.user.models import UserID
 
 
 @pytest.mark.parametrize(
@@ -65,11 +47,11 @@ class CurrentUserMock:
         ),
     ],
 )
-def test_has_current_user_permission(
-    site_app, permissions_assigned, permission_requested, expected
+def test_current_user_has_permission(
+    permissions_assigned, permission_requested, expected
 ):
-    g.user = CurrentUserMock(permissions_assigned)
-    assert has_current_user_permission(permission_requested) == expected
+    current_user = build_current_user(permissions_assigned)
+    assert current_user.has_permission(permission_requested) == expected
 
 
 @pytest.mark.parametrize(
@@ -100,8 +82,22 @@ def test_has_current_user_permission(
         ),
     ],
 )
-def test_has_current_user_any_permission(
-    site_app, permissions_assigned, permissions_requested, expected
+def test_current_user_has_any_permission(
+    permissions_assigned, permissions_requested, expected
 ):
-    g.user = CurrentUserMock(permissions_assigned)
-    assert has_current_user_any_permission(*permissions_requested) == expected
+    current_user = build_current_user(permissions_assigned)
+    assert current_user.has_any_permission(*permissions_requested) == expected
+
+
+def build_current_user(permissions: set[str]) -> CurrentUser:
+    return CurrentUser(
+        id=UserID(UUID('d14b0dd0-886f-42a1-990a-c2435ae60123')),
+        screen_name='SomeUser',
+        initialized=True,
+        suspended=False,
+        deleted=False,
+        avatar_url='/static/user_avatar_fallback.svg',
+        locale=None,
+        authenticated=False,
+        permissions=frozenset(permissions),
+    )
