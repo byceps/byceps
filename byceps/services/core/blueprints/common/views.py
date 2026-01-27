e69cef640ab237bd4ca9303f1eb041cb6b53abaf
Jochen Kupperschmidt <homework@nwsnet.de>
2026-01-27 04:04:14 +0100

    Replace `has_current_user_any_permission` in Python code with `g.user.has_any_permission`

--- byceps/services/core/blueprints/common/views.py
+++ byceps/services/core/blueprints/common/views.py
@@ -12,7 +12,6 @@ from typing import Any
 from flask import g, render_template
 
 from byceps.byceps_app import get_current_byceps_app
-from byceps.util.authz import has_current_user_any_permission
 from byceps.util.framework.blueprint import create_blueprint
 from byceps.util.navigation import Navigation
 
@@ -37,7 +36,7 @@ def inject_template_variables() -> dict[str, Any]:
         'now': datetime.utcnow(),
         'today': date.today(),
         'Navigation': Navigation,
-        'has_current_user_any_permission': has_current_user_any_permission,
+        'has_current_user_any_permission': g.user.has_any_permission,
         'has_current_user_permission': g.user.has_permission,
     }
 
